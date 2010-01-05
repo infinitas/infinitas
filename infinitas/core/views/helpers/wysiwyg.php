35184c3a2ecd4f33fa13414edfe7714d29b2beda
deizel <web@deizel.co.uk>
2010-01-05 00:28:30 +0000

    forgot the add quotes in last commit

--- infinitas/core/views/helpers/wysiwyg.php
+++ infinitas/core/views/helpers/wysiwyg.php
@@ -49,7 +49,7 @@
 <script type="text/javascript">
 fckLoader_$did = function () {
     var bFCKeditor_$did = new FCKeditor('$did');
-    bFCKeditor_$did.BasePath = $path;
+    bFCKeditor_$did.BasePath = '$path';
     bFCKeditor_$did.ToolbarSet = '$toolbar';
     bFCKeditor_$did.ReplaceTextarea();
 }
