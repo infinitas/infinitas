fd27b5cf678cb712a73f5e307612c3ed8a003a67
dogmatic <dogmatic69@gmail.com>
2010-01-06 03:45:19 +0200

    new format

--- app_helper.php
+++ app_helper.php
@@ -1,361 +1,314 @@
 <?php
-    /**
-     * Comment Template.
-     *
-     * @todo -c Implement .this needs to be sorted out.
-     *
-     * Copyright (c) 2009 Carl Sutton ( dogmatic69 )
-     *
-     * Licensed under The MIT License
-     * Redistributions of files must retain the above copyright notice.
-     *
-     * @filesource
-     * @copyright     Copyright (c) 2009 Carl Sutton ( dogmatic69 )
-     * @link          http://www.dogmatic.co.za
-     * @package       sort
-     * @subpackage    sort.comments
-     * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
-     * @since         0.5a
-     */
+/**
+ * Comment Template.
+ *
+ * @todo Implement .this needs to be sorted out.
+ *
+ * Copyright (c) 2009 Carl Sutton ( dogmatic69 )
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ * @filesource
+ * @copyright Copyright (c) 2009 Carl Sutton ( dogmatic69 )
+ * @link http://www.dogmatic.co.za
+ * @package sort
+ * @subpackage sort.comments
+ * @license http://www.opensource.org/licenses/mit-license.php The MIT License
+ * @since 0.5a
+ */
 
-    class AppHelper extends Helper
-    {
-        var $rowClassCounter = 0;
+class AppHelper extends Helper {
+	var $rowClassCounter = 0;
 
-        var $paginationCounterFormat = 'Page %page% of %pages%.';
+	var $paginationCounterFormat = 'Page %page% of %pages%.';
 
-        var $errors = array();
+	var $errors = array();
 
-        var $wysiwyg = 'fck';
+	var $wysiwyg = 'fck';
 
-        var $helpers = array( 'Html', 'Design', 'Core.Wysiwyg', 'Core.Gravatar' );
+	var $helpers = array('Html', 'Design', 'Core.Wysiwyg', 'Core.Gravatar');
 
-        /**
-        * create some bread crumbs.
-        *
-        * Creates some bread crumbs.
-        *
-        * @todo -cAppHelper Implement AppHelper.
-        * - generate some links
-        *
-        * @param array $here is $this from the view.
-        */
-        function breadcrumbs( $view = array(), $seperator = ' :: ' )
-        {
-            if ( empty( $view ) )
-            {
-                return false;
-            }
+	/**
+	 * create some bread crumbs.
+	 *
+	 * Creates some bread crumbs.
+	 *
+	 * @todo -c"AppHelper" Implement AppHelper.
+	 * - generate some links
+	 * @param array $here is $this from the view.
+	 */
+	function breadcrumbs($view = array(), $seperator = ' :: ') {
+		if (empty($view)) {
+			return false;
+		}
 
-            $breadcrumbs = array(
-                $view->params['prefix'],
-                $view->params['plugin'],
-                $view->name
-            );
+		$breadcrumbs = array($view->params['prefix'],
+			$view->params['plugin'],
+			$view->name
+			);
 
-            return implode( $seperator, $breadcrumbs );
-        }
+		return implode($seperator, $breadcrumbs);
+	}
 
-        /**
-         * switch the class for table rows
-         *
-         * @param integer $i the number of the row
-         * @param string $class1 class 1 highlight
-         * @param string $class2 class 2 highlight
-         * @return string the class
-         */
-        function rowClass( $class1 = 'bg', $class2 = '' )
-        {
-            return ( ( $this->rowClassCounter++ % 2 ) ? $class1 : $class2 );
-        }
+	/**
+	 * switch the class for table rows
+	 *
+	 * @param integer $i the number of the row
+	 * @param string $class1 class 1 highlight
+	 * @param string $class2 class 2 highlight
+	 * @return string the class
+	 */
+	function rowClass($class1 = 'bg', $class2 = '') {
+		return (($this->rowClassCounter++ % 2) ? $class1 : $class2);
+	}
 
-        function adminPageHead( $view )
-        {
-            if ( empty( $view ) )
-            {
-                return false;
-            }
+	function adminPageHead($view) {
+		if (empty($view)) {
+			return false;
+		}
 
-            return '<div class="top-bar"><h1>'.__( $view->name, true ).'</h1>'.
-                '<div class="breadcrumbs">'.$this->breadcrumbs( $view ).'</div></div>';
-        }
+		return '<div class="top-bar"><h1>' . __($view->name, true) . '</h1>' .
+		'<div class="breadcrumbs">' . $this->breadcrumbs($view) . '</div></div>';
+	}
 
-        /**
-        * creates table headers for admin.
-        *
-        * if the format is just array( 'head1', 'head2' ... ) it will output a
-        * normal table with TH that have no classes/styles applied.  you can
-        * also pass things like array ( 'head1' => array( 'class' => 'something' ) )
-        * to get out put like <th class="something">head1</th>
-        *
-        * @param array $data an array of items for the head.
-        */
-        function adminTableHeader( $data )
-        {
-            $out = '<tr>';
-                foreach( $data as $field => $params )
-                {
-                    $atributes = '';
+	/**
+	 * creates table headers for admin.
+	 *
+	 * if the format is just array( 'head1', 'head2' ... ) it will output a
+	 * normal table with TH that have no classes/styles applied.  you can
+	 * also pass things like array ( 'head1' => array( 'class' => 'something' ) )
+	 * to get out put like <th class="something">head1</th>
+	 *
+	 * @param array $data an array of items for the head.
+	 */
+	function adminTableHeader($data) {
+		$out = '<tr>';
+		foreach($data as $field => $params) {
+			$atributes = '';
 
-                    if ( is_int( $field ) && !is_array( $params ) )
-                    {
-                        $field = $params;
-                        $params = '';
-                    }
+			if (is_int($field) && !is_array($params)) {
+				$field = $params;
+				$params = '';
+			}else {
+				foreach($params as $type => $param) {
+					$atributes = '';
+					$atributes .= $type . '="' . $param . '" ';
+				}
 
-                    else
-                    {
+				$atributes .= $atributes;
+			}
 
-                        foreach( $params as $type => $param )
-                        {
-                            $atributes = '';
-                            $atributes .= $type.'="'.$param.'" ';
-                        }
+			if ($atributes != '') {
+				$params = $atributes;
+			}
 
-                        $atributes .= $atributes;
-                    }
+			$out .= '<th ' . $params . '>' . $field . '</th>';
+		}
+		$out .= '</tr>';
 
-                    if ( $atributes != '' )
-                    {
-                        $params = $atributes;
-                    }
+		return $out;
+	}
 
-                    $out .= '<th '.$params.'>'.$field.'</th>';
-                }
-            $out .= '</tr>';
+	/**
+	 * creates the header images for the admin table headers.
+	 */
+	function adminTableHeadImages() {
+		return $this->Html->image(
+			'admin/bg-th-left.gif',
+			array(
+				'width' => '8px',
+				'height' => '7px',
+				'class' => 'left'
+				)
+			) .
+		$this->Html->image(
+			'admin/bg-th-right.gif',
+			array(
+				'width' => '8px',
+				'height' => '7px',
+				'class' => 'right'
+				)
+			);
+	}
 
-            return $out;
-        }
+	function adminIndexHead($view = array(), $pagintion = array(), $filterOptions = array(), $massActions = null) {
+		if (empty($view)) {
+			$this->errors[] = 'I need the view.';
+			return false;
+		}
 
-        /**
-        * creates the header images for the admin table headers.
-        */
-        function adminTableHeadImages()
-        {
+		App::import('Helper', 'FilterHelper');
 
-            return $this->Html->image(
-                'admin/bg-th-left.gif',
-                array(
-                    'width' => '8px',
-                    'height' => '7px',
-                    'class' => 'left'
-                )
-            ).
-            $this->Html->image(
-                'admin/bg-th-right.gif',
-                array(
-                    'width' => '8px',
-                    'height' => '7px',
-                    'class' => 'right'
-                )
-            );
-        }
+		$filters = $this->Design->niceBox('filter', FilterHelper::clear($filterOptions));
 
-        function adminIndexHead( $view = array(), $pagintion = array(), $filterOptions = array(), $massActions = null )
-        {
-            if ( empty( $view ) )
-            {
-                $this->errors[] = 'I need the view.';
-                return false;
-            }
+		return $this->Design->niceBox('adminTopBar', $this->adminPageHead($view) . $massActions) . $filters;
+	}
 
-            App::import( 'Helper', 'FilterHelper' );
+	function adminOtherHead($view = array()) {
+		if (empty($view)) {
+			$this->errors[] = 'I need the view.';
+			return false;
+		}
 
-            $filters = $this->Design->niceBox( 'filter', FilterHelper::clear( $filterOptions ) );
+		$out = '<div class="adminTopBar">';
+		$out .= $this->adminPageHead($view);
+		$out .= '<div class="main-actions">';
+		$out .= $this->Html->link(
+			'Index',
+			array(
+				'action' => 'index'
+				)
+			) . ' ';
+		$out .= $this->Html->link(
+			'Add',
+			array(
+				'action' => 'add'
+				)
+			);
+		$out .= '</div>';
+		$out .= '</div><div class="clr">&nbsp;</div>';
 
-            return $this->Design->niceBox( 'adminTopBar', $this->adminPageHead( $view ).$massActions ).$filters;
-        }
+		return $out;
+	}
 
-        function adminOtherHead( $view = array() )
-        {
-            if ( empty( $view ) )
-            {
-                $this->errors[] = 'I need the view.';
-                return false;
-            }
+	function ordering($id = null, $order = null) {
+		if (!$id) {
+			$this->errors[] = 'You need an id to move something';
+			return false;
+		}
 
-            $out = '<div class="adminTopBar">';
-                $out .= $this->adminPageHead( $view );
-                $out .= '<div class="main-actions">';
-                    $out .= $this->Html->link(
-                        'Index',
-                        array(
-                            'action' => 'index'
-                        )
-                    ).' ';
-                    $out .= $this->Html->link(
-                        'Add',
-                        array(
-                            'action' => 'add'
-                        )
-                    );
-                $out .= '</div>';
-            $out .= '</div><div class="clr">&nbsp;</div>';
+		if (!$order) {
+			$this->errors[] = 'The order was not passed';
+		}
 
-            return $out;
-        }
+		$out = $this->Html->link($order, array('#' => $order));
 
-        function ordering( $id = null, $order = null )
-        {
-            if ( !$id )
-            {
-                $this->errors[] = 'You need an id to move something';
-                return false;
-            }
+		$out .= $this->Html->link($this->Html->image($this->Image->getRelativePath('actions', 'arrow-up'),
+				array(
+					'alt' => __('Up', true),
+					'title' => __('Move up', true),
+					'width' => '16px',
+					'class' => 'arrow-up'
+					)
+				),
+			array(
+				'action' => 'reorder',
+				$id,
+				'direction' => 'up',
+				'amount' => 1
+				),
+			array(
+				'escape' => false,
+				)
+			);
 
-            if ( !$order )
-            {
-                $this->errors[] = 'The order was not passed';
-            }
+		$out .= $this->Html->link($this->Html->image($this->Image->getRelativePath('actions', 'arrow-down'),
+				array(
+					'alt' => __('Down', true),
+					'title' => __('Move down', true),
+					'width' => '16px',
+					'class' => 'arrow-down'
+					)
+				),
+			array(
+				'action' => 'reorder',
+				$id,
+				'direction' => 'down',
+				'amount' => 1
+				),
+			array(
+				'escape' => false,
+				)
+			);
 
-            $out  = $this->Html->link( $order, array( '#' => $order ) );
+		return $out;
+	}
 
-            $out .= $this->Html->link(
-                $this->Html->image(
-                    $this->Image->getRelativePath( 'actions', 'arrow-up' ),
-                    array(
-                        'alt'   => __( 'Up', true ),
-                        'title' => __( 'Move up', true ),
-                        'width' => '16px',
-                        'class' => 'arrow-up'
-                    )
-                ),
-                array(
-                    'action' => 'reorder',
-                    $id,
-                    'direction' => 'up',
-                    'amount' => 1
-                ),
-                array(
-                    'escape' => false,
-                )
-            );
+	function paginationCounter($pagintion) {
+		if (empty($pagintion)) {
+			$this->errors[] = 'You did not pass the pagination object.';
+			return false;
+		}
 
-            $out .= $this->Html->link(
-                $this->Html->image(
-                    $this->Image->getRelativePath( 'actions', 'arrow-down' ),
-                    array(
-                        'alt'   => __( 'Down', true ),
-                        'title' => __( 'Move down', true ),
-                        'width' => '16px',
-                        'class' => 'arrow-down'
-                    )
-                ),
-                array(
-                    'action' => 'reorder',
-                    $id,
-                    'direction' => 'down',
-                    'amount' => 1
-                ),
-                array(
-                    'escape' => false,
-                )
-            );
+		$out = '';
+		$out .= $pagintion->counter(array('format' => __($this->paginationCounterFormat, true)));
+		$out .= '';
 
-            return $out;
-        }
+		return $out;
+	}
 
-        function paginationCounter( $pagintion )
-        {
-            if ( empty( $pagintion ) )
-            {
-                $this->errors[] = 'You did not pass the pagination object.';
-                return false;
-            }
+	function wysiwyg($id = null, $toolbar = 'Basic') {
+		if (!$id) {
+			$this->errors[] = 'No field specified for the wysiwyg editor';
+			return false;
+		}
 
-            $out = '';
-                $out .= $pagintion->counter( array( 'format' => __( $this->paginationCounterFormat, true ) ) );
-            $out .= '';
+		if (!Configure::read('Wysiwyg.editor')) {
+			$this->errors[] = 'There is no editor configured';
+			return false;
+		}
 
-            return $out;
-        }
+		$editor = (Configure::read('Wysiwyg.editor')) ? Configure::read('Wysiwyg.editor') : 'text';
 
-        function wysiwyg( $id = null, $toolbar = 'Basic' )
-        {
-            if ( !$id )
-            {
-                $this->errors[] = 'No field specified for the wysiwyg editor';
-                return false;
-            }
+		return $this->Wysiwyg->$editor($id, $toolbar);
+	}
 
-            if ( !Configure::read( 'Wysiwyg.editor' ) )
-            {
-                $this->errors[] = 'There is no editor configured';
-                return false;
-            }
+	function gravatar($email = null, $options = array()) {
+		if (!$email) {
+			$this->errors[] = 'no email specified for the gravatar.';
+			return false;
+		}
 
-            $editor = ( Configure::read( 'Wysiwyg.editor' ) ) ? Configure::read( 'Wysiwyg.editor' ) : 'text';
+		return $this->Gravatar->image($email, $options);
+	}
 
-            return $this->Wysiwyg->$editor( $id, $toolbar );
-        }
+	function niceBox($class = '', $content = false) {
+		$out = '<div class="niceBox ' . $class . '"><div class="top"><div class="top"><div class="top"></div></div></div>';
+		$out .= '<div class="middle">';
+		if ($content) {
+			$out .= $content . '<div class="clr"></div>';
+		}
+		if ($content) {
+			$out .= $this->niceBoxEnd();
+		}
+		return $out;
+	}
 
-        function gravatar( $email = null, $options = array() )
-        {
-            if ( !$email )
-            {
-                $this->errors[] = 'no email specified for the gravatar.';
-                return false;
-            }
+	function niceBoxEnd() {
+		$out = '</div>';
+		$out .= '<div class="bottom"><div class="bottom"><div class="bottom"></div></div></div></div>';
+		return $out;
+	}
 
-            return $this->Gravatar->image( $email, $options );
-        }
+	function massActionButtons($buttons = null, $niceBox = false) {
+		if (!$buttons) {
+			$this->errors[] = 'No buttons set';
+			return false;
+		}
 
-        function niceBox( $class = '', $content = false )
-        {
-            $out = '<div class="niceBox '.$class.'"><div class="top"><div class="top"><div class="top"></div></div></div>';
-                $out .= '<div class="middle">';
-                    if ( $content )
-                    {
-                        $out .= $content.'<div class="clr"></div>';
-                    }
-                if ( $content )
-                {
-                    $out .= $this->niceBoxEnd();
-                }
-            return $out;
-        }
+		$out = '';
+		foreach($buttons as $button) {
+			$imagePath = $this->Image->getRelativePath(array('actions'), $button);
 
-        function niceBoxEnd()
-        {
-                $out = '</div>';
-            $out .= '<div class="bottom"><div class="bottom"><div class="bottom"></div></div></div></div>';
-            return $out;
-        }
+			if (!$imagePath) {
+				$imagePath = __(Inflector::humanize($button), true);
+			}
 
-        function massActionButtons( $buttons = null, $niceBox = false )
-        {
-            if ( !$buttons )
-            {
-                $this->errors[] = 'No buttons set';
-                return false;
-            }
+			$out .= $this->Form->submit($imagePath,
+				array(
+					'value' => strtolower(str_replace(array('-', ' '), '_', $button)),
+					'name' => 'action',
+					'title' => __(Inflector::humanize($button), true)
+					)
+				);
+		}
 
-            $out = '';
-            foreach( $buttons as $button )
-            {
-                $imagePath = $this->Image->getRelativePath( array( 'actions' ), $button );
+		if ($niceBox) {
+			return $this->Design->niceBox('massActions', $out);
+		}
 
-                if ( !$imagePath )
-                {
-                    $imagePath = __( Inflector::humanize( $button ), true );
-                }
+		return '<div class="massActions">' . $out . '<div class="clr">&nbsp;</div></div>';
+	}
+}
 
-                $out .= $this->Form->submit(
-                    $imagePath,
-                    array(
-                        'value' => strtolower( str_replace( array( '-', ' ' ), '_', $button ) ),
-                        'name'  => 'action',
-                        'title' => __( Inflector::humanize( $button ), true )
-                    )
-                );
-            }
-
-            if ( $niceBox )
-            {
-                return $this->Design->niceBox( 'massActions', $out );
-            }
-
-            return '<div class="massActions">'.$out.'<div class="clr">&nbsp;</div></div>';
-        }
-    }
 ?>
