61b2b6a9b496d3056e416511dfee2ad725346b36
dogmatic <dogmatic69@gmail.com>
2010-01-22 15:26:19 +0200

    adding checks to the route in the module loader so that modules are loaded only where they are set to load in the backend [#106]

--- extensions/libs/views/helpers/infinitas.php
+++ extensions/libs/views/helpers/infinitas.php
@@ -13,20 +13,33 @@
 			$modules = ClassRegistry::init('Management.Module')->getModules($position);
 
 			$out = '<div class="modules '.$position.'">';
+				$currentRoute = Router::currentRoute();
+
 				foreach($modules as $module){
-					$out .= '<div class="module '.$module['Module']['module'].'">';
+					$moduleOut = '<div class="module '.$module['Module']['module'].'">';
 						if ($module['Module']['show_heading']) {
-							$out .= '<h2>'.__($module['Module']['name'],true).'</h2>';
+							$moduleOut .= '<h2>'.__($module['Module']['name'],true).'</h2>';
 						}
 
 						if (!empty($module['Module']['module'])) {
 							$View = ClassRegistry::getObject('view');
-							$out .= $View->element('modules/'.$module['Module']['module']);
+							$moduleOut .= $View->element('modules/'.$module['Module']['module']);
 						}
 						else if (!empty($module['Module']['content'])) {
-							$out .= $module['Module']['content'];
+							$moduleOut .= $module['Module']['content'];
 						}
-					$out .= '</div>';
+					$moduleOut .= '</div>';
+
+					if (!empty($module['Route']) && is_object($currentRoute)){
+						foreach($module['Route'] as $route){
+							if ($route['url'] == $currentRoute->template) {
+								$out .= $moduleOut;
+							}
+						}
+					}
+					else if (empty($module['Route'])) {
+						$out .= $moduleOut;
+					}
 				}
 			$out .= '</div>';
 
