ea296b88a88eff16e34d89c5a5f838d860af4551
dogmatic <dogmatic69@gmail.com>
2010-01-27 00:49:06 +0200

    adding some comments

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,62 +1,21 @@
 <?php
-/**
- * This file is loaded automatically by the app/webroot/index.php file after the core bootstrap.php
- *
- * This is an application wide file to load any function that is not used within a class
- * define. You can also use this to include or require any files in your application.
- *
- * PHP versions 4 and 5
- *
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link http://cakephp.org CakePHP(tm) Project
- * @package cake
- * @subpackage cake.app.config
- * @since CakePHP(tm) v 0.10.8.2117
- * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
- */
-
-/**
- * The settings below can be used to set additional paths to models, views and controllers.
- * This is related to Ticket #470 (https://trac.cakephp.org/ticket/470)
- *
- * App::build(array(
- *      'plugins' => array('/full/path/to/plugins/', '/next/full/path/to/plugins/'),
- *      'models' =>  array('/full/path/to/models/', '/next/full/path/to/models/'),
- *      'views' => array('/full/path/to/views/', '/next/full/path/to/views/'),
- *      'controllers' => array('/full/path/to/controllers/', '/next/full/path/to/controllers/'),
- *      'datasources' => array('/full/path/to/datasources/', '/next/full/path/to/datasources/'),
- *      'behaviors' => array('/full/path/to/behaviors/', '/next/full/path/to/behaviors/'),
- *      'components' => array('/full/path/to/components/', '/next/full/path/to/components/'),
- *      'helpers' => array('/full/path/to/helpers/', '/next/full/path/to/helpers/'),
- *      'vendors' => array('/full/path/to/vendors/', '/next/full/path/to/vendors/'),
- *      'shells' => array('/full/path/to/shells/', '/next/full/path/to/shells/'),
- *      'locales' => array('/full/path/to/locale/', '/next/full/path/to/locale/')
- * ));
- */
-
-/**
- * As of 1.3, additional rules for the inflector are added below
- *
- * Inflector::rules('singular', array('rules' => array(), 'irregular' => array(), 'uninflected' => array()));
- * Inflector::rules('plural', array('rules' => array(), 'irregular' => array(), 'uninflected' => array()));
- */
-App::build(
-	array(
-		'plugins' => array(
-			APP . 'infinitas' . DS,
-			APP . 'extensions' . DS
+	/**
+	* Load all the plugin dirs
+	*/
+	App::build(
+		array(
+			'plugins' => array(
+				APP . 'infinitas' . DS,
+				APP . 'extensions' . DS
+			)
 		)
-	)
-);
+	);
 
-if(!defined('JSON_ERROR_NONE')){define('JSON_ERROR_NONE', 0);}
-if(!defined('JSON_ERROR_DEPTH')){define('JSON_ERROR_DEPTH', 1);}
-if(!defined('JSON_ERROR_CTRL_CHAR')){define('JSON_ERROR_CTRL_CHAR', 3);}
-if(!defined('JSON_ERROR_SYNTAX')){define('JSON_ERROR_SYNTAX', 4);}
+	/**
+	* Make sure the json defines are loaded.
+	*/
+	if(!defined('JSON_ERROR_NONE')){define('JSON_ERROR_NONE', 0);}
+	if(!defined('JSON_ERROR_DEPTH')){define('JSON_ERROR_DEPTH', 1);}
+	if(!defined('JSON_ERROR_CTRL_CHAR')){define('JSON_ERROR_CTRL_CHAR', 3);}
+	if(!defined('JSON_ERROR_SYNTAX')){define('JSON_ERROR_SYNTAX', 4);}
 ?>
