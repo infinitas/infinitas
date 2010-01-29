c1dfabe63f6cb1acc0c27633e095326723ce3290
dogmatic <dogmatic69@gmail.com>
2010-01-29 13:06:14 +0200

    code for menus

--- infinitas/management/controllers/menus_controller.php
+++ infinitas/management/controllers/menus_controller.php
@@ -2,12 +2,35 @@
 	class MenusController extends ManagementAppController{
 		var $name = 'Menus';
 
-		function admin_index(){
+		var $helpers = array('Libs.Infinitas');
 
+		function admin_index(){
+			$this->Menu->recursive = 0;
+
+			$menus = $this->paginate(
+				null,
+				$this->Filter->filter
+			);
+
+			$filterOptions = $this->Filter->filterOptions;
+			$filterOptions['fields'] = array(
+				'name',
+				'class'
+			);
+
+			$this->set(compact('menus','filterOptions'));
 		}
 
 		function admin_add(){
+			if (!empty($this->data)) {
+				$this->Menu->create();
+				if ($this->Menu->saveAll($this->data)) {
+					$this->Session->setFlash('Your menu has been saved.');
+					$this->redirect(array('action' => 'index'));
+				}
+			}
 
+			$this->set(compact());
 		}
 
 		function admin_edit($id = null){
