d01b77e6e131518c56fe884358a27c1519a643a6
dogmatic <dogmatic69@gmail.com>
2010-01-30 17:37:14 +0200

    when installing ignore other routes

--- config/routes.php
+++ config/routes.php
@@ -5,18 +5,19 @@
 	if (!file_exists(APP . 'config' . DS . 'database.php')) {
 		Router::connect('/', array('plugin' => 'installer', 'controller' => 'install', 'action' => 'index'));
 	}
+	else{
+		$routes = Cache::read('routes', 'core');
 
-	$routes = Cache::read('routes', 'core');
-
-	if (!$routes) {
-		$routes = Classregistry::init('Management.Route')->getRoutes();
-		if (empty($routes)) {
-			//something is broken
-			// @todo -c Implement .some error message or something
+		if (!$routes) {
+			$routes = Classregistry::init('Management.Route')->getRoutes();
+			if (empty($routes)) {
+				//something is broken
+				// @todo -c Implement .some error message or something
+			}
 		}
-	}
 
-	foreach($routes as $route ){
-		Router::connect($route['Route']['url'], $route['Route']['values'], $route['Route']['regex'] );
+		foreach($routes as $route ){
+			Router::connect($route['Route']['url'], $route['Route']['values'], $route['Route']['regex'] );
+		}
 	}
 ?>
