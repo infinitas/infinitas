dc057105765470ac7d2c560f1cd114042f7e6ed0
dogmatic <dogmatic69@gmail.com>
2010-02-06 15:40:49 +0200

    cleaning up internal helpers and moving status code to infinitas helper

--- extensions/libs/views/helpers/infinitas.php
+++ extensions/libs/views/helpers/infinitas.php
@@ -4,6 +4,17 @@
 	 *
 	 */
 	class InfinitasHelper extends AppHelper{
+		var $helpers = array(
+			'Html',
+			'Libs.Image'
+		);
+
+		/**
+		* JSON errors.
+		*
+		* Set up some errors for json.
+		* @access public
+		*/
 		var $_json_errors = array(
 		    JSON_ERROR_NONE      => 'No error',
 		    JSON_ERROR_DEPTH     => 'The maximum stack depth has been exceeded',
@@ -14,6 +25,16 @@
 		var $_menuData = '';
 		var $_menuLevel = 0;
 
+		/**
+		* Module Loader.
+		*
+		* This is used to load modules. it generates a wrapper div with the class
+		* set as the module name for easy styleing, and will create a header if set
+		* in the backend.
+		*
+		* @params string $position this is the possition that is to be loaded, can be anything from the database
+		* @params bool $admin if true its a admin module that should be loaded.
+		*/
 		function loadModules($position = null, $admin = false){
 			if (!$position) {
 				$this->errors[] = 'No position selected to load modules';
@@ -65,6 +86,16 @@
 			return $out;
 		}
 
+		/**
+		* Module Config.
+		*
+		* This method works out params from JSON data in the module. if there is something
+		* wrong with the JSON code that is submitted it will return an empty array(), or it
+		* will return an array with the config.
+		*
+		* @access protected
+		* @params string $config some JSON data to be decoded.
+		*/
 		function _moduleConfig($config = ''){
 			if (empty($config['config'])) {
 				return array();
@@ -156,6 +187,161 @@
 				$this->_menuData .= '</a>';
 
 			$this->_menuData .= '</li>';
+		}
+
+		var $external = true;
+
+		function status( $status = null )
+		{
+			$image = false;
+			$params = array();
+
+			switch ( strtolower( $status ) )
+			{
+				case 1:
+				case 'yes':
+				case 'on':
+					if ( $this->external )
+					{
+						$params['title'] = __( 'Active', true );
+					}
+
+					$image = $this->Html->image(
+					    $this->Image->getRelativePath( 'status', 'active' ),
+					    $params + array(
+					        'width' => '16px',
+					        'alt' => __( 'On', true )
+					    )
+					);
+					break;
+
+				case 0:
+				case 'no':
+				case 'off':
+					if ( $this->external )
+					{
+						$params['title'] = __( 'Disabled', true );
+					}
+
+					$image = $this->Html->image(
+					    $this->Image->getRelativePath( 'status', 'inactive' ),
+					    $params + array(
+					        'width' => '16px',
+					        'alt' => __( 'Off', true )
+					    )
+					);
+					break;
+			}
+
+			return $image;
+		}
+
+		/**
+		 * Toogle button
+		 *
+		 * Uses Status::status to get the image and then creates a link based on
+		 * the $method param
+		 */
+		function toggle( $status = null, $id = null, $url = array( 'action' => 'toggle' ) )
+		{
+			$params = array();
+
+			switch( $status )
+			{
+				case 0:
+				case 'off':
+				case 'no':
+					$params['title'] = __( 'Click to activate', true );
+					$params['alt'] = __( 'Disabled', true );
+					break;
+
+				case 1:
+				case 'yes':
+				case 'on':
+					$params['title'] = __( 'Click to disable', true );
+					$params['alt'] = __( 'Active', true );
+					break;
+				default:
+				;
+			} // switch
+
+			$this->external = false;
+
+			$link = $this->Html->link(
+			    $this->status( $status ),
+			    (array)$url + (array)$id
+			    ,
+			    $params + array(
+			        'escape' => false
+			    )
+			);
+
+			return $link;
+		}
+
+		function locked( $item = array(), $model = null )
+		{
+			if ( !$model || empty( $item ) || empty( $item[$model] ) )
+			{
+				$this->errors[] = 'you missing some data there.';
+				return false;
+			}
+
+			switch ( strtolower( $item[$model]['locked'] ) )
+			{
+				case 1:
+					$image = $this->Html->image(
+					    $this->Image->getRelativePath( 'status', 'locked' ),
+					    array(
+					        'alt' => __( 'Locked', true ),
+					        'width' => '16px',
+					        'title' => sprintf(
+					            __( 'This record was locked %s by %s', true ),
+					            $this->Time->timeAgoInWords( $item[$model]['locked_since'] ),
+					            $item['Locker']['username'] )
+					    )
+					);
+					break;
+
+				case 0:
+					$image = $this->Html->image(
+					    $this->Image->getRelativePath( 'status', 'not-locked' ),
+					    array(
+					        'alt' => __( 'Not Locked', true ),
+					        'width' => '16px',
+					        'title' => __( 'This record is not locked', true )
+					    )
+					);
+					break;
+			}
+
+			return $image;
+		}
+
+		function featured( $record = array(), $model = 'Feature' )
+		{
+			if ( empty( $record[$model] ) )
+			{
+				$this->messages[] = 'This has no featured items.';
+
+				return $this->Html->image(
+				    $this->Image->getRelativePath( 'status', 'not-featured' ),
+				    array(
+				        'alt'   => __( 'No', true ),
+				        'title' => __( 'Not a featured item', true ),
+				        'width' => '16px'
+				    )
+				);
+			}
+
+			return $this->Html->image(
+			    $this->Image->getRelativePath( 'status', 'featured' ),
+			    array(
+			        'alt'   => __( 'Yes', true ),
+			        'title' => __( 'Featured Item', true ),
+			        'width' => '16px'
+			    )
+			);
 		}
 	}
 ?>
