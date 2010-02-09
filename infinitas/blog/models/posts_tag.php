4e3852bb3fe7f3f672bbd8eadbf4903ceb61eb38
dogmatic <dogmatic69@gmail.com>
2010-02-09 17:07:55 +0200

    refactor code a bit and add a index for tags to display stats

--- infinitas/blog/models/posts_tag.php
+++ infinitas/blog/models/posts_tag.php
@@ -5,5 +5,18 @@
 	 */
 	class PostsTag extends BlogAppModel{
 		var $name = 'PostsTag';
+
+		function getUsedTagIds(){
+			$tags = $this->find(
+				'all',
+				array(
+					'fields' => array(
+						'DISTINCT(PostsTag.tag_id) as tag_id'
+					)
+				)
+			);
+
+			return Set::extract('/PostsTag/tag_id', $tags);
+		}
 	}
 ?>
