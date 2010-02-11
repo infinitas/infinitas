d6758081cb4e15a3afacc783e2f880de53c1ac9a
dogmatic <dogmatic69@gmail.com>
2010-02-11 17:57:24 +0200

    function for escaping regex terms in boot strap and some browsers and operating system config files

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -18,4 +18,36 @@
 	if(!defined('JSON_ERROR_DEPTH')){define('JSON_ERROR_DEPTH', 1);}
 	if(!defined('JSON_ERROR_CTRL_CHAR')){define('JSON_ERROR_CTRL_CHAR', 3);}
 	if(!defined('JSON_ERROR_SYNTAX')){define('JSON_ERROR_SYNTAX', 4);}
+
+	/**
+	 * Escape things for preg_match
+	 *
+	 * will escape a string for goind preg match.
+	 * http://www.php.net/manual/en/function.preg-replace.php#92456
+	 *
+	 * - Escapes the following
+	 *   - \ ^ . $ | ( ) [ ] * + ? { } ,
+	 *
+	 * - Example
+	 *   - regexEscape('http://www.example.com/s?q=php.net+docs')
+	 *   - http:\/\/www\.example\.com\/s\?q=php\.net\+docs
+	 *
+	 * @author alammar at gmail dot com
+	 *
+	 * @param string $str the stuff you want escaped
+	 * @return string the escaped string
+	 */
+	function regexEscape($str)
+	{
+		$patterns = array(
+			'/\//', '/\^/', '/\./', '/\$/', '/\|/',
+			'/\(/', '/\)/', '/\[/', '/\]/', '/\*/',
+			'/\+/', '/\?/', '/\{/', '/\}/', '/\,/'
+		);
+
+		$replace = array('\/', '\^', '\.', '\$', '\|', '\(', '\)',
+		'\[', '\]', '\*', '\+', '\?', '\{', '\}', '\,');
+
+		return preg_replace($patterns,$replace, $str);
+	}
 ?>
