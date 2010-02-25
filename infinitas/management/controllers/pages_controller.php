6fa9e1287e6bb7513b7e03fafe0475d0e20c162a
dogmatic <dogmatic69@gmail.com>
2010-02-25 18:25:44 +0200

    overloading default mass actions so files are deleted and not records

--- infinitas/management/controllers/pages_controller.php
+++ infinitas/management/controllers/pages_controller.php
@@ -71,4 +71,79 @@
 				$this->data = $this->Page->read(null, $filename);
 			}
 		}
+
+		function admin_mass() {
+			$ids = $this->__massGetIds($this->data['Page']);
+
+			switch ($this->__massGetAction($this->params['form'])) {
+				case 'delete':
+					$this->__massActionDelete($ids);
+					break;
+
+				case 'toggle':
+					$this->__massActionToggle($ids);
+					break;
+
+				case 'copy':
+					$this->__massActionCopy($ids);
+					break;
+
+				case 'filter':
+					$data = array();
+					foreach( $this->data['Page'] as $k => $field ){
+						if ( is_int( $k ) || $k == 'all' ){
+							continue;
+						}
+						$data[$this->modelClass.'.'.$k] = $field;
+					}
+					$this->redirect(array(
+							'plugin' => $this->params['plugin'],
+							'controller' => $this->params['controller'],
+							'action' => 'index'
+						) + $this->params['named'] + $data
+					);
+					break;
+
+				default:
+					$this->Session->setFlash(__('That is an invalid option', true));
+					$this->redirect($this->referer());
+					break;
+			} // switch
+		}
+
+		function __massGetIds($data) {
+			if (in_array($this->__massGetAction($this->params['form']), array('add','filter'))) {
+				return null;
+			}
+
+			$ids = array();
+			foreach($data as $id => $selected) {
+				if ($selected) {
+					$ids[] = $selected['id'];
+				}
+			}
+
+			if (empty($ids)) {
+				$this->Session->setFlash(__('Nothing was selected, please select something and try again.', true));
+				$this->redirect($this->referer());
+			}
+
+			return $ids;
+		}
+
+		function __massActionDelete($ids) {
+			$deleted = true;
+			foreach($ids as $id){
+				$deleted = $deleted && $this->Page->delete($id);
+			}
+
+			if ($delete) {
+				$this->Session->setFlash(__('All pages were deleted', true));
+			}
+			else{
+				$this->Session->setFlash(__('Some pages could not be deleted', true));
+			}
+
+			$this->redirect($this->referer());
+		}
 	}
