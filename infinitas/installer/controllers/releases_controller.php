fcd126223f4e7773421646321342e055bbfed630
dogmatic <dogmatic69@gmail.com>
2010-02-26 15:22:22 +0200

    creating a reset and update method for installed systems

--- infinitas/installer/controllers/releases_controller.php
+++ infinitas/installer/controllers/releases_controller.php
@@ -13,8 +13,33 @@
 		}
 
 		function admin_index(){
-			$this->Release->getCoreData();
-			$this->Release->getSampleData();
+			if ($this->Release->getCoreData() && $this->Release->getSampleData()) {
+				$this->Session->setFlash(__('Data has been updated', true));
+			}
+			else{
+				$this->Session->setFlash(__('Something went wrong', true));
+			}
+			$this->redirect('/admin');
+		}
+
+		function admin_update_core(){
+			if ($this->Release->writeCoreData()) {
+				$this->Session->setFlash(__('Core data updated', true));
+			}
+			else{
+				$this->Session->setFlash(__('Something went wrong', true));
+			}
+			$this->redirect('/admin');
+		}
+
+		function admin_update_sample(){
+			if ($this->Release->writeSampleData()) {
+				$this->Session->setFlash(__('Sample data updated', true));
+			}
+			else{
+				$this->Session->setFlash(__('Something went wrong', true));
+			}
+			$this->redirect('/admin');
 		}
 
 		function admin_core_data(){
