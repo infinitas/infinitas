fd0b11c41b9ab61eb6f50695bba3b75612355d56
dogmatic <dogmatic69@gmail.com>
2010-03-01 22:57:23 +0200

    generic xml datasource

---
+++ extensions/libs/models/datasources/xml_source.php
@@ -0,0 +1,88 @@
+<?php
+	class XmlSource extends DataSource {
+		var $request = array(
+			'method' => 'GET',
+			'uri' => array(
+				'scheme' => 'http',
+				'host' => null,
+				'port' => 80,
+				'user' => null,
+				'pass' => null,
+				'path' => null,
+				'query' => null,
+				'fragment' => null
+			),
+			'auth' => array(
+				'method' => 'Basic',
+				'user' => null,
+				'pass' => null
+			),
+			'version' => '1.1',
+			'body' => '',
+			'line' => null,
+			'header' => array(
+				'Connection' => 'close',
+				'User-Agent' => 'Infinitas'
+			),
+			'raw' => null,
+			'cookies' => array()
+		);
+
+		function __construct($config) {
+			parent::__construct($config);
+			//$this->Http =& new HttpSocket();
+		}
+
+		function describe(&$model){
+			return $model->schema['tweets'];
+		}
+
+		function listSources(){
+			return 'listSources';
+		}
+
+		function read(&$model, $map = array(), $recursive = null){
+			$this->request = array_merge($this->request, $model->request);
+			return $this->__process(
+				$this->__getData($this->request)
+			);
+		}
+
+		function calculate(&$model, $func = 'count', $params = array()) {
+			return $model->map['count'];
+		}
+
+		function _sort($map = array(), $data = array()){
+			if (empty($data)) {
+				return array();
+			}
+
+			if (empty($map)) {
+				return $data;
+			}
+
+			return Set::extract($map, $data);
+		}
+
+		function __getData($request){
+			App::import('HttpSocket');
+			$this->HttpSocket = new HttpSocket();
+			$data = $this->HttpSocket->request($request);
+
+			unset($this->HttpSocket);
+
+			return $data;
+		}
+
+		function __process($response) {
+			App::import('Xml');
+			$this->Xml = new Xml($response);
+
+			$data = $this->Xml->toArray();
+			$this->Xml->__destruct();
+			unset($this->Xml);
+
+			return $data;
+		}
+	}
+?>
