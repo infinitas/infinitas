43fe7c18f06cec1f0c94db1feb1a0639f1e41e25
dogmatic <dogmatic69@gmail.com>
2010-03-03 23:02:16 +0200

    adding comments

--- extensions/libs/models/datasources/xml_source.php
+++ extensions/libs/models/datasources/xml_source.php
@@ -1,5 +1,26 @@
 <?php
+	/**
+	 * XmlSource
+	 *
+	 * @package
+	 * @author dogmatic
+	 * @copyright Copyright (c) 2010
+	 * @version $Id$
+	 * @access public
+	 */
+	/**
+	 * XmlSource
+	 *
+	 * @package
+	 * @author dogmatic
+	 * @copyright Copyright (c) 2010
+	 * @version $Id$
+	 * @access public
+	 */
 	class XmlSource extends DataSource {
+		/**
+		 * A default request array
+		 */
 		var $request = array(
 			'method' => 'GET',
 			'uri' => array(
@@ -30,29 +51,60 @@
 
 		var $driver = null;
 
+
+		/**
+		 * __construct()
+		 *
+		 * @param mixed $config
+		 */
 		function __construct($config) {
 			parent::__construct($config);
-			//$this->Http =& new HttpSocket();
 		}
 
+		/**
+		 * describe the data
+		 *
+		 * @param mixed $model
+		 * @return array the shcema of the model
+		 */
 		function describe(&$model){
 			return $model->schema;
 		}
 
+		/**
+		 * listSources
+		 *
+		 * list the sources???
+		 *
+		 * @return array sources
+		 */
 		function listSources(){
-			return 'listSources';
+			return array('listSources');
 		}
 
+		/**
+		 * read data
+		 *
+		 * this is the main method that reads data from the datasource and
+		 * formats it according to the request from the model.
+		 *
+		 * @param mixed $model the model that is requesting data
+		 * @param mixed $query the qurey that was sent
+		 *
+		 * @return the data requested by the model
+		 */
 		function read(&$model, $query){
 			$this->request = array_merge($this->request, $model->request);
 			$response = $this->__process(
 				$this->__getData($this->request)
 			);
+
 			if ($query['fields'] == 'count') {
 				$count = Set::extract($model->map['count'], $response);
 				$result[0][$model->alias]['count'] = $count;
 				return $result;
 			}
+
 			$results = Set::extract($model->map['data'], $response);
 			foreach ($results as $key => $value) {
 				$result[$key][$model->alias] = $value;
@@ -60,27 +112,16 @@
 			return $result;
 		}
 
-		function calculate(&$model, $func, $params = array()) {
-			$params = (array)$params;
-			switch (strtolower($func)) {
-				case 'count':
-					return 'count';
-				break;
-			}
-		}
-
-		function _sort($map = array(), $data = array()){
-			if (empty($data)) {
-				return array();
-			}
-
-			if (empty($map)) {
-				return $data;
-			}
-
-			return Set::extract($map, $data);
-		}
-
+		/**
+		 * Get xml data from a site.
+		 *
+		 * Uses cake socket to get the data based on the connection specified
+		 * in the model
+		 *
+		 * @param array $request the request details
+		 *
+		 * @return string the xml that is returned from the site.
+		 */
 		function __getData($request){
 			App::import('HttpSocket');
 			$this->HttpSocket = new HttpSocket();
@@ -91,7 +132,20 @@
 			return $data;
 		}
 
-		function __process($response) {
+		/**
+		 * Process xml.
+		 *
+		 * Takes xml and convers it to an array using cakes xml class
+		 *
+		 * @param string $response the xml that will be converted
+		 *
+		 * @return array the data from xml or empty array if there is nothing passed
+		 */
+		function __process($response = null) {
+			if (empty($response)) {
+				return array();
+			}
+
 			App::import('Xml');
 			$this->Xml = new Xml($response);
 
