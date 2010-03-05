bc8eb4e4975666312de19694ccb87a83317b6ec9
dogmatic <dogmatic69@gmail.com>
2010-03-05 13:29:22 +0200

    moved to infinitas behavior, added comments and code format

--- infinitas/management/controllers/trash_controller.php
+++ infinitas/management/controllers/trash_controller.php
@@ -1,25 +1,30 @@
 <?php
 class TrashController extends ManagementAppController {
 	var $name = 'Trash';
-	
+
 	var $uses = array();
 
 	function beforeFilter(){
+		parent::beforeFilter();
 		if(isset($this->params['form']['action']) && $this->params['form']['action'] == 'cancel'){
 			unset($this->params['form']['action']);
 			$this->redirect(array_merge(array('action' => 'list_items'), $this->params['named']));
-		}		
-		
-		parent::beforeFilter();
-		$this->db = ConnectionManager::getDataSource('default');
+		}
+
+		//$this->db = ConnectionManager::getDataSource('default');
 	}
 
+	/**
+	* List all table with deleted in the schema
+	*/
 	function admin_index(){
-		$trashableTables = $this->_getTrashableTables();
+		App::import('AppModel');
+		$this->AppModel = new AppModel(array('table' => false));
+		$trashableTables = $this->AppModel->getTablesByField('default', 'deleted');
 
 		$trashed = array();
 		foreach($trashableTables as $table ){
-			$count = $this->db->query(
+			$count = $this->AppModel->query(
 				'SELECT COUNT(*) AS `count` FROM `'.$table['table'].'` AS `'.$table['model'].'`   WHERE `'.$table['model'].'`.`deleted` = 1'
 			);
 
@@ -34,82 +39,55 @@ class TrashController extends ManagementAppController {
 		$this->set(compact('trashed'));
 	}
 
+	/**
+	* List all records from selected table.
+	*/
 	function admin_list_items(){
 		if(isset($this->params['named']['pluginName']) && isset($this->params['named']['modelName'])){
 			extract($this->params['named']);
-			
-			if($pluginName == 'Core') {
-				$pluginName = 'Management';
-			}
-			
+
 			$this->loadModel($pluginName . '.' . $modelName);
-			
+
 			$fieldList = array(
 						$modelName . '.id',
 						$modelName . '.deleted_date'
 			);
-			
+
 			if($this->{$modelName}->hasField('title')) {
 				$fieldList[] = $modelName . '.title';
 			}
+
 			elseif($this->{$modelName}->hasField('name')) {
 				$fieldList[] = $modelName . '.name';
 			}
-			
-			
+
+
 			$this->paginate = array(
 				$modelName => array(
 					'contain' => false,
 					'fields' => $fieldList
 				)
 			);
-			
+
 			$trashedItems = $this->paginate($modelName, array($modelName . '.deleted' => 1));
-			
+
 			$this->set(compact('trashedItems', 'pluginName', 'modelName'));
 		}
-		else
-		{
-			$this->redirect(array('index'));
+		else{
+			$this->redirect(array('action' => 'index'));
 		}
 	}
 
-	function _getTables(){
-		$tables = $this->db->query('SHOW TABLES;');
-		return Set::extract('/TABLE_NAMES/Tables_in_infinitas', $tables);
-	}
-
-	function _getTrashableTables(){
-		$tableNames = $this->_getTables();
-		$lockableTables = array();
-
-		foreach($tableNames as $table ){
-			$fields = $this->db->query('DESCRIBE '.$table);
-			$fields = Set::extract('/COLUMNS/Field', $fields);
-
-			if (in_array('deleted', $fields)) {
-				$_table = explode('_', $table, 2);
-				$lockableTables[] = array(
-					'plugin' => ucfirst($_table[0]),
-					'model'  => Inflector::classify($_table[1]),
-					'table'  => $table
-				);
-			}
-		}
-
-		return $lockableTables;
-	}
-	
 	function __massActionRestore($ids) {
 		if(isset($this->params['named']['pluginName']) && isset($this->params['named']['modelName'])){
 			extract($this->params['named']);
-			
+
 			$this->loadModel($pluginName . '.' . $modelName);
 
 			foreach($ids as $id) {
 				$this->{$modelName}->undelete($id);
 			}
-			
+
 			$prettyModelName = low(Inflector::humanize(Inflector::underscore(Inflector::pluralize($modelName))));
 			$this->Session->setFlash(__('The ' . $prettyModelName . ' have been restored', true));
 			$this->redirect($this->referer());
@@ -119,15 +97,15 @@ class TrashController extends ManagementAppController {
 			$this->redirect($this->referer());
 		}
 	}
-		
+
 	function __massActionDelete($ids) {
 		if(isset($this->params['named']['pluginName']) && isset($this->params['named']['modelName'])){
 			extract($this->params['named']);
-			
+
 			$this->loadModel($pluginName . '.' . $modelName);
 
 			$this->{$modelName}->enableSoftDeletable('find', false);
-			
+
 			$this->MassAction->delete($ids);
 		}
 		else
@@ -135,16 +113,16 @@ class TrashController extends ManagementAppController {
 			$this->redirect($this->referer());
 		}
 	}
-	
+
 	function __handleDeletes($ids)
 	{
 		extract($this->params['named']);
 		$this->{$modelName}->enableSoftDeletable('delete', false);
-		
+
 		$conditions = array($modelName . '.' . $this->$modelName->primaryKey => $ids);
-		
+
 		$message = __('deleted', true);
-		
+
 		if($this->{$modelName}->deleteAll($conditions)) {
 			$this->Session->setFlash(__('The ' . Inflector::pluralize(low($modelName)) . ' have been', true) . ' ' . $message);
 		}
@@ -152,5 +130,5 @@ class TrashController extends ManagementAppController {
 			$this->Session->setFlash(__('The ' . Inflector::pluralize(low($modelName)) . ' could not be', true) . ' ' . $message);
 		}
 		$this->redirect($this->data['Confirm']['referer']);
-	}	
+	}
 }
