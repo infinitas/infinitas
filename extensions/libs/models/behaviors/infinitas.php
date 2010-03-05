8ebfd1a3631ce53b81dc55b502dde322d0a177f3
dogmatic <dogmatic69@gmail.com>
2010-03-05 13:31:54 +0200

    moving code to infinitas behavior

--- extensions/libs/models/behaviors/infinitas.php
+++ extensions/libs/models/behaviors/infinitas.php
@@ -2,6 +2,30 @@
 	class InfinitasBehavior extends ModelBehavior {
 		var $_defaults = array();
 
+		var $blockedPlugins = array(
+			'DebugKit',
+			'Filter',
+			'Libs'
+		);
+
+		/**
+		 * JSON error messages.
+		 *
+		 * Set up some errors for json.
+		 * @access public
+		 */
+		var $_json_messages = array(
+		    JSON_ERROR_NONE      => 'No error',
+		    JSON_ERROR_DEPTH     => 'The maximum stack depth has been exceeded',
+		    JSON_ERROR_CTRL_CHAR => 'Control character error, possibly incorrectly encoded',
+		    JSON_ERROR_SYNTAX    => 'Syntax error',
+		);
+
+		/**
+		 * error messages from checking json
+		 */
+		var $__jsonErrors = array();
+
 		function setup(&$Model, $config = null) {
 			if (is_array($config)) {
 				$this->settings[$Model->alias] = array_merge($this->_defaults, $config);
@@ -68,6 +92,62 @@
 				}
 			}
 			return $return;
+		}
+
+		/**
+		 * convert json data.
+		 *
+		 * takes a string and returns some data. can pass return false for validation.
+		 *
+		 * @params string $data a string of json data.
+		 * @params array $config the params to pass to json_decode (assoc && depth)
+		 * @params bool $return will return the array/object by default but can be set to false to just check its valid.
+		 */
+		function getJson($data = null, $config = array(), $return = true){
+			if (!$data) {
+				$this->_errors[] = 'No data for json';
+				return false;
+			}
+
+			$defaultConfig = array('assoc' => true);
+			$config = array_merge($defaultConfig, (array)$config);
+			$json = json_decode($data, $config['assoc']);
+
+			if (!$json) {
+				$this->__jsonErrors[] = $this->_json_messages[json_last_error()];
+				return false;
+			}
+
+			if ($return) {
+				return $json;
+			}
+
+			unset($json);
+			return true;
+		}
+
+		/**
+		 * Get a list of plugins.
+		 *
+		 * Just gets a list of plugins and returns them after rempving the plugins
+		 * that should not be displayed to the user.
+		 *
+		 * @return array an array of all the plugins in infinitas.
+		 */
+		function getPlugins($skipBlocked = true){
+			$plugins = Configure::listObjects('plugin');
+
+			if ($skipBlocked === false) {
+				return $plugins;
+			}
+
+			foreach($plugins as $plugin){
+				if (!in_array($plugin, $this->blockedPlugins)){
+					$return[Inflector::underscore($plugin)] = $plugin;
+				}
+			}
+
+			return array('' => 'None') + (array)$return;
 		}
 	}
 ?>
