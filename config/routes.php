7f167e819390463c06f9559489b587a81c70ee5d
dogmatic <dogmatic69@gmail.com>
2010-03-19 16:17:28 +0200

    adding a method to see the routes that are created for debugging

--- config/routes.php
+++ config/routes.php
@@ -37,8 +37,37 @@
 
 		if (!empty($routes)) {
 			foreach($routes as $route ){
+				if (false) {
+					debugRoute($route);
+					continue;
+				}
 				Router::connect($route['Route']['url'], $route['Route']['values'], $route['Route']['regex'] );
 			}
 		}
+	}
+
+
+	/**
+	 * having issues with routes ?
+	 *
+	 * @todo this can be removed after stable.
+	 *
+	 * @param mixed $route
+	 * @return null echo's out the route.
+	 */
+	function debugRoute($route){
+		echo 'Router::connect(\''.$route['Route']['url'].'\', array(';
+		$parts = array();
+		foreach($route['Route']['values'] as $k => $v){
+			$parts[] = "'$k' => '$v'";
+		}
+		echo implode(', ', $parts);
+		echo '), array(';
+		$parts = array();
+		foreach($route['Route']['regex'] as $k => $v){
+			$parts[] = "'$k' => '$v'";
+		}
+		echo implode(', ', $parts);
+		echo ')); </br>';
 	}
 ?>
