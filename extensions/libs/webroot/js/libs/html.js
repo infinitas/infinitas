1c7815354dd83526cf16d2be9d47dc4db8e6df7f
dogmatic <dogmatic69@gmail.com>
2010-03-24 01:20:09 +0200

    adding a loading image for ajax calls, will add the image after the target and remove it on success... still need to find a way to remove it if there is a problem

--- extensions/libs/webroot/js/libs/html.js
+++ extensions/libs/webroot/js/libs/html.js
@@ -42,17 +42,36 @@
 	}
 
 	/**
+	 * Loading image for ajax.
+	 *
+	 * This can create and detroy a loading image depending on what is passed.
+	 *
+	 * @param string target the target where the loading icon will be displayed or removed from.
+	 * @param bool status if it should create or destroy the icon.
+	 */
+	HtmlHelper.loading = function(target, create){
+		if (!create) {
+			$('#' + target).next().remove();
+		}
+		else{
+			$('#' + target).after('<img class="loadingImage" src="' + Infinitas.base + 'img/core/icons/notifications/loading.gif" alt="loading" width="16px"/>');
+		}
+	}
+
+	/**
 	 * get data from a controller.
 	 *
 	 * Requests data from the server and when the data is not
 	 */
 	HtmlHelper.requestAction = function(metaData, callback) {
+		HtmlHelper.loading(metaData.target, true);
 		var getUrl = HtmlHelper.url(metaData);
 		$.getJSON(
 			getUrl + metaData.param + '.json',
 			function(returnData) {
 				if(returnData !== null) {
 					callback(returnData['json'], metaData);
+					HtmlHelper.loading(metaData.target, false);
 				}
 			}
 		);
