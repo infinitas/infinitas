d91ebc5efcbc61aeb94ad899396959a2d5a1c68d
dogmatic <dogmatic69@gmail.com>
2010-03-25 23:43:21 +0200

    code format

--- infinitas/cms/models/feature.php
+++ infinitas/cms/models/feature.php
@@ -1,36 +1,35 @@
 <?php
-/**
-* Comment Template.
-*
-* @todo Implement .this needs to be sorted out.
-*
-* Copyright (c) 2009 Carl Sutton ( dogmatic69 )
-*
-* Licensed under The MIT License
-* Redistributions of files must retain the above copyright notice.
-* @filesource
-* @copyright Copyright (c) 2009 Carl Sutton ( dogmatic69 )
-* @link http://infinitas-cms.org
-* @package sort
-* @subpackage sort.comments
-* @license http://www.opensource.org/licenses/mit-license.php The MIT License
-* @since 0.5a
-*/
+	/**
+	 * Comment Template.
+	 *
+	 * @todo Implement .this needs to be sorted out.
+	 *
+	 * Copyright (c) 2009 Carl Sutton ( dogmatic69 )
+	 *
+	 * Licensed under The MIT License
+	 * Redistributions of files must retain the above copyright notice.
+	 * @filesource
+	 * @copyright Copyright (c) 2009 Carl Sutton ( dogmatic69 )
+	 * @link http://infinitas-cms.org
+	 * @package sort
+	 * @subpackage sort.comments
+	 * @license http://www.opensource.org/licenses/mit-license.php The MIT License
+	 * @since 0.5a
+	 */
 
-class Feature extends CmsAppModel {
-	var $name = 'Feature';
+	class Feature extends CmsAppModel {
+		var $name = 'Feature';
 
-	var $order = array(
-		'Feature.ordering' => 'DESC'
+		var $order = array(
+			'Feature.ordering' => 'DESC'
+			);
+
+		var $actsAs = array(
+			'Libs.Sequence'
 		);
 
-	var $actsAs = array(
-		'Libs.Sequence'
-	);
-
-	var $belongsTo = array(
-		'Cms.Content'
-	);
-}
-
+		var $belongsTo = array(
+			'Cms.Content'
+		);
+	}
 ?>
