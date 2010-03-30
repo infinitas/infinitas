9f05c9b68bee5d062ce31be695430895306ce233
dogmatic <dogmatic69@gmail.com>
2010-03-30 23:22:46 +0200

    updating analogue helper

--- extensions/libs/views/helpers/analogue.php
+++ extensions/libs/views/helpers/analogue.php
@@ -1,19 +1,39 @@
 <?php
 	/**
 	 * AnalogueHelper
-	 * Maps helpers to other helper names
+	 * Renames, or "maps", helpers prior to rendering
 	 * @author Joe Beeson <jbeeson@gmail.com>
 	 *
-	 * var $helpers = array('Analogue' => array('html' => 'mine'));
+	 * Example
+	 *
+	 * public $helpers = array(
+	 *   'Analogue' => array(
+	 *     array(
+	 *       'helper' => 'MyHtml',
+	 *       'rename' => 'Html'
+	 *     ),
+	 *     array(
+	 *       'helper' => 'MyForm',
+	 *       'rename' => 'Form'
+	 *     )
+	 *   )
+	 * );
 	 */
 	class AnalogueHelper extends AppHelper {
 
 		/**
-		 * Settings
+		 * Mappings
 		 * @var array
 		 * @access private
 		 */
-		private $settings = array();
+		private $mappings = array();
+
+		/**
+		 * View
+		 * @var View
+		 * @access private
+		 */
+		private $view;
 
 		/**
 		 * Executed prior to rendering
@@ -21,13 +41,10 @@
 		 * @access public
 		 */
 		public function beforeRender() {
-			$view = ClassRegistry::getObject('view');
 
-			// Map the helper against our link
-			foreach ($this->settings as $link=>$helper) {
-				$link 	= strtolower($link);
-				$helper = strtolower($helper);
-				$view->loaded[$link] = $view->loaded[$helper];
+			// Loop through and call _mapHelper()
+			foreach ($this->mappings as $mapping) {
+				$this->_mapHelper($mapping);
 			}
 		}
 
@@ -37,11 +54,38 @@
 		 * @return null
 		 * @access public
 		 */
-		public function __construct($settings = array()) {
-			$this->settings = am(
-				$this->settings,
-				$settings
+		public function __construct($mappings = array()) {
+
+			// Grab our View object for use later...
+			$this->view = ClassRegistry::getObject('view');
+
+			// Merge our mappings together...
+			$this->mappings = am(
+				$this->mappings,
+				$mappings
 			);
+		}
+
+		/**
+		 * Performs the mapping of a helper object to a new name
+		 * @param array $mapping
+		 * @return null
+		 * @access private
+		 */
+		private function _mapHelper($mapping = array()) {
+
+			// Helpers are always lowercased in the View object
+			$mapping = array_map('strtolower', $mapping);
+
+			// Extract our array for use
+			extract($mapping);
+
+			// Only continue if we have a valid, loaded helper
+			if (isset($helper)) {
+				if (isset($this->view->loaded[$helper]) and isset($rename)) {
+					$this->view->loaded[$rename] = $this->view->loaded[$helper];
+				}
+			}
 		}
 
 	}
