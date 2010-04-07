35e538e0755d531466795c055b7c1e619d2183fc
dogmatic <dogmatic69@gmail.com>
2010-04-07 18:43:32 +0200

    updates to the database, schema also updated and included

--- config/migrations/map.php
+++ config/migrations/map.php
@@ -4,5 +4,7 @@ $map = array(
 		'063' => 'M4b8bee220d3c41f18f25117a6318cd70'),
 	2 => array(
 		'0631' => 'M4b8beff7199c482fb0e011f16318cd70'),
+	3 => array(
+		'064' => 'M4bbcb56f88dc4bbb88b60ba06318cd70'),
 );
 ?>
