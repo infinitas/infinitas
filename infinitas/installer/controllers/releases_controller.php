daa06d9418fedd0444659ef53cc3530ec0903942
dogmatic <dogmatic69@gmail.com>
2010-04-07 19:07:13 +0200

    updating the updater to first run db updates before updating data in the db

--- infinitas/installer/controllers/releases_controller.php
+++ infinitas/installer/controllers/releases_controller.php
@@ -23,6 +23,8 @@
 		}
 
 		function admin_update_core(){
+			$this->_uppdateDatabase();
+			
 			if ($this->Release->writeCoreData()) {
 				$this->Session->setFlash(__('Core data updated', true));
 			}
@@ -33,6 +35,8 @@
 		}
 
 		function admin_update_sample(){
+			$this->_uppdateDatabase();
+			
 			if ($this->Release->writeSampleData()) {
 				$this->Session->setFlash(__('Sample data updated', true));
 			}
@@ -40,6 +44,26 @@
 				$this->Session->setFlash(__('Something went wrong', true));
 			}
 			$this->redirect('/admin');
+		}
+		
+		function _uppdateDatabase(){
+			$type = 'app';
+			
+			if(!isset($this->MigrationVersion)){			
+				App::import('Lib', 'Migrations.MigrationVersion');
+				$this->MigrationVersion = new MigrationVersion();	
+			}
+			
+			// Get the mapping and the latest version avaiable
+			$mapping = $this->MigrationVersion->getMapping($type);
+			$latest = array_pop($mapping);
+			
+			try{
+				$this->MigrationVersion->run(array('type' => $type, 'version' => $latest['version']));
+			}						
+			catch(Exception $e){
+				$this->Session->setFlash(__('Seems you are up to date with the latest db changes', true));
+			}
 		}
 
 		function admin_core_data(){
