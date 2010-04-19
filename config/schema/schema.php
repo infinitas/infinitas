cc5ce68606d6bd717b6ff4b2477aa87f33bba7cd
dogmatic <dogmatic69@gmail.com>
2010-04-20 00:42:18 +0200

    adding tables for the cart and updating migration/schema

--- config/schema/schema.php
+++ config/schema/schema.php
@@ -1,6 +1,6 @@
-<?php
+<?php 
 /* SVN FILE: $Id$ */
-/* Infinitas schema generated on: 2010-04-19 13:04:38 : 1271672378*/
+/* Infinitas schema generated on: 2010-04-20 01:04:25 : 1271716885*/
 class InfinitasSchema extends CakeSchema {
 	var $name = 'Infinitas';
 
@@ -689,6 +689,166 @@ class InfinitasSchema extends CakeSchema {
 		'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 40, 'key' => 'primary'),
 		'data' => array('type' => 'text', 'null' => true, 'default' => NULL),
 		'expires' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+	var $shop_branches_categories = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'branch_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'category_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+	var $shop_branches_products = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'branch_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'product_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+	var $shop_branches_specials = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'branch_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'special_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'MyISAM')
+	);
+	var $shop_branches_spotlights = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'branch_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'spotlight_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'MyISAM')
+	);
+	var $shop_categories = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 45),
+		'slug' => array('type' => 'string', 'null' => false, 'default' => NULL),
+		'description' => array('type' => 'string', 'null' => true, 'default' => NULL),
+		'keywords' => array('type' => 'string', 'null' => true, 'default' => NULL),
+		'image_id' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'product_count' => array('type' => 'integer', 'null' => true, 'default' => '0'),
+		'active' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
+		'lft' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'rght' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'parent_id' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'deleted' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
+		'deleted_date' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+	var $shop_categories_products = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'category_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'product_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+	var $shop_images = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => NULL),
+		'ext' => array('type' => 'string', 'null' => false, 'default' => 'jpg', 'length' => 4),
+		'width' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'height' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'deleted' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
+		'deleted_date' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+	var $shop_images_products = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'image_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'product_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'MyISAM')
+	);
+	var $shop_products = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 45),
+		'slug' => array('type' => 'string', 'null' => false, 'default' => NULL),
+		'description' => array('type' => 'text', 'null' => false, 'default' => NULL),
+		'unit_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'cost' => array('type' => 'float', 'null' => false, 'default' => '0'),
+		'retail' => array('type' => 'float', 'null' => false, 'default' => '0'),
+		'price' => array('type' => 'float', 'null' => false, 'default' => NULL),
+		'active' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
+		'image_id' => array('type' => 'string', 'null' => true, 'default' => NULL),
+		'rating' => array('type' => 'float', 'null' => true, 'default' => '0'),
+		'rating_count' => array('type' => 'integer', 'null' => false, 'default' => '0'),
+		'supplier_id' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'deleted' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
+		'deleted_date' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+	var $shop_shop_branches = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'branch_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'manager_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'ordering' => array('type' => 'integer', 'null' => false, 'default' => '1'),
+		'active' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
+		'deleted' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
+		'deleted_date' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+	var $shop_specials = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'product_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'image_id' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'discount' => array('type' => 'float', 'null' => true, 'default' => NULL),
+		'amount' => array('type' => 'float', 'null' => true, 'default' => NULL),
+		'start' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+		'end' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+		'deleted' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
+		'deleted_date' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+	var $shop_spotlights = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'product_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'image_id' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'start' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+		'end' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+		'deleted' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
+		'deleted_date' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+	var $shop_stock = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'branch_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'product_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'stock' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+	var $shop_suppliers = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => NULL),
+		'slug' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 100),
+		'address_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'email' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100),
+		'phone' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 15),
+		'fax' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 15),
+		'logo' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 150),
+		'deleted' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
+		'deleted_date' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
 		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
 		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
 	);
