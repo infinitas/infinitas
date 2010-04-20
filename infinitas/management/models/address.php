4dad2de44ff733e2b43f18ad0942efa0b0a15626
dogmatic <dogmatic69@gmail.com>
2010-04-20 23:35:27 +0200

    adding a virtual field to show the full address

--- infinitas/management/models/address.php
+++ infinitas/management/models/address.php
@@ -1,10 +1,14 @@
 <?php
 	/**
 	 *
-	 *
 	 */
 	class Address extends ManagementAppModel{
 		var $name = 'Address';
+
+		var $virtualFields = array(
+		    'address' => 'CONCAT(Address.street, ", ", Address.city, ", ", Address.province)'
+		);
+
 
 		var $belongsTo = array(
 			'Management.Country'
