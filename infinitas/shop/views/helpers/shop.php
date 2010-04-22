8ec4a3f5fec108eae4c09a597b1ae13bff6d7ab2
dogmatic <dogmatic69@gmail.com>
2010-04-22 23:06:36 +0200

    adding a method for currency conversion

--- infinitas/shop/views/helpers/shop.php
+++ infinitas/shop/views/helpers/shop.php
@@ -1,4 +1,15 @@
 <?php
 	class ShopHelper extends AppHelper{
+		var $helpers = array(
+			'Number', 'Form',
+			'Libs.Wysiwyg',
+		);
 
+		function currency($amount = 0, $currency = null){
+			if(!$currency){
+				$currency = Configure::read('Currency.unit');
+			}
+
+			return $this->Number->currency($amount, $currency);
+		}
 	}
