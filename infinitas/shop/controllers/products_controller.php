db87b67a421e171c4998e14588db28c9db8a4ac6
dogmatic <dogmatic69@gmail.com>
2010-04-23 15:40:44 +0200

    adding product manager to the shop

---
+++ infinitas/shop/controllers/products_controller.php
@@ -0,0 +1,127 @@
+<?php
+	class ProductsController extends ShopAppController{
+		var $name = 'Products';
+
+		var $helpers = array(
+			'Filter.Filter'
+		);
+
+		function admin_index(){
+			$this->paginate = array(
+				'fields' => array(
+					'Product.id',
+					'Product.name',
+					'Product.slug',
+					'Product.active',
+					'Product.views',
+					'Product.rating',
+					'Product.rating_count',
+					'Product.cost',
+					'Product.retail',
+					'Product.price',
+					'Product.modified',
+					'Product.supplier_id',
+					'Product.unit_id',
+					'Product.image_id',
+				),
+				'contain' => array(
+					'Image' => array(
+						'fields' => array(
+							'Image.id',
+							'Image.image'
+						)
+					),
+					'Unit' => array(
+						'fields' => array(
+							'Unit.id',
+							'Unit.name',
+							'Unit.slug'
+						)
+					),
+					'Supplier' => array(
+						'fields' => array(
+							'Supplier.id',
+							'Supplier.name',
+							'Supplier.slug'
+						)
+					),
+					'ProductCategory' => array(
+						'fields' => array(
+							'ProductCategory.id',
+							'ProductCategory.name',
+							'ProductCategory.slug'
+						)
+					),
+					'ShopBranch' => array(
+						'fields' => array(
+							'ShopBranch.id',
+							'ShopBranch.branch_id',
+						),
+						'BranchDetail' => array(
+							'fields' => array(
+								'BranchDetail.id',
+								'BranchDetail.name',
+							)
+						)
+					)
+				)
+			);
+
+			$products = $this->paginate(
+				null,
+				$this->Filter->filter
+			);
+
+			$filterOptions = $this->Filter->filterOptions;
+			$filterOptions['fields'] = array(
+				'name',
+				'category_id' => $this->Product->ProductCategory->generatetreelist(null, null, null, '_'),
+				'supplier_id' => $this->Product->Supplier->find('list'),
+				'unit_id' => $this->Product->Unit->find('list'),
+				'active' => (array)Configure::read('CORE.active_options')
+			);
+			$this->set(compact('products','filterOptions'));
+		}
+
+		function admin_add(){
+			if (!empty($this->data)) {
+				$this->Product->create();
+				if ($this->Product->saveAll($this->data)) {
+					$this->Session->setFlash('Your product has been saved.');
+					$this->redirect(array('action' => 'index'));
+				}
+			}
+
+			$productCategories = $this->Product->ProductCategory->generatetreelist(null, null, null, '_');
+			$units = $this->Product->Unit->find('list');
+			$suppliers = $this->Product->Supplier->find('list');
+			$shopBranches = $this->Product->ShopBranch->getList();
+			$images = $this->Product->Image->find('list');
+			$this->set(compact('productCategories', 'units', 'suppliers', 'shopBranches', 'images'));
+		}
+
+		function admin_edit($id = null){
+			if (!$id) {
+				$this->Session->setFlash(__('That product could not be found', true), true);
+				$this->redirect($this->referer());
+			}
+
+			if (!empty($this->data)) {
+				if ($this->Product->saveAll($this->data)) {
+					$this->Session->setFlash('Your product has been saved.');
+					$this->redirect(array('action' => 'index'));
+				}
+			}
+
+			if ($id && empty($this->data)) {
+				$this->data = $this->Product->read(null, $id);
+			}
+
+			$productCategories = $this->Product->ProductCategory->generatetreelist(null, null, null, '_');
+			$units = $this->Product->Unit->find('list');
+			$suppliers = $this->Product->Supplier->find('list');
+			$shopBranches = $this->Product->ShopBranch->getList();
+			$images = $this->Product->Image->find('list');
+			$this->set(compact('productCategories', 'units', 'suppliers', 'shopBranches', 'images'));
+		}
+	}
---
+++ infinitas/shop/views/products/admin_form.ctp
@@ -0,0 +1,62 @@
+<?php
+    /**
+     * Shop suppliers add/edit
+     *
+     * This page is used to add/edit suppliers for your products.
+     *
+     * Copyright (c) 2009 Carl Sutton ( dogmatic69 )
+     *
+     * Licensed under The MIT License
+     * Redistributions of files must retain the above copyright notice.
+     *
+     * @filesource
+     * @copyright     Copyright (c) 2009 Carl Sutton ( dogmatic69 )
+     * @link          http://infinitas-cms.org
+     * @package       shop
+     * @subpackage    shop.views.suppliers.form
+     * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
+     * @since 0.8a
+     */
+
+    echo $this->Form->create('Product', array('type' => 'file'));
+        echo $this->Infinitas->adminEditHead($this);
+        echo $this->Design->niceBox();
+        	?>
+				<div class="data">
+					<?php
+						echo $this->Form->input('id');
+						echo $this->Form->input('name');
+						echo $this->Shop->wysiwyg('Product.description');
+					?>
+				</div>
+				<div class="config">
+					<?php
+        				echo $this->Design->niceBox();
+        					?><h2><?php __('Config'); ?></h2><?php
+							echo $this->Form->input('supplier_id');
+							echo $this->Form->input('ShopBranch');
+							echo $this->Form->input('ProductCategory');
+							echo $this->Form->input('active');
+						echo $this->Design->niceBoxEnd();
+
+        				echo $this->Design->niceBox();
+        					?><h2><?php __('Pricing'); ?></h2><?php
+							echo $this->Form->input('cost', array('before' => Configure::read('Currency.unit'),'title' => __('Cost :: This is the price that you pay for the item', true)));
+							echo $this->Form->input('retail', array('before' => Configure::read('Currency.unit'), 'title' => __('Retail :: This is the going rate of the item in other stores', true)));
+							echo $this->Form->input('price', array('before' => Configure::read('Currency.unit'), 'title' => __('Price :: This is your selling price', true)));
+							echo $this->Form->input('unit_id', array('title' => __('Unit :: The unit that the product is sold in [eg: ounces]', true)));
+        				echo $this->Design->niceBoxEnd();
+
+        				echo $this->Design->niceBox();
+        					?><h2><?php __('Images'); ?></h2><?php
+							echo $this->Form->input('Image.image', array('label' => __('New Main Image', true), 'type' => 'file'));
+							echo $this->Form->input('image_id', array('label' => __('Exsisting Main Image', true), 'empty' => __(Configure::read('Website.empty_select'), true)));
+        					echo $this->Form->input('ProductImage', array('options' => $images, 'multiple' => true));
+        				echo $this->Design->niceBoxEnd();
+					?>
+				</div>
+				<div class="clr">&nbsp;</div>
+			<?php
+        echo $this->Design->niceBoxEnd();
+    echo $this->Form->end();
+?>
