314cd462d8b41ce308352f29fd84828944ec64ab
dogmatic <dogmatic69@gmail.com>
2010-05-08 00:55:22 +0200

    adding a method to easily get cart items with cache

--- infinitas/shop/models/cart.php
+++ infinitas/shop/models/cart.php
@@ -36,4 +36,55 @@
 				)
 			)
 		);
+
+		function getCartData($user_id = null){
+			if((int)$user_id > 0){
+				$cacheName = cacheName('cart', $user_id);
+				$cartData = Cache::read($cacheName, 'shop');
+				if(!empty($cartData)){
+					return $cartData;
+				}
+
+				$cartData = $this->find(
+					'all',
+					array(
+						'conditions' => array(
+							'Cart.user_id' => $user_id
+						),
+						'contain' => array(
+							'User',
+							'Product'
+						)
+					)
+				);
+
+				Cache::write($cacheName, $cartData, 'shop');
+
+				return $cartData;
+			}
+
+			App::import('CakeSession');
+			$this->Session = new CakeSession();
+
+			$data = $this->Session->read('Cart.TempCart');
+
+			return (array)$data;
+		}
+
+		function afterSave($created){
+			return $this->dataChanged('afterSave');
+		}
+
+		function afterDelete(){
+			return $this->dataChanged('afterDelete');
+		}
+
+		function dataChanged($from){
+			App::import('CakeSession');
+			$this->Session = new CakeSession();
+
+			Cache::delete(cacheName('cart', $this->Session->read('Auth.User.id')), 'shop');
+
+			return true;
+		}
 	}
