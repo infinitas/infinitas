596dadfa37b35f4d08a3f544544dcc50a824d8b0
dogmatic <dogmatic69@gmail.com>
2010-05-07 15:22:49 +0200

    making methods more generic so they can be used for carts and wishlists

--- infinitas/shop/controllers/components/shop.php
+++ infinitas/shop/controllers/components/shop.php
@@ -13,100 +13,115 @@
 			);
 		}
 
-		function sessionCartSave($product){
-			$carts = $this->Controller->Session->read('Cart.TempCart');
+		function sessionCartSave($Model, $product){
+			$carts = $this->Controller->Session->read($Model->alias.'.Temp'.$Model->alias);
 			if(!empty($carts)){
 				foreach($carts as &$cart){
-					if($cart['Cart']['product_id'] == $this->Controller->params['named']['product_id']){
-						if($cart['Cart']['quantity'] == 0){
+					if($cart[$Model->alias]['product_id'] == $this->Controller->params['named']['product_id']){
+						if($cart[$Model->alias]['quantity'] == 0){
 							unset($cart);
-							$this->Controller->Session->setFlash(__('Product was removed from the cart', true));
+							$this->Controller->Session->setFlash(__('Product was removed from the '.$Model->alias, true));
 						}
 						else{
-							$cart['Cart']['quantity'] += $this->Controller->params['named']['quantity'];
-							$cart['Cart']['price'] = $product['Product']['price'];
-							$cart['Cart']['name'] = $product['Product']['name'];
-							$this->Controller->Session->setFlash(__('Your cart was updated', true));
+							$cart[$Model->alias]['quantity'] += $this->Controller->params['named']['quantity'];
+							$cart[$Model->alias]['price'] = $product['Product']['price'];
+							$cart[$Model->alias]['name'] = $product['Product']['name'];
+							$this->Controller->Session->setFlash(__('Your '.$Model->alias.' was updated', true));
 						}
 					}
 				}
 
-				$this->Controller->Session->write('Cart.TempCart', $carts);
+				$this->Controller->Session->write($Model->alias.'.TempCart', $carts);
 				$this->Controller->redirect($this->Controller->referer());
 			}
 
-			$currentCart[0]['Cart']['product_id'] = $this->Controller->params['named']['product_id'];
-			$currentCart[0]['Cart']['quantity'] = $this->Controller->params['named']['quantity'];
-			$currentCart[0]['Cart']['price'] = $product['Product']['price'];
-			$currentCart[0]['Cart']['name'] = $product['Product']['name'];
+			$currentCart[0][$Model->alias]['product_id'] = $this->Controller->params['named']['product_id'];
+			$currentCart[0][$Model->alias]['quantity'] = $this->Controller->params['named']['quantity'];
+			$currentCart[0][$Model->alias]['price'] = $product['Product']['price'];
+			$currentCart[0][$Model->alias]['name'] = $product['Product']['name'];
 
 			$this->_updateAddCount($product['Product']);
-			$this->Controller->Session->setFlash(__('The product was added to your cart', true));
+			$this->Controller->Session->setFlash(__('The product was added to your '.$Model->alias, true));
 			$this->Controller->redirect($this->Controller->referer());
 		}
 
-		function dbCartSave($product){
-			$currentCart = $this->Controller->Cart->find(
+		function dbCartSave($Model,$product){
+			$currentCart = $Model->find(
 				'first',
 				array(
 					'conditions' => array(
-						'Cart.user_id' => $this->Controller->Session->read('Auth.User.id'),
-						'Cart.product_id' => $this->Controller->params['named']['product_id'],
-						'Cart.deleted' => array(0, 1)
+						$Model->alias.'.user_id' => $this->Controller->Session->read('Auth.User.id'),
+						$Model->alias.'.product_id' => $this->Controller->params['named']['product_id'],
+						$Model->alias.'.deleted' => array(0, 1)
 					)
 				)
 			);
 
 			if(!empty($currentCart)){
 				if($this->Controller->params['named']['quantity'] == 0){
-					if($this->Controller->Cart->delete($currentCart)){
-						$this->Controller->Session->setFlash(__('Product was removed from the cart', true));
+					if($Model->delete($currentCart)){
+						$this->Controller->Session->setFlash(__('Product was removed from the '.$Model->alias, true));
 					}
 					else{
 						$this->Controller->Session->setFlash(__('Something went wrong', true));
 					}
 				}
 				else{
-					$currentCart['Cart']['quantity'] += $this->Controller->params['named']['quantity'];
-					$currentCart['Cart']['deleted'] = 0;
-					$currentCart['Cart']['deleted_date'] = '';
-					$currentCart['Cart']['price'] = $product['Product']['price'];
-					$currentCart['Cart']['name'] = $product['Product']['name'];
+					$currentCart[$Model->alias]['quantity'] += $this->Controller->params['named']['quantity'];
+					$currentCart[$Model->alias]['deleted'] = 0;
+					$currentCart[$Model->alias]['deleted_date'] = '';
+					$currentCart[$Model->alias]['price'] = $product['Product']['price'];
+					$currentCart[$Model->alias]['name'] = $product['Product']['name'];
 
-					if($this->Controller->Cart->save($currentCart)){
-						$this->Controller->Session->setFlash(__('Your cart was updated', true));
+					if($Model->save($currentCart)){
+						$this->Controller->Session->setFlash(__('Your '.$Model->alias.' was updated', true));
 					}
 				}
 				$this->Controller->redirect($this->Controller->referer());
 			}
 
-			$cart['Cart']['product_id'] = $this->Controller->params['named']['product_id'];
-			$cart['Cart']['quantity'] = $this->Controller->params['named']['quantity'];
-			$cart['Cart']['price'] = $product['Product']['price'];
-			$cart['Cart']['name'] = $product['Product']['name'];
-			$cart['Cart']['user_id'] = $this->Controller->Session->read('Auth.User.id');
+			$cart[$Model->alias]['product_id'] = $this->Controller->params['named']['product_id'];
+			$cart[$Model->alias]['quantity'] = $this->Controller->params['named']['quantity'];
+			$cart[$Model->alias]['price'] = $product['Product']['price'];
+			$cart[$Model->alias]['name'] = $product['Product']['name'];
+			$cart[$Model->alias]['user_id'] = $this->Controller->Session->read('Auth.User.id');
 
-			$this->Controller->Cart->create();
-			if($this->Controller->Cart->save($cart)){
-				$this->_updateAddCount($product['Product']);
-				$this->Controller->Session->setFlash(__('The product was added to your cart', true));
+			$Model->create();
+			if($Model->save($cart)){
+				$this->_updateAddCount($Model, $product['Product']);
+				$this->Controller->Session->setFlash(__('The product was added to your '.$Model->alias, true));
 				$this->Controller->redirect($this->Controller->referer());
 			}
 		}
 
-		function _updateAddCount($product = null){
+		function _updateAddCount($Model, $product = null){
 			if(!$product || empty($product)){
 				$this->errors[] = 'no product selected';
 				return false;
 			}
-			return $this->Controller->Cart->Product->updateAll(
-				array(
-					'Product.added_to_cart' => $product['added_to_cart'] + 1
-				),
-				array(
-					'Product.id' => $product['id']
-				)
-			);
+			switch(isset($product['added_to_cart'])){
+				case true:
+					return $Model->Product->updateAll(
+						array(
+							'Product.added_to_cart' => $product['added_to_cart'] + 1
+						),
+						array(
+							'Product.id' => $product['id']
+						)
+					);
+					break;
+
+				case false:
+					return $Model->Product->updateAll(
+						array(
+							'Product.added_to_wishlist' => $product['added_to_wishlist'] + 1
+						),
+						array(
+							'Product.id' => $product['id']
+						)
+					);
+					break;
+			}
 		}
 	}
 ?>
