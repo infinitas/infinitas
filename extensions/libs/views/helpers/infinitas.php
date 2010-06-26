854f534f3d4ed0896f1605548b2d2923fa167184
dogmatic69 <dogmatic69@gmail.com>
2010-06-27 00:45:57 +0100

    updating the current link css to include showing when you are on an item in the same section of the site and not just the exact url match

--- extensions/libs/views/helpers/infinitas.php
+++ extensions/libs/views/helpers/infinitas.php
@@ -243,7 +243,8 @@
 				return false;
 			}
 
-			$suffix = '';
+			$currentCss = $suffix = '';
+
 			if ($this->_menuLevel == 0) {
 				$suffix = '0';
 			}
@@ -277,6 +278,11 @@
 					$menuLink['action']     = (!empty($array['MenuItem']['action'])     ? $array['MenuItem']['action']     : 'index');
 					$menuLink[]             = (!empty($array['MenuItem']['params'])     ? $array['MenuItem']['params']     : null);
 
+					if($menuLink['controller'] == $this->params['controller']){
+						$currentCss = ' current';
+						$this->_currentCssDone = true;
+					}
+
 					foreach($menuLink as $key => $value ){
 						if (empty($value)) {
 							unset($menuLink[$key]);
@@ -291,8 +297,7 @@
 					}
 				}
 
-				$currentCss = '';
-				if($this->_currentCssDone === false && Router::url($menuLink) == $this->here){
+				if(!empty($currentCss) && $this->_currentCssDone === false && Router::url($menuLink) == $this->here){
 					$currentCss = ' current';
 					$this->_currentCssDone = true;
 				}
