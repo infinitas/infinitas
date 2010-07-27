db60fcbdc44bad3d7a82d5c43fa4136d5e75f7c7
dogmatic69 <dogmatic69@gmail.com>
2010-07-27 20:28:09 +0100

    ?

--- index.php
+++ index.php
@@ -15,5 +15,4 @@
 	 * @since         CakePHP(tm) v 0.10.0.1076
 	 * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
 	 */
-
 	require 'webroot' . DIRECTORY_SEPARATOR . 'index.php';
