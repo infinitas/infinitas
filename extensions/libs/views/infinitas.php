cbe8b205e1a957aee00c8c53ffd068f7f391d1ac
dogmatic69 <dogmatic69@gmail.com>
2010-08-30 07:05:53 +0100

    refactoring mustache into the InfinitasView so that its rendered automatically. Now you dont need to call it all over the place.

--- extensions/libs/views/infinitas.php
+++ extensions/libs/views/infinitas.php
@@ -24,10 +24,68 @@
 	App::import('View', 'Theme');
 
 	class InfinitasView extends ThemeView{
+		/**
+		 * place holder for the mustache templating engine.
+		 */
 		public $Mustache = null;
-		
-		function __construct(&$controller, $register = true) {
+
+		/**
+		 * internal cache of template parts from the entire system
+		 */
+		private $__mustacheTemplates = array();
+
+		/**
+		 * internal cache of vars that are used in the mustache template rendering
+		 */
+		private $__vars = array();
+
+		/**
+		 * get the
+		 */
+		public function __construct(&$controller, $register = true) {
 			$this->Mustache = new Mustache();
 			parent::__construct(&$controller, $register);
 		}
+
+		/**
+		 * render views
+		 *
+		 * Lets cake do its thing, then takes the output and runs it through
+		 * mustache, doing all the template rendering things, and then returns
+		 * the final output to where ever its going.
+		 *
+		 * you can pass ?mustache=false in the url to see the raw output skipping
+		 * the template rendering. could be handy for debugging. if debug is off
+		 * this has no effect.
+		 */
+		public function _render($___viewFn, $___dataForView, $loadHelpers = true, $cached = false) {
+			$out = parent::_render($___viewFn, $___dataForView, $loadHelpers, $cached);
+			
+			// only on for admin or it renders the stuff in the editor which is pointless
+			// could maybe just turn it off for edit or some other work around
+			if(!isset($this->params['admin'])){
+				$this->params['admin'] = false;
+			}
+			
+			if($this->Mustache !== null && !$this->params['admin']){
+				if(empty($this->__mustacheTemplates)){
+					$this->__mustacheTemplates = $this->Event->trigger('requireGlobalTemplates');
+				}
+
+				$this->__vars['templates'] =& $this->__mustacheTemplates['requireGlobalTemplates'];
+				$this->__vars['viewVars']  =& $this->viewVars;
+				$this->__vars['params']    =& $this->params;
+
+				if(Configure::read('debug') < 1){
+					unset($this->params['url']['mustache']);
+				}
+
+				if(!isset($this->params['url']['mustache']) || $this->params['url']['mustache'] != 'false'){
+					$out = $this->Mustache->render('{{%UNESCAPED}}{{%DOT-NOTATION}}' . $out, $this->__vars);
+				}
+			}
+			
+			return $out;
+		}
+
 	}
