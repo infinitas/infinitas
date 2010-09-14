c57ef5891146996f2372e08013ef6393156e1a50
dogmatic69 <dogmatic69@gmail.com>
2010-09-14 12:37:24 +0100

    cleaning up the code

--- core/tags/controllers/tags_controller.php
+++ core/tags/controllers/tags_controller.php
@@ -1,152 +1,20 @@
 <?php
-	/**
-	 * CakePHP Tags Plugin
-	 *
-	 * Copyright 2009 - 2010, Cake Development Corporation
-	 *                        1785 E. Sahara Avenue, Suite 490-423
-	 *                        Las Vegas, Nevada 89104
-	 *
-	 * Licensed under The MIT License
-	 * Redistributions of files must retain the above copyright notice.
-	 *
-	 * @copyright 2009 - 2010, Cake Development Corporation (http://cakedc.com)
-	 * @link      http://github.com/CakeDC/Tags
-	 * @package   plugins.tags
-	 * @license   MIT License (http://www.opensource.org/licenses/mit-license.php)
-	 */
-
-	/**
-	 * Short description for class.
-	 *
-	 * @package		plugins.tags
-	 * @subpackage	plugins.tags.controllers
-	 */
-
 	class TagsController extends TagsAppController {
-	/**
-	 * Name
-	 *
-	 * @var string $name
-	 * @access public
-	 */
+		/**
+		 * Name
+		 *
+		 * @var string $name
+		 * @access public
+		 */
 		public $name = 'Tags';
 
-	/**
-	 * Helpers
-	 *
-	 * @var array $name
-	 * @access public
-	 */
-		public $helpers = array('Html', 'Form');
-
-	/**
-	 * Index
-	 *
-	 * @return void
-	 * @access public
-	 */
-		public function index() {
-			$this->Tag->recursive = 0;
-			$this->set('tags', $this->paginate());
-		}
-
-	/**
-	 * Index
-	 *
-	 * @param string
-	 * @return void
-	 * @access public
-	 */
-		public function view($keyName = null) {
-			try {
-				$this->set('tag', $this->Tag->view($keyName));
-			} catch (Exception $e) {
-				$this->Session->setFlash($e->getMessage());
-				$this->redirect('/');
-			}
-		}
-
-	/**
-	 * Admin Index
-	 *
-	 * @return void
-	 * @access public
-	 */
+		/**
+		 * Admin Index
+		 *
+		 * @return void
+		 * @access public
+		 */
 		public function admin_index() {
-			$this->Tag->recursive = 0;
 			$this->set('tags', $this->paginate());
 		}
-
-	/**
-	 * Views a single tag
-	 *
-	 * @param string tag UUID
-	 * @return void
-	 * @access public
-	 */
-		public function admin_view($keyName) {
-			try {
-				$this->set('tag', $this->Tag->view($keyName));
-			} catch (Exception $e) {
-				$this->Session->setFlash($e->getMessage());
-				$this->redirect('/');
-			}
-		}
-
-	/**
-	 * Adds one or more tags
-	 *
-	 * @return void
-	 * @access public
-	 */
-		public function admin_add() {
-			if (!empty($this->data)) {
-				if ($this->Tag->add($this->data)) {
-					$this->Session->setFlash(__d('tags', 'The Tags has been saved.', true));
-					$this->redirect(array('action' => 'index'));
-				}
-			}
-		}
-
-	/**
-	 * Edits a tag
-	 *
-	 * @param string tag UUID
-	 * @return void
-	 * @access public
-	 */
-		public function admin_edit($tagId = null) {
-			try {
-				$result = $this->Tag->edit($tagId, $this->data);
-				if ($result === true) {
-					$this->Session->setFlash(__d('tags', 'Tag saved.', true));
-					$this->redirect(array('action' => 'index'));
-				} else {
-					$this->data = $result;
-				}
-			} catch (Exception $e) {
-				$this->Session->setFlash($e->getMessage());
-				$this->redirect(array('action' => 'index'));
-			}
-
-			if (empty($this->data)) {
-				$this->data = $this->Tag->data;
-			}
-		}
-
-	/**
-	 * Deletes a tag
-	 *
-	 * @param string tag UUID
-	 * @return void
-	 * @access public
-	 */
-		public function admin_delete($id = null) {
-		if ($this->Tag->delete($id)) {
-			$this->Session->setFlash(__d('tags', 'Tag deleted.', true));
-		} else {
-			$this->Session->setFlash(__d('tags', 'Invalid Tag.', true));
-		}
-		$this->redirect(array('action' => 'index'));
 	}
-}
--- core/tags/models/tag.php
+++ core/tags/models/tag.php
@@ -44,14 +44,6 @@
 		);
 
 		/**
-		 * HABTM associations
-		 *
-		 * @var array $hasAndBelongsToMany
-		 * @access public
-		 */
-		public $hasAndBelongsToMany = array();
-
-		/**
 		 * Validation rules
 		 *
 		 * @var array
@@ -61,96 +53,4 @@
 			'name' => array('rule' => 'notEmpty'),
 			'keyname' => array('rule' => 'notEmpty')
 		);
-
-		/**
-		 * Returns the data for a single tag
-		 *
-		 * @param string keyname
-		 * @return array
-		 * @access public
-		 */
-		public function view($keyName = null) {
-			$result = $this->find(
-				'first',
-				array(
-					'conditions' => array(
-						$this->alias . '.keyname' => $keyName
-					)
-				)
-			);
-
-			if (empty($result)) {
-				throw new Exception(__d('tags', 'Invalid Tag.', true));
-			}
-
-			return $result;
-		}
-
-
-		/**
-		 * Pre-populates the tag table with entered tags
-		 *
-		 * @param array post data, should be Contoller->data
-		 * @return boolean
-		 * @access public
-		 */
-		public function add($postData = null) {
-			if (isset($postData[$this->alias]['tags'])) {
-				$this->Behaviors->attach(
-					'Tags.Taggable',
-					array(
-						'resetBinding' => true,
-						'automaticTagging' => false
-					)
-				);
-
-				$this->Tag = $this;
-				$result = $this->saveTags($postData[$this->alias]['tags'], false, false);
-				unset($this->Tag);
-				$this->Behaviors->detach('Tags.Taggable');
-				return $result;
-			}
-		}
-
-		/**
-		 * Edits an existing tag, allows only to modify upper/lowercased characters
-		 *
-		 * @param string tag uuid
-		 * @param array controller post data usually $this->data
-		 * @return mixed True on successfully save else post data as array
-		 * @access public
-		 */
-		public function edit($tagId = null, $postData = null) {
-			$tag = $this->find(
-				'first',
-				array(
-					'contain' => array(),
-					'conditions' => array(
-						'Tag.id' => $tagId
-					)
-				)
-			);
-
-			$this->set($tag);
-			if (empty($tag)) {
-				throw new Exception(__d('tags', 'Invalid Tag.', true));
-			}
-
-			if (!empty($postData[$this->alias]['name'])) {
-				if (strcasecmp($tag['Tag']['name'], $postData[$this->alias]['name']) !== 0) {
-					return false;
-				}
-
-				$this->set($postData);
-				$result = $this->save(null, true);
-
-				if ($result) {
-					$this->data = $result;
-					return true;
-				}
-				else {
-					return $postData;
-				}
-			}
-		}
 	}
