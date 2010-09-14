a52876212a47736481b2339e98963dc511657b51
dakota <waltherlalk@gmail.com>
2010-09-14 16:50:28 +0200

    Removed blog, cms and comment plugins as per current dev branch

--- core/blog/config/releases/map.php
+++
@@ -1,6 +0,0 @@
-<?php
-$map = array(
-	1 => array(
-		'0008_blog' => 'R4c8e68c07dc04f0cb89f38ba6318cd70'),
-);
-?>
--- core/comment/config/releases/map.php
+++
@@ -1,6 +0,0 @@
-<?php
-$map = array(
-	1 => array(
-		'0008_comment' => 'R4c8e68c10a844ddab32738ba6318cd70'),
-);
-?>
