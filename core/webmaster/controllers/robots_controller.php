3b03d1226064c60b148d06c9f48d97b33d7e04b7
dogmatic69 <dogmatic69@gmail.com>
2010-09-17 20:32:38 +0100

    adding a way to edit robots.txt from the webmaster plugin

--- core/webmaster/controllers/robots_controller.php
+++ core/webmaster/controllers/robots_controller.php
@@ -5,4 +5,24 @@
 	 */
 	class RobotsController extends WebmasterAppController{
 		public $name = 'Robots';
+
+		public $uses = array();
+
+		public function admin_edit(){
+			Configure::write('Wysiwyg.editor', 'text');
+			$File = new File(APP . 'webroot' . DS . 'robots.txt');
+
+			if(!empty($this->data)){
+				if($File->write($this->data['Robot']['robots'])){
+					$this->Session->setFlash(__('Robots file updated', true));
+					$this->redirect(array('controller' => 'webmaster', 'action' => 'dashboard'));
+				}
+
+				$this->Session->write(__('There was a problem updating the robots file', true));
+			}
+			
+			if(!isset($this->data['Robot']['robots'])){
+				$this->data['Robot']['robots'] = $File->read();
+			}
+		}
 	}
