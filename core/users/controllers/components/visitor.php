260257244bc44d0c1493f9eb7d160dc2bf6afa94
dogmatic69 <dogmatic69@gmail.com>
2010-09-29 14:08:12 +0100

    using the wrong param in date

--- core/users/controllers/components/visitor.php
+++ core/users/controllers/components/visitor.php
@@ -26,8 +26,15 @@
 
 			if($this->Controller->Session->read('Auth.User.id')){
 				$User = ClassRegistry::init('Users.User');
+				$User->unbindModel(
+					array(
+						'belongsTo' => array_keys($User->belongsTo),
+						'hasOne' => array_keys($User->hasOne)
+					)
+				);
+				
 				$User->updateAll(
-					array('User.last_login' => '\''.date('Y-m-d H:m:s').'\''),
+					array('User.last_login' => '\''.date('Y-m-d H:i:s').'\''),
 					array('User.id' => $this->Controller->Session->read('Auth.User.id'))
 				);
 			}
