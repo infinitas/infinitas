1d44c4ec31522b1a34b506fa8136cc005b988a39
dogmatic69 <dogmatic69@gmail.com>
2010-10-17 14:50:33 +0100

    php5

--- core/management/controllers/management_controller.php
+++ core/management/controllers/management_controller.php
@@ -1,25 +1,26 @@
 <?php
-/**
-* Comment Template.
-*
-* @todo Implement .this needs to be sorted out.
-*
-* Copyright (c) 2009 Carl Sutton ( dogmatic69 )
-*
-* Licensed under The MIT License
-* Redistributions of files must retain the above copyright notice.
-* @filesource
-* @copyright Copyright (c) 2009 Carl Sutton ( dogmatic69 )
-* @link http://infinitas-cms.org
-* @package sort
-* @subpackage sort.comments
-* @license http://www.opensource.org/licenses/mit-license.php The MIT License
-* @since 0.5a
-*/
-	class ManagementController extends ManagementAppController {
-		var $name = 'Management';
+	/**
+	 * Comment Template.
+	 *
+	 * @todo Implement .this needs to be sorted out.
+	 *
+	 * Copyright (c) 2009 Carl Sutton ( dogmatic69 )
+	 *
+	 * Licensed under The MIT License
+	 * Redistributions of files must retain the above copyright notice.
+	 * @filesource
+	 * @copyright Copyright (c) 2009 Carl Sutton ( dogmatic69 )
+	 * @link http://infinitas-cms.org
+	 * @package sort
+	 * @subpackage sort.comments
+	 * @license http://www.opensource.org/licenses/mit-license.php The MIT License
+	 * @since 0.5a
+	 */
 
-		var $uses = array();
+	class ManagementController extends ManagementAppController {
+		public $name = 'Management';
+
+		public $uses = array();
 
 		public function beforeFilter(){
 			parent::beforeFilter();
@@ -27,6 +28,6 @@
 			$this->helpers[] = 'Google.Chart';
 		}
 
-		function admin_dashboard() {
+		public function admin_dashboard() {
 		}
 	}
