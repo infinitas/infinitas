01346b6785f1abebeeb8594c3e55f68963e8410a
dogmatic69 <dogmatic69@gmail.com>
2010-10-18 20:41:56 +0100

    adding validation for the modules

--- core/modules/models/module_position.php
+++ core/modules/models/module_position.php
@@ -1,4 +1,21 @@
 <?php
 	class ModulePosition extends CoreAppModel{
 		public $name = 'ModulePosition';
+
+		public function  __construct($id = false, $table = null, $ds = null) {
+			parent::__construct($id, $table, $ds);
+
+			$this->validate = array(
+				'name' => array(
+					'validName' => array(
+						'rule' => '/[a-z0-9_]{1,50}/',
+						'message' => __('Please enter a valid name, lowercase letters, numbers and underscores only', true)
+					),
+					'isUnique' => array(
+						'rule' => 'isUnique',
+						'message' => __('There is already a position with that name', true)
+					)
+				)
+			);
+		}
 	}
