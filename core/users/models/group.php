edd56507b949331f06e3ea41b4f37f22dc17728f
dogmatic69 <dogmatic69@gmail.com>
2010-10-18 23:14:56 +0100

    adding validation for groups

--- core/users/models/group.php
+++ core/users/models/group.php
@@ -7,6 +7,23 @@
 		public $name = 'Group';
 		public $actsAs1 = array('Tree', 'Acl' => array('requester'));
 
+		public function  __construct($id = false, $table = null, $ds = null) {
+			parent::__construct($id, $table, $ds);
+
+			$this->validate = array(
+				'name' => array(
+					'notEmpty' => array(
+						'rule' => 'notEmpty',
+						'message' => __('Please enter a name for this group', true)
+					),
+					'isUnique' => array(
+						'rule' => 'isUnique',
+						'message' => __('There is alread a group with this name', true)
+					)
+				)
+			);
+		}
+
 		public function parentNode() {
 			return null;
 		}
