7fc0a11c493a3abd255a00b73552d1bd01ff7f59
dogmatic69 <dogmatic69@gmail.com>
2010-10-18 14:44:59 +0100

    adding the data from the release generator

---
+++ core/emails/config/releases/000010_emails.php
@@ -0,0 +1,81 @@
+<?php
+class R4cbc4ef355dc49f1b73016836318cd70 extends CakeRelease {
+
+/**
+ * Migration description
+ *
+ * @var string
+ * @access public
+ */
+	public $description = 'Migration for Emails version 1.0';
+
+/**
+ * Plugin name
+ *
+ * @var string
+ * @access public
+ */
+	public $plugin = 'Emails';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ * @access public
+ */
+	public $migration = array(
+		'up' => array(
+			'create_table' => array(
+				'accounts' => array(
+					'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'primary', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+					'name' => array('type' => 'string', 'null' => false, 'default' => 'My Account', 'length' => 50, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+					'slug' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+					'server' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 150, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+					'username' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 150, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+					'password' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 150, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+					'email' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 150, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+					'ssl' => array('type' => 'boolean', 'null' => false, 'default' => NULL),
+					'port' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 6),
+					'type' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 4, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+					'readonly' => array('type' => 'boolean', 'null' => false, 'default' => NULL),
+					'user_id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+					'system' => array('type' => 'boolean', 'null' => false, 'default' => NULL),
+					'outgoing' => array('type' => 'boolean', 'null' => false, 'default' => NULL),
+					'indexes' => array(
+						'PRIMARY' => array('column' => 'id', 'unique' => 1),
+					),
+					'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB'),
+				),
+			),
+		),
+		'down' => array(
+			'drop_table' => array(
+				'accounts'
+			),
+		),
+	);
+
+	
+/**
+ * Before migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function after($direction) {
+		return true;
+	}
+}
+?>
---
+++ core/emails/config/releases/map.php
@@ -0,0 +1,6 @@
+<?php
+$map = array(
+	1 => array(
+		'000010_emails' => 'R4cbc4ef355dc49f1b73016836318cd70'),
+);
+?>
