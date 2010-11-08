d053a204a25cb5fc530c9f88a67dffddac2b4b19
dogmatic69 <dogmatic69@gmail.com>
2010-11-08 17:25:33 +0000

    adding a find for data by regions

--- core/view_counter/controllers/view_counts_controller.php
+++ core/view_counter/controllers/view_counts_controller.php
@@ -8,6 +8,8 @@
 		 * Create pretty graphs of all the data collected for the
 		 */
 		public function admin_reports(){
+			$byMonth = $byWeek = $byDay = $relatedModel = $byRegion = $foreignKeys = $allModels = null;
+			
 			$conditions = $this->Filter->filter;
 			$conditions['month >= '] = date('Y-m', mktime(0, 0, 0, date('m') - 12));
 			if(isset($this->params['named']['ViewCount.foreign_key'])){
@@ -27,7 +29,9 @@
 				if(isset($relatedModel[0])){
 					$relatedModel = $relatedModel[0];
 				}
-				$this->set(compact('relatedModel'));
+
+				$byRegion = $this->ViewCount->reportByRegion($conditions);
+				$this->set(compact('relatedModel', 'byRegion'));
 			}
 
 			else if(isset($this->params['named']['ViewCount.model']) && !isset($this->params['named']['ViewCount.foreign_key'])){
