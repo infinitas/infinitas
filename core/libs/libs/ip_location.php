ac40497dcb93611e595b2dd0918d5ad92799f35b
dogmatic69 <dogmatic69@gmail.com>
2010-11-15 00:44:31 +0000

    adding the method for getting city when the dat file is available

--- core/libs/libs/ip_location.php
+++ core/libs/libs/ip_location.php
@@ -1,11 +1,42 @@
 <?php
 	Class IpLocation extends Object{
 		private $__emptyCountry = array('country' => false, 'country_code' => false);
+		private $__emptyCity = array('country' => false, 'country_code' => false);
 		
 		public function __construct(){
 			$this->countryDataFile = dirname(dirname(__FILE__)).DS.'libs'.DS.'geoip'.DS.'country.dat';
 			$this->cityDataFile = dirname(dirname(__FILE__)).DS.'libs'.DS.'geoip'.DS.'city.dat';
 		}
+
+		private function __loadFile($type = 'country'){
+			$type = strtolower((string)$type);
+			App::import('Lib', 'Libs.Geoip/inc.php');
+			switch($type){
+				case 'country':
+					if(!is_file($this->countryDataFile)){
+						trigger_error(sprintf(__('%s data file is missing', true), $type), E_USER_WARNING);
+						return false;
+					}
+					return geoip_open($this->countryDataFile, GEOIP_STANDARD);
+					break;
+				
+				case 'city':
+					App::import('Lib', 'Libs.Geoip/city.php');
+					App::import('Lib', 'Libs.Geoip/region_vars.php');
+					if(!is_file($this->cityDataFile)){
+						trigger_error(sprintf(__('%s data file is missing', true), $type), E_USER_WARNING);
+						return false;
+					}
+					return geoip_open($this->cityDataFile, GEOIP_STANDARD);
+					break;
+
+				default:
+					trigger_error(sprintf(__('%s is not a valid data file', true), $type), E_USER_WARNING);
+					return false;
+					break;
+			}
+		}
+
 		/**
 		 * Find users country.
 		 *
@@ -14,7 +45,7 @@
 		 *
 		 * @param string $ipAddress the ip address to check against
 		 * @param bool $code get the country code or not
-		 * 
+		 *
 		 * @return array the data requested
 		 */
 		public function getCountryData($ipAddress = null, $code = false){
@@ -22,16 +53,14 @@
 				return $this->__emptyCountry;
 			}
 
-			App::import('Lib', 'Libs.Geoip/inc.php');
-			if (!is_file($this->countryDataFile)) {
-
-				//pr($ipAddress);
+			$data = $this->__loadFile();
+			if(!$data){
 				return $this->__emptyCountry;
 			}
 
-			$data = geoip_open($this->countryDataFile, GEOIP_STANDARD);
-
 			$country = geoip_country_name_by_addr($data, $ipAddress);
+			pr($country);
+			exit;
 			$country = empty($country) ? 'Unknown' : $country;
 
 			if ($code) {
@@ -42,5 +71,32 @@
 			geoip_close($data);
 
 			return array('country' => $country, 'country_code' => $code);
+		}
+
+		/**
+		 * Find users country.
+		 *
+		 * Attempt to get the country the user is from.  returns unknown if its not
+		 * able to match something.
+		 *
+		 * @param string $ipAddress the ip address to check against
+		 * @param bool $code get the country code or not
+		 *
+		 * @return array the data requested
+		 */
+		public function getCityData($ipAddress = null, $fields = array()){
+			if (!$ipAddress){
+				return $this->__emptyCity;
+			}
+
+			$data = $this->__loadFile('city');
+			if(!$data){
+				return $this->__emptyCity;
+			}
+
+			$city = geoip_record_by_addr($data, $ipAddress);
+			geoip_close($data);
+
+			return (array)$city;
 		}
 	}
