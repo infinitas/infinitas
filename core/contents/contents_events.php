189aaa0887562cf1a26881d27d594d8076d50ade
dogmatic69 <dogmatic69@gmail.com>
2010-11-21 17:30:36 +0000

    ?

--- core/contents/contents_events.php
+++ core/contents/contents_events.php
@@ -11,7 +11,7 @@
 
 		public function  onAttachBehaviors($event) {
 			if(is_subclass_of($event->Handler, 'Model') && isset($event->Handler->_schema) && is_array($event->Handler->_schema)) {
-				if (isset($event->Handler->contentable) && $event->Handler->contentable && !$event->Handler->Behaviors->enabled('Contents.Contentable')) {
+				if (isset($event->Handler->contentable) && $event->Handler->contentable && !$event->Handler->Behaviors->enabled('Contents.Contentable')) {					
 					$event->Handler->Behaviors->attach('Contents.Contentable');
 				}
 			}
