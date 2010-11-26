ddf5eb3aa6c3a58a16b19be455f7609f19e0a19e
dogmatic69 <dogmatic69@gmail.com>
2010-11-26 14:28:09 +0000

    adding some more docs for the categories

--- core/categories/categories_app_controller.php
+++ core/categories/categories_app_controller.php
@@ -1,5 +1,32 @@
 <?php
+	/**
+	 * Categories base controller
+	 *
+	 * All the category related controllers extends from this controller
+	 *
+	 * Copyright (c) 2010 Carl Sutton ( dogmatic69 )
+	 *
+	 * @filesource
+	 * @copyright Copyright (c) 2010 Carl Sutton ( dogmatic69 )
+	 * @link http://www.infinitas-cms.org
+	 * @package Infinitas.Categories
+	 * @subpackage Infinitas.Categories.AppController
+	 * @license http://www.opensource.org/licenses/mit-license.php The MIT License
+	 * @since 0.8a
+	 *
+	 * @author dogmatic69
+	 *
+	 * Licensed under The MIT License
+	 * Redistributions of files must retain the above copyright notice.
+	 */
+
 	class CategoriesAppController extends AppController {
+		/**
+		 * before anything starts processing this fires to include the filter
+		 * helper
+		 * 
+		 * @access public
+		 */
 		public function beforeFilter(){
 			parent::beforeFilter();
 			$this->helpers[] = 'Filter.Filter';
--- core/categories/categories_events.php
+++ core/categories/categories_events.php
@@ -1,4 +1,26 @@
 <?php
+	/**
+	 * Categories plugin events.
+	 *
+	 * The events for the menu plugin for setting up cache and the general
+	 * configuration of the plugin.
+	 *
+	 * Copyright (c) 2010 Carl Sutton ( dogmatic69 )
+	 *
+	 * @filesource
+	 * @copyright Copyright (c) 2010 Carl Sutton ( dogmatic69 )
+	 * @link http://www.infinitas-cms.org
+	 * @package Infinitas.Categories
+	 * @subpackage Infinitas.Categories.AppEvents
+	 * @license http://www.opensource.org/licenses/mit-license.php The MIT License
+	 * @since 0.8a
+	 *
+	 * @author dogmatic69
+	 *
+	 * Licensed under The MIT License
+	 * Redistributions of files must retain the above copyright notice.
+	 */
+
 	final class CategoriesEvents extends AppEvents{
 		public function onAdminMenu($event){
 			$menu['main'] = array(
