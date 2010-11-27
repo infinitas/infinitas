7fd27c1273be63aedb3b62ee1570a8dd487b829d
dogmatic69 <dogmatic69@gmail.com>
2010-11-27 17:57:57 +0000

    cleaning up the contact plugin docs

--- core/contact/contact_app_controller.php
+++ core/contact/contact_app_controller.php
@@ -1,8 +1,11 @@
 <?php
 	/**
-	 * The main contact's plugin model
+	 * @brief The main contact's plugin model
 	 *
 	 * this is extended by the other models in contact plugin
+	 *
+	 * @todo some sort of telephone index using the Branch and Contact model
+	 * could be done with FeedableBehavior
 	 *
 	 * @copyright Copyright (c) 2010 Carl Sutton ( dogmatic69 )
 	 * @link http://www.infinitas-cms.org
@@ -10,7 +13,7 @@
 	 * @license http://www.opensource.org/licenses/mit-license.php The MIT License
 	 * @since 0.7a
 	 *
-	 * @author Carl Sutton ( dogmatic69 )
+	 * @author dogmatic69
 	 *
 	 * Licensed under The MIT License
 	 * Redistributions of files must retain the above copyright notice.
