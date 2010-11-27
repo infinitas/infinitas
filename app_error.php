599a5f88e943e05bfc46d840b125f8434f8ded95
dogmatic69 <dogmatic69@gmail.com>
2010-11-27 23:32:08 +0000

    adjusting the namespace and some comments for other methods to inherit

--- app_error.php
+++ app_error.php
@@ -21,7 +21,7 @@
 	 * 
 	 * @copyright Copyright (c) 2010 Carl Sutton ( dogmatic69 )
 	 * @link http://www.infinitas-cms.org
-	 * @package AppError
+	 * @package Infinitas
 	 * @license http://www.opensource.org/licenses/mit-license.php The MIT License
 	 * @since 0.7a
 	 *
