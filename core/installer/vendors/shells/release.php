57b1a9ed77f5b25216e97b20f76d0945a757429d
dogmatic69 <dogmatic69@gmail.com>
2010-12-07 13:12:18 +0000

    adding some help and using the new methods for formatting

--- core/installer/vendors/shells/release.php
+++ core/installer/vendors/shells/release.php
@@ -1,19 +1,44 @@
 <?php
 
 class ReleaseShell extends Shell {
-	public $tasks = array('Plugin');
+	public $tasks = array('Infinitas', 'Plugin');
+
+	public function help(){
+		$this->Infinitas->h1('Interactive Release Shell Help');
+		$this->Infinitas->p(
+			'The interactive shell is for generating a release for your Infinitas '.
+			'powered plugin. It will allow you to enter the description and name '.
+			'along with setting any dependencies. After you have answered some '.
+			'questions all the necesery files will be generated to allow others '.
+			'to use the plugin via the installer.'
+		);
+
+		$this->Infinitas->h2(
+			'Available Options'
+		);
+
+		$this->Infinitas->li(
+			array(
+				'cake release - For normal plugins',
+				'cake release all - If you are creating a core plugin'
+			)
+		);
+	}
 
 	public function main() {
 		do {
-			$this->out('Interactive Release Shell');
-			$this->hr();
-			$this->out('[P]lugin');
-			$this->out('[M]odule');
-			$this->out('[T]heme');
-			$this->out('Plugin [A]dd-on');
-			$this->out('[Q]uit');
-
-			$input = strtoupper($this->in('What do yolu wish to release?'));
+			$this->Infinitas->h1('Interactive Release Shell');
+			$this->Infinitas->li(
+				array(
+					'[P]lugin',
+					'[M]odule',
+					'[T]heme',
+					'Plugin [A]dd-on',
+					'[Q]uit'
+				)
+			);
+			$this->Infinitas->br();
+			$input = strtoupper($this->in('What do you wish to release?'));
 
 			switch ($input) {
 				case 'P':
