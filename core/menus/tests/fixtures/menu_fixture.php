a56dab4ce2ba3a69fcdf58b11696e1963ea558b5
dogmatic69 <dogmatic69@gmail.com>
2010-12-13 17:35:48 +0000

    fixing up the fixtures for the menus plugin

--- core/menus/tests/fixtures/menu_fixture.php
+++ core/menus/tests/fixtures/menu_fixture.php
@@ -7,8 +7,8 @@ class MenuFixture extends CakeTestFixture {
 
 	var $fields = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10, 'key' => 'primary'),
-		'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100),
-		'type' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'key' => 'index'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'type' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'key' => 'index', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'item_count' => array('type' => 'integer', 'null' => false, 'default' => '0'),
 		'active' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
@@ -20,21 +20,21 @@ class MenuFixture extends CakeTestFixture {
 	var $records = array(
 		array(
 			'id' => 1,
-			'name' => 'Admin Menu',
-			'type' => 'core_admin',
-			'item_count' => 0,
-			'active' => 1,
-			'created' => '2010-01-27 18:07:51',
-			'modified' => '2010-01-27 18:07:51'
-		),
-		array(
-			'id' => 2,
-			'name' => 'Main User Menu',
+			'name' => 'Public Users',
 			'type' => 'main_menu',
 			'item_count' => 0,
 			'active' => 1,
 			'created' => '2010-02-01 00:35:47',
 			'modified' => '2010-02-01 00:35:47'
+		),
+		array(
+			'id' => 2,
+			'name' => 'Registered Users',
+			'type' => 'registered_users',
+			'item_count' => 0,
+			'active' => 1,
+			'created' => '2010-05-13 18:59:35',
+			'modified' => '2010-05-13 18:59:35'
 		),
 	);
 }
