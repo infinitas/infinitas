5711426dd87e40f38018b4f24b70310f7681bbb1
dogmatic69 <dogmatic69@gmail.com>
2011-06-06 17:16:52 +0100

    fixing up a few errors in the contacts, and a method to find contacts for a specific model plugin combo

--- core/contact/models/address.php
+++ core/contact/models/address.php
@@ -38,4 +38,20 @@
 
 			return $address;
 		}
+
+		/**
+		 * Find a list of addresses for the currently selected plugin that may
+		 * be related to what they user is looking for.
+		 *
+		 * @param array $conditions the conditions to search for
+		 * @return array list of addresses
+		 */
+		public function getAddressesByRelated($conditions = array()){
+			return $this->find(
+				'list',
+				array(
+					'conditions' => (array)$conditions
+				)
+			);
+		}
 	}
