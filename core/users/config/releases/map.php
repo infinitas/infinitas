cb449de2e2e00cb45b05b72653002e6d7c279c1a
dogmatic69 <dogmatic69@gmail.com>
2011-07-20 22:21:15 +0100

    updating the user table structure

--- core/users/config/releases/map.php
+++ core/users/config/releases/map.php
@@ -2,5 +2,7 @@
 $map = array(
 	1 => array(
 		'000008_users' => 'R4c94edce5f344b39992078d86318cd70'),
+	2 => array(
+		'000081_users' => 'R4e2746ba9a9c40c6b19349266318cd70'),
 );
 ?>
--- core/users/config/schema/schema.php
+++ core/users/config/schema/schema.php
@@ -1,6 +1,6 @@
 <?php 
 /* SVN FILE: $Id$ */
-/* Users schema generated on: 2010-09-18 18:09:22 : 1284828622*/
+/* Users schema generated on: 2011-07-20 22:07:58 : 1311196858*/
 class UsersSchema extends CakeSchema {
 	var $name = 'Users';
 
@@ -13,8 +13,8 @@ class UsersSchema extends CakeSchema {
 
 	var $groups = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10, 'key' => 'primary'),
-		'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 50),
-		'description' => array('type' => 'text', 'null' => false, 'default' => NULL),
+		'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 50, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'description' => array('type' => 'text', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
 		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
 		'parent_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'index'),
@@ -25,21 +25,21 @@ class UsersSchema extends CakeSchema {
 	);
 	var $users = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
-		'username' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 20, 'key' => 'unique'),
-		'email' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'key' => 'unique'),
-		'password' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 40),
+		'username' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 20, 'key' => 'unique', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'email' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'key' => 'unique', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'password' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 40, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'facebook_id' => array('type' => 'integer', 'null' => false, 'default' => '0', 'length' => 20),
 		'twitter_id' => array('type' => 'integer', 'null' => false, 'default' => '0', 'length' => 20, 'key' => 'index'),
 		'birthday' => array('type' => 'date', 'null' => true, 'default' => NULL),
 		'active' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
 		'group_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
-		'ip_address' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100),
-		'browser' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100),
-		'operating_system' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 50),
+		'ip_address' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'browser' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'operating_system' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 50, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'last_login' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
 		'last_click' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
-		'country' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 150),
-		'city' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 150),
+		'country' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 150, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'city' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 150, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'is_mobile' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
 		'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
 		'modified' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
