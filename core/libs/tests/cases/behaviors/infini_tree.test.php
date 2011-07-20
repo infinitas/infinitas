50f4c5d38b957538bfe245dc2ba381606125c064
Jelle Henkens <jelle.henkens@gmail.com>
2011-07-20 17:27:42 +0100

    Added tests for modifying the nodes

--- core/libs/tests/cases/behaviors/infini_tree.test.php
+++ core/libs/tests/cases/behaviors/infini_tree.test.php
@@ -81,7 +81,7 @@
 			//Try adding a root node without specifying the scope field
 			//should fail because we cant get the scope
 			$data = array(
-				'name' => '3 root',
+				'name' => '3 Root',
 				'parent_id' => null
 			);
 			$this->ScopedNumberTree->create();
@@ -89,7 +89,7 @@
 			
 			//should work with the scope
 			$data = array(
-				'name' => '3 root',
+				'name' => '3 Root',
 				'category_id' => 'cat-a',
 				'parent_id' => null
 			);
@@ -113,12 +113,38 @@
 			);
 			$this->ScopedNumberTree->create();
 			$this->assertTrue($this->ScopedNumberTree->save($data));			
+
+			
+			//Should be able to modify the name of a node without breaking stuff
+			$data = array(
+				'name' => '1.2 Node (edited)'
+			);
+			$this->ScopedNumberTree->id = 'cat-a-1-2';
+			$this->assertTrue($this->ScopedNumberTree->save($data));
+			
+			//Move 1.2.3 into 1.1 (becomes 1.1.1)
+			$data = array(
+				'name' => '1.1.1 Node (edited)',
+				'parent_id' => 'cat-a-1-1'
+			);
+			$this->ScopedNumberTree->id = 'cat-a-1-2-3';
+			$this->assertTrue($this->ScopedNumberTree->save($data));
+			
+			//Check the structure after all the changes
+			$expected = array('1 Root', '_1.1 Node', '__1.1.1 Node (edited)', '_1.2 Node (edited)', '__1.2.1 Node', '__1.2.2 Node', '_1.3 Node', '__1.3.1 node', '__1.3.2 node', '2 Root', '3 Root');
+			$this->assertEqual($expected, array_values($this->ScopedNumberTree->generatetreelist(array('ScopedNumberTree.category_id' => 'cat-a'))));
 			
 			//Verify trees after saving a bunch of new nodes
 			$validTree = $this->ScopedNumberTree->verify('cat-a');
 			$this->assertIdentical($validTree, true);
 			$validTree = $this->ScopedNumberTree->verify('cat-b');
 			$this->assertIdentical($validTree, true);
+			
+			
+		}
+		
+		public function testDeletingNodes() {
+			
 		}
 		
 		public function testFinds() {
