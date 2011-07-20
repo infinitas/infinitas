4489b60daec40b30d688bcc0811f21083e17d1cc
dogmatic69 <dogmatic69@gmail.com>
2011-07-20 22:19:08 +0100

    updating the server status table data

--- core/server_status/config/releases/map.php
+++ core/server_status/config/releases/map.php
@@ -2,5 +2,7 @@
 $map = array(
 	1 => array(
 		'000008_server_status' => 'R4cfe4072364844ea9a141bbd6318cd70'),
+	2 => array(
+		'000081_server_status' => 'R4e274621abbc4f6bb40849266318cd70'),
 );
 ?>
