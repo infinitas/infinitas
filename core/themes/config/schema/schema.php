57eec3f6f588caec3e00c9673aa52425ff6999c3
dogmatic69 <dogmatic69@gmail.com>
2011-07-20 22:20:39 +0100

    updating the themes table data

--- core/themes/config/schema/schema.php
+++ core/themes/config/schema/schema.php
@@ -1,6 +1,6 @@
 <?php 
 /* SVN FILE: $Id$ */
-/* Themes schema generated on: 2010-09-18 18:09:22 : 1284828622*/
+/* Themes schema generated on: 2011-07-20 22:07:24 : 1311196824*/
 class ThemesSchema extends CakeSchema {
 	var $name = 'Themes';
 
@@ -13,12 +13,12 @@ class ThemesSchema extends CakeSchema {
 
 	var $themes = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10, 'key' => 'primary'),
-		'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100),
-		'description' => array('type' => 'text', 'null' => true, 'default' => NULL),
-		'author' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 150),
-		'url' => array('type' => 'string', 'null' => true, 'default' => NULL),
-		'update_url' => array('type' => 'string', 'null' => true, 'default' => NULL),
-		'licence' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100),
+		'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'description' => array('type' => 'text', 'null' => true, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'author' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 150, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'url' => array('type' => 'string', 'null' => true, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'update_url' => array('type' => 'string', 'null' => true, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'licence' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'active' => array('type' => 'boolean', 'null' => false, 'default' => '0', 'key' => 'index'),
 		'core' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
