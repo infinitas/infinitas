e84ef837effd490356373ce87f3a0fbcd4dc2300
dogmatic69 <dogmatic69@gmail.com>
2011-07-25 23:51:37 +0100

    adding a release for the geo_location plugin

---
+++ core/geo_location/config/releases/map.php
@@ -0,0 +1,6 @@
+<?php
+$map = array(
+	1 => array(
+		'000008_geo_location' => 'R4e2df363be544c018625412e6318cd70'),
+);
+?>
---
+++ core/geo_location/config/schema/schema.php
@@ -0,0 +1,15 @@
+<?php 
+/* SVN FILE: $Id$ */
+/* GeoLocation schema generated on: 2011-07-25 23:07:15 : 1311634275*/
+class GeoLocationSchema extends CakeSchema {
+	var $name = 'GeoLocation';
+
+	function before($event = array()) {
+		return true;
+	}
+
+	function after($event = array()) {
+	}
+
+}
+?>
