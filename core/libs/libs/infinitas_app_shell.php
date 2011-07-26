2152bd99b987752c24ea2abad25d143d8a916503
dogmatic69 <dogmatic69@gmail.com>
2011-07-26 14:23:09 +0100

    removing a check for a variable that is not set

--- core/libs/libs/infinitas_app_shell.php
+++ core/libs/libs/infinitas_app_shell.php
@@ -43,24 +43,20 @@
 				App::import('Folder');
 			}
 
-			if($paths === false){
-				$Folder = new Folder(APP);
-				$folders = $Folder->read();
-				$folders = array_flip($folders[0]);
-				unset($Folder, $folders['.git'], $folders['config'], $folders['locale'],
-					$folders['nbproject'], $folders['tmp'], $folders['views'],
-					$folders['webroot'], $folders['tests']);
+			$Folder = new Folder(APP);
+			$folders = $Folder->read();
+			$folders = array_flip($folders[0]);
+			unset($Folder, $folders['.git'], $folders['config'], $folders['locale'],
+				$folders['nbproject'], $folders['tmp'], $folders['views'],
+				$folders['webroot'], $folders['tests']);
 
-				$paths = array();
-				foreach(array_flip($folders) as $folder){
-					$paths[] = APP . $folder . DS;
-				}
-
-				Cache::write('plugin_paths', $paths);
-				unset($Folder, $folders);
-
-				// @todo trigger event to get oter plugin paths
+			$paths = array();
+			foreach(array_flip($folders) as $folder){
+				$paths[] = APP . $folder . DS;
 			}
+
+			Cache::write('plugin_paths', $paths);
+			unset($Folder, $folders);
 
 			App::build(
 				array(
