355c01e58d5855e62f95b98f49f0ec3e1cd96024
dogmatic69 <dogmatic69@gmail.com>
2011-08-04 17:59:10 +0100

    fixing the file being included a second time when you bake two in a row, and updating the docs

--- core/libs/vendors/shells/tasks/infinitas_fixture_bake.php
+++ core/libs/vendors/shells/tasks/infinitas_fixture_bake.php
@@ -1,11 +1,53 @@
 <?php
+	/**
+	 * @file
+	 * 
+	 * @copyright Copyright (c) 2009 Carl Sutton ( dogmatic69 )
+	 * @link http://infinitas-cms.org
+	 * @package Infinitas.Libs.Vendors.Shells
+	 * @license http://www.opensource.org/licenses/mit-license.php The MIT License
+	 * @since 0.8a
+	 *
+	 * @author dogmatic69
+	 *
+	 * Licensed under The MIT License
+	 * Redistributions of files must retain the above copyright notice.
+	 */
+
+	 /**
+	  * @brief InfinitasFixtureBakeTask for generating fixtures.
+	  *
+	  * This task is for generating fixtures in your app. Its similar to the cake
+	  * fixture stuff but will also allow you to update your fixtures.
+	  */
 	class InfinitasFixtureBakeTask extends FixtureTask {
+		/**
+		 * @brief additional tasks that are used within this task.
+		 * @var <type>
+		 */
 		public $tasks = array('Fixture', 'Model', 'Template');
 
+		/**
+		 * @brief generate the fixture specified
+		 *
+		 * @access public
+		 *
+		 * @param string $modelName the model to be generated in Plugin.Model format
+		 * @param bool $new true if its a new fixture, false if you are updating
+		 *
+		 * @return see generateFixtureFile()
+		 */
 		public function setup($modelName = null, $new = true){
 			list($this->plugin, $modelName) = pluginSplit($modelName);
 			$this->connection = 'default';
+
 			$useTable = $this->Model->getTable($modelName, $this->connection);
+			try{
+				$useTable = ClassRegistry::init($this->plugin . '.' . $modelName)->tablePrefix . ClassRegistry::init($this->plugin . '.' . $modelName)->useTable;
+			}
+			catch(Exception $e){
+				
+			}
 
 			$newFixture = $this->getNewFixture($modelName, $useTable, array());
 			
@@ -50,6 +92,23 @@
 			return $this->generateFixtureFile($modelName, $oldFixture);
 		}
 
+		/**
+		 * @brief clean up the old fixture, merging the new data with it
+		 *
+		 * This method will remove any fields that are no longer present in the
+		 * database and remove any fields in the fixture data that are not needed
+		 *
+		 * It will also add any new fields that were found in the database to the
+		 * fixture and data.
+		 *
+		 * @access public
+		 *
+		 * @param array $fixture the old version of the fixture
+		 * @param array $new list of new fields (to add)
+		 * @param array $old list of old fields (to remove)
+		 *
+		 * @return array the old fixture with new stuff merged in
+		 */
 		private function __cleanupRecords($fixture, $new, $old){
 			foreach($new as $newField){
 				foreach($fixture['records'] as $k => $record){
@@ -74,6 +133,17 @@
 			return $fixture['records'];
 		}
 
+		/**
+		 * @brief generate the data for a new fixture
+		 *
+		 * @access public
+		 *
+		 * @param string $model the model to generate the fixture for
+		 * @param string $useTable the table that the model uses
+		 * @param array $importOptions options for the import
+		 *
+		 * @return array of fixture data
+		 */
 		public function getNewFixture($model, $useTable = false, $importOptions = array()) {
 			if (!class_exists('CakeSchema')) {
 				App::import('Model', 'CakeSchema', false);
@@ -111,6 +181,16 @@
 			return $fixture;
 		}
 
+		/**
+		 * @brief read the data from an existing fixture
+		 *
+		 * @access public
+		 *
+		 * @param string $plugin the name of the plugin
+		 * @param string $model the name of the model
+		 *
+		 * @return array details of the fixture (records, fields etc)
+		 */
 		public function getOldFixture($plugin, $model){
 			$file = App::pluginPath($plugin) . 'tests' . DS . 'fixtures' . DS . Inflector::underscore($model) . '_fixture.php';
 			if(!is_file($file)){
@@ -118,14 +198,27 @@
 			}
 
 			$cakeFixture = CAKE_CORE_INCLUDE_PATH . DS . 'cake' . DS . 'tests' . DS . 'lib' . DS . 'cake_test_fixture.php';
-			require $cakeFixture;
-			require $file;
+			require_once $cakeFixture;
+			require_once $file;
 
 			$fixtureClass = $model . 'Fixture';
 
 			return get_class_vars($fixtureClass);
 		}
 
+		/**
+		 * @generate a fixture
+		 *
+		 * This method will first convert the arrays to flat text and then pass it
+		 * to cakes normal fixture writing method.
+		 *
+		 * @access public
+		 * 
+		 * @param string $model the
+		 * @param array $options the data for the fixture
+		 *
+		 * @return see FixtureTask::generateFixtureFile()
+		 */
 		public function generateFixtureFile($model, $options){
 			$options['fields'] = explode("\n", $this->_generateSchema($options['fields']));
 			foreach($options['fields'] as $k => $line){
