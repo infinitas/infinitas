4298092ab5c864cb9749dd2d33385c03f6c41d4d
Jelle Henkens <jelle.henkens@gmail.com>
2011-08-04 17:35:17 +0100

    Modified the username field to 50 chars

--- core/users/config/releases/map.php
+++ core/users/config/releases/map.php
@@ -4,5 +4,7 @@ $map = array(
 		'000008_users' => 'R4c94edce5f344b39992078d86318cd70'),
 	2 => array(
 		'000081_users' => 'R4e2746ba9a9c40c6b19349266318cd70'),
+	3 => array(
+		'000082_users' => 'R4e3ac986225c412d9ca144c96318cd70'),
 );
 ?>
