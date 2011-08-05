3f50a3aec12e62cc4a0d3d04285c677cbcd4c03d
dogmatic69 <dogmatic69@gmail.com>
2011-08-05 12:17:00 +0100

    updating fixtures to use uuid inline with schema changes

--- core/modules/tests/fixtures/module_position_fixture.php
+++ core/modules/tests/fixtures/module_position_fixture.php
@@ -1,10 +1,11 @@
 <?php
 	class ModulePositionFixture extends CakeTestFixture {
 		public $name = 'ModulePosition';
-			public $table = 'core_module_positions';
 		
-			public $fields = 	array(
-			'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10, 'key' => 'primary'),
+		public $table = 'core_module_positions';
+		
+		public $fields = 	array(
+			'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'primary', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 			'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 50, 'key' => 'index', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 			'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
 			'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
@@ -13,83 +14,83 @@
 			'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
 		);
 	
-			public $records = 	array(
+		public $records = 	array(
 			array(
-				'id' => 1,
+				'id' => 'module-position-top',
 				'name' => 'top',
 				'created' => '2010-01-18 21:45:23',
 				'modified' => '2010-01-18 21:45:23',
 				'module_count' => 'null'
 			),
 			array(
-				'id' => 2,
+				'id' => 'module-position-bottom',
 				'name' => 'bottom',
 				'created' => '2010-01-18 21:45:23',
 				'modified' => '2010-01-18 21:45:23',
 				'module_count' => 'null'
 			),
 			array(
-				'id' => 3,
+				'id' => 'module-position-left',
 				'name' => 'left',
 				'created' => '2010-01-18 21:45:23',
 				'modified' => '2010-01-18 21:45:23',
 				'module_count' => 'null'
 			),
 			array(
-				'id' => 4,
+				'id' => 'module-position-right',
 				'name' => 'right',
 				'created' => '2010-01-18 21:45:23',
 				'modified' => '2010-01-18 21:45:23',
 				'module_count' => 'null'
 			),
 			array(
-				'id' => 5,
+				'id' => 'module-position-custom1',
 				'name' => 'custom1',
 				'created' => '2010-01-18 21:45:23',
 				'modified' => '2010-01-18 21:45:23',
 				'module_count' => 'null'
 			),
 			array(
-				'id' => 6,
+				'id' => 'module-position-custom2',
 				'name' => 'custom2',
 				'created' => '2010-01-18 21:45:23',
 				'modified' => '2010-01-18 21:45:23',
 				'module_count' => 'null'
 			),
 			array(
-				'id' => 7,
+				'id' => 'module-position-custom3',
 				'name' => 'custom3',
 				'created' => '2010-01-18 21:45:23',
 				'modified' => '2010-01-18 21:45:23',
 				'module_count' => 'null'
 			),
 			array(
-				'id' => 8,
+				'id' => 'module-position-custom4',
 				'name' => 'custom4',
 				'created' => '2010-01-18 21:45:23',
 				'modified' => '2010-01-18 21:45:23',
 				'module_count' => 'null'
 			),
 			array(
-				'id' => 9,
+				'id' => 'module-position-bread-crumbs',
 				'name' => 'bread_crumbs',
 				'created' => '2010-01-18 21:45:23',
 				'modified' => '2010-01-18 21:45:23',
 				'module_count' => 'null'
 			),
 			array(
-				'id' => 10,
+				'id' => 'module-position-debug',
 				'name' => 'debug',
 				'created' => '2010-01-18 21:45:23',
 				'modified' => '2010-01-18 21:45:23',
 				'module_count' => 'null'
 			),
 			array(
-				'id' => 11,
+				'id' => 'module-position-hidden',
 				'name' => 'hidden',
 				'created' => '2010-01-18 21:45:23',
 				'modified' => '2010-01-18 21:45:23',
 				'module_count' => 'null'
 			),
 		);
-		}
+	}
