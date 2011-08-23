8e7ceacdf44f9063eafed29d56ad848195d81cd1
Dakota <waltherlalk@gmail.com>
2011-08-23 14:47:55 +0200

    Fix missing lcfirst function issue with php < 5.3

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -66,6 +66,11 @@
 
 	unset($paths);
 
+	if ( false === function_exists('lcfirst') ) {
+		function lcfirst( $str ) 
+		{ return (string)(strtolower(substr($str,0,1)).substr($str,1));} 
+	}
+	
 	/**
 	 * Load plugin events
 	 */
