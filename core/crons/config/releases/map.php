0c5670f08002e9377b6cdeceb4d2f8f54457e86c
dogmatic69 <dogmatic69@gmail.com>
2011-09-03 23:51:18 +0100

    updating the schema migration for crons

--- core/crons/config/releases/map.php
+++ core/crons/config/releases/map.php
@@ -4,5 +4,7 @@ $map = array(
 		'000008_crons' => 'R4cfe4011f4784cd9aef31bbd6318cd70'),
 	2 => array(
 		'000081_crons' => 'R4e2743305f204f6b842149266318cd70'),
+	3 => array(
+		'000082_crons' => 'R4e62aef55ca04bca860140406318cd70'),
 );
 ?>
