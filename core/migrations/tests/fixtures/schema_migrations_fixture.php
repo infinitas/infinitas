bd45fda6ae628b428a7abdc6430d29b09234aba8
dogmatic69 <dogmatic69@gmail.com>
2011-09-07 10:45:50 +0100

    fixing up the migrations for schemas to test installing

--- core/migrations/tests/fixtures/schema_migrations_fixture.php
+++ core/migrations/tests/fixtures/schema_migrations_fixture.php
@@ -1,25 +1,42 @@
 <?php
-class SchemaMigrationsFixture extends CakeTestFixture {
-/**
- *
- */
-	public $name = 'SchemaMigrations';
-/**
- *
- */
-	public $fields = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
-		'version' => array('type' => 'integer', 'null' => false, 'default' => NULL),
-		'type' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 50),
-		'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
-		'indexes' => array(
-			'PRIMARY' => array('column' => 'id', 'unique' => 1)
-	));
-/**
- *
- */
-	public $records = array(
-		array('id' => '1', 'version' => '1', 'type' => 'migrations', 'created' => '2009-11-10 00:55:34')
-	);
-}
-?>
+	class SchemaMigrationFixture extends CakeTestFixture {
+		public $name = 'SchemaMigration';
+		
+		public $table = 'schema_migrations';
+		
+		public $fields = array(
+			'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+			'version' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+			'type' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 50, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+			'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+			'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+			'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'MyISAM')
+		);
+	
+		public $records = array(
+			array(
+				'id' => 1,
+				'version' => 1,
+				'type' => 'migrations',
+				'created' => '2011-09-06 16:53:39'
+			),
+			array(
+				'id' => 2,
+				'version' => 1,
+				'type' => 'app',
+				'created' => '2011-09-06 16:54:08'
+			),
+			array(
+				'id' => 3,
+				'version' => 1,
+				'type' => 'Installer',
+				'created' => '2011-09-06 16:54:09'
+			),
+			array(
+				'id' => 4,
+				'version' => 2,
+				'type' => 'Installer',
+				'created' => '2011-09-06 16:54:09'
+			),
+		);
+	}
