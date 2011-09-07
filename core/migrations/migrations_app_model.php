7c8ef8ddd3ba462f32d0f94d9d841dcd0e0b61be
dogmatic69 <dogmatic69@gmail.com>
2011-09-07 15:09:17 +0100

    adding a model for the migrations plugin

---
+++ core/migrations/migrations_app_model.php
@@ -0,0 +1,21 @@
+<?php
+	/**
+	 * MigrationsAppModel model
+	 *
+	 * @brief Add some documentation for SchemaMigration model.
+	 *
+	 * @copyright Copyright (c) 2009 Carl Sutton (dogmatic69)
+	 *
+	 * @link		http://infinitas-cms.org/Migrations
+	 * @package		Migrations.models.SchemaMigration
+	 * @license		http://infinitas-cms.org/mit-license The MIT License
+	 * @since		0.9b1
+	 *
+	 * @author dogmatic69
+	 *
+	 * Licensed under The MIT License
+	 * Redistributions of files must retain the above copyright notice.
+	 */
+	class MigrationsAppModel extends AppModel {
+		
+	}
---
+++ core/migrations/models/schema_migration.php
@@ -0,0 +1,109 @@
+<?php
+	/**
+	 * SchemaMigration model
+	 *
+	 * @brief Add some documentation for SchemaMigration model.
+	 *
+	 * @copyright Copyright (c) 2009 Carl Sutton (dogmatic69)
+	 *
+	 * @link		http://infinitas-cms.org/Migrations
+	 * @package		Migrations.models.SchemaMigration
+	 * @license		http://infinitas-cms.org/mit-license The MIT License
+	 * @since		0.9b1
+	 *
+	 * @author dogmatic69
+	 *
+	 * Licensed under The MIT License
+	 * Redistributions of files must retain the above copyright notice.
+	 */
+
+	class SchemaMigration extends MigrationsAppModel {
+		/**
+		 * The name of the model
+		 *
+		 * @access public
+		 * @var string
+		 */
+		public $name = 'SchemaMigration';
+
+		/**
+		 * Additional behaviours that are attached to this model
+		 *
+		 * @access public
+		 * @var array
+		 */
+		public $actsAs = array(
+			// 'Libs.Feedable',
+			// 'Libs.Rateable'
+		);
+
+		/**
+		 * How the default ordering on this model is done
+		 *
+		 * @access public
+		 * @var array
+		 */
+		public $order = array(
+		);
+
+		/**
+		 * hasOne relations for this model
+		 *
+		 * @access public
+		 * @var array
+		 */
+		public $hasOne = array(
+		);
+
+		/**
+		 * belongsTo relations for this model
+		 *
+		 * @access public
+		 * @var array
+		 */
+		public $belongsTo = array(
+		);
+
+		/**
+		 * hasMany relations for this model
+		 *
+		 * @access public
+		 * @var array
+		 */
+		public $hasMany = array(
+		);
+
+		/**
+		 * hasAndBelongsToMany relations for this model
+		 *
+		 * @access public
+		 * @var array
+		 */
+		public $hasAndBelongsToMany = array(
+		);
+
+		/**
+		 * overload the construct method so that you can use translated validation
+		 * messages.
+		 *
+		 * @access public
+		 *
+		 * @param mixed $id string uuid or id
+		 * @param string $table the table that the model is for
+		 * @param string $ds the datasource being used
+		 *
+		 * @return void
+		 */
+		public function __construct($id = false, $table = null, $ds = null) {
+			parent::__construct($id, $table, $ds);
+
+			foreach($this->Behaviors->attached() as $attached) {
+				$this->Behaviors->detach($attached);
+			}
+			
+			$this->hasOne = $this->hasMany = $this->belongsTo = $this->hasAndBelongsToMany = array();
+			
+			$this->validate = array(
+			);
+		}
+	}
