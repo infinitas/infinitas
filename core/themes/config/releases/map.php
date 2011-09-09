0dc758b10aa69d89d637a38c9bf4d08138cd8f49
dogmatic69 <dogmatic69@gmail.com>
2011-09-09 13:43:44 +0100

    updating themes plugin to use uuid and innodb and utf8

--- core/themes/config/releases/map.php
+++ core/themes/config/releases/map.php
@@ -4,5 +4,7 @@ $map = array(
 		'000008_themes' => 'R4c94edce538c4968a3a678d86318cd70'),
 	2 => array(
 		'000081_themes' => 'R4e27469857644de9903d49266318cd70'),
+	3 => array(
+		'000082_themes' => 'R4e6a045f4ac048b2ad4c01486318cd70'),
 );
 ?>
