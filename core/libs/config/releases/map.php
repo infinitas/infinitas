8a7ab1ec4f7ed9e5aeb15c934fa77e7849752511
dogmatic69 <dogmatic69@gmail.com>
2011-09-09 13:45:17 +0100

    updating libs plugin to use uuid and innodb and utf8

--- core/libs/config/releases/map.php
+++ core/libs/config/releases/map.php
@@ -4,5 +4,7 @@ $map = array(
 		'000008_libs' => 'R4c94edcd4680495f801a78d86318cd70'),
 	2 => array(
 		'000081_libs' => 'R4e274386ffe846f88c1b49266318cd70'),
+	3 => array(
+		'000082_libs' => 'R4e6a0416a0304defabe901486318cd70'),
 );
 ?>
