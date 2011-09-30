fbb59fe4842469850c354d9a774a2cb1bd9547b6
dogmatic69 <dogmatic69@gmail.com>
2011-10-01 00:24:55 +0100

    adding a before filter so that the events will auto filter out and data for disabled plugins

--- core/events/models/behaviors/event.php
+++ core/events/models/behaviors/event.php
@@ -12,4 +12,50 @@
 		public function triggerEvent($Model, $eventName, $data = array()){
 			return EventCore::trigger($Model, $eventName, $data);
 		}
+
+		public function beforeFind($Model, $query) {
+			if($Model->hasField('plugin')) {
+				$query['conditions'] = array_merge(
+					!empty($query['conditions']) ? $query['conditions'] : array(),
+					$this->__getPossiblePlugins($Model, 'plugin')
+				);
+			}
+			
+			if($Model->hasField('class')) {
+				pr('asd');
+				exit;
+			}
+			
+			return $query;
+		}
+
+		/**
+		 * @brief build up some conditions to only select rows with active pluging data
+		 *
+		 * This stops things like routes and modules (or any other data) that is plugin
+		 * specific being pulled out when a plugin is disabled
+		 *
+		 * @access private
+		 *
+		 * @param Model $Model the model doing the find
+		 *
+		 * @return array conditions to add to a find
+		 */
+		private function __getPossiblePlugins($Model, $field) {
+			$camelCasePlugins = EventCore::getAvailablePlugins();
+
+			$mixedCasePlugins = array();
+			foreach($camelCasePlugins as $plugin) {
+				$mixedCasePlugins[] = $plugin;
+				$mixedCasePlugins[] = Inflector::underscore($plugin);
+			}
+
+			return array(
+				'and' => array(
+					'or' => array(
+						$Model->alias . '.' . $field => $mixedCasePlugins
+					)
+				)
+			);
+		}
 	}
