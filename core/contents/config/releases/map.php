addad7e2f95b8a263d1aac396c71d06a88fe3af3
dogmatic69 <dogmatic69@gmail.com>
2012-02-05 21:21:48 +0000

    creating an updated release for the contents plugin

--- core/contents/config/releases/map.php
+++ core/contents/config/releases/map.php
@@ -2,5 +2,7 @@
 $map = array(
 	1 => array(
 		'000008_contents' => 'R4ce2a9e20b9c4ecd87d315fb6318cd70'),
+	2 => array(
+		'000081_contents' => 'R4f2ef2cd04f04dc994d909b76318cd70'),
 );
 ?>
--- core/contents/config/schema/schema.php
+++ core/contents/config/schema/schema.php
@@ -1,6 +1,6 @@
 <?php 
 /* SVN FILE: $Id$ */
-/* Contents schema generated on: 2010-11-16 15:11:22 : 1289923042*/
+/* Contents schema generated on: 2012-02-05 21:02:17 : 1328476877*/
 class ContentsSchema extends CakeSchema {
 	var $name = 'Contents';
 
@@ -11,6 +11,23 @@ class ContentsSchema extends CakeSchema {
 	function after($event = array()) {
 	}
 
+	var $global_categories = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'title' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'slug' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'description' => array('type' => 'text', 'null' => true, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'active' => array('type' => 'boolean', 'null' => false, 'default' => '0', 'key' => 'index'),
+		'group_id' => array('type' => 'integer', 'null' => true, 'default' => NULL, 'length' => 3, 'key' => 'index'),
+		'item_count' => array('type' => 'integer', 'null' => false, 'default' => '0'),
+		'parent_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'lft' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'index'),
+		'rght' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'views' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1), 'tree_list_all' => array('column' => array('lft', 'id', 'title', 'rght'), 'unique' => 1), 'cat_idx' => array('column' => array('active', 'group_id'), 'unique' => 0), 'idx_access' => array('column' => 'group_id', 'unique' => 0)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
 	var $global_contents = array(
 		'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'primary', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'model' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'key' => 'index', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
