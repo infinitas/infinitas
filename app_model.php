c2a6e5800a4b4e2078313a12576ede176e2e944f
dogmatic69 <dogmatic69@gmail.com>
2012-02-07 22:35:26 +0000

    adding some methods for custom find pagination

--- app_model.php
+++ app_model.php
@@ -164,6 +164,8 @@
 
 			parent::__construct($id, $table, $ds);
 
+			$this->_findMethods['paginatecount'] = true;
+
 			$this->__setupDatabaseConnections();
 			
 			$thisClass = get_class($this);
@@ -490,6 +492,57 @@
 		 */
 		public function fullModelName() {
 			return $this->plugin . '.' . $this->alias;
+		}
+
+		/**
+		 * @brief find a count for pagination
+		 *
+		 * Removes 'fields' key from count query on custom finds when it is an array,
+		 * as it will completely break the Model::_findCount() call
+		 *
+		 * @param string $state Either "before" or "after"
+		 * @param array $query
+		 * @param array $data
+		 * @return int The number of records found, or false
+		 * @access protected
+		 * @see Model::find()
+		 */
+		function _findPaginatecount($state, $query, $results = array()) {
+			if ($state == 'before' && isset($query['operation'])) {
+				if (!empty($query['fields']) && is_array($query['fields'])) {
+					if (!preg_match('/^count/i', $query['fields'][0])) {
+						unset($query['fields']);
+					}
+				}
+			}
+
+			return parent::_findCount($state, $query, $results);
+		}
+
+		/**
+		 * @brief Custom Model::paginateCount() method to support custom model find pagination
+		 *
+		 * @param array $conditions
+		 * @param int $recursive
+		 * @param array $extra
+		 *
+		 * @return array
+		 */
+		function paginateCount($conditions = array(), $recursive = 0, $extra = array()) {
+			$parameters = compact('conditions');
+
+			if ($recursive != $this->recursive) {
+				$parameters['recursive'] = $recursive;
+			}
+
+			if (isset($extra['type']) && isset($this->_findMethods[$extra['type']])) {
+				$extra['operation'] = 'count';
+				return $this->find($extra['type'], array_merge($parameters, $extra));
+			}
+
+			else {
+				return $this->find('count', array_merge($parameters, $extra));
+			}
 		}
 	}
 
--- core/contents/contents_app_model.php
+++ core/contents/contents_app_model.php
@@ -17,19 +17,4 @@
 	 */
 
 	class ContentsAppModel extends AppModel {
-		public function paginateCount($conditions = array(), $recursive = 0, $extra = array()) {
-			$parameters = compact('conditions');
-			if ($recursive != $this->recursive) {
-				$parameters['recursive'] = $recursive;
-			}
-			
-			if (isset($extra['type']) && isset($this->_findMethods[$extra['type']])) {
-				$extra['operation'] = 'count';
-				return $this->find($extra['type'], array_merge($parameters, $extra));
-			}
-
-			else {
-				return $this->find('count', array_merge($parameters, $extra));
-			}
-		}
 	}
