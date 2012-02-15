b10637b779d53ff587c2d4c46ea1b6e14ac1c7a2
dogmatic69 <dogmatic69@gmail.com>
2012-02-15 17:28:42 +0000

    using a modified version of cakes default routes that only includes plugin routing

--- Config/routes.php
+++ Config/routes.php
@@ -2,4 +2,34 @@
 	App::uses('InfinitasRouting', 'Routes.Lib');
 
 	InfinitasRouting::setup();
-	require CAKE . 'Config' . DS . 'routes.php';
+
+	$prefixes = Router::prefixes();
+	$plugins = CakePlugin::loaded();
+
+	App::uses('PluginShortRoute', 'Routing/Route');
+	foreach ($plugins as $key => $value) {
+		$plugins[$key] = Inflector::underscore($value);
+	}
+	$pluginPattern = implode('|', $plugins);
+	$match = array('plugin' => $pluginPattern);
+	$shortParams = array('routeClass' => 'PluginShortRoute', 'plugin' => $pluginPattern);
+
+	foreach ($prefixes as $prefix) {
+		$params = array('prefix' => $prefix, $prefix => true);
+		$indexParams = $params + array('action' => 'index');
+		Router::connect("/{$prefix}/:plugin", $indexParams, $shortParams);
+		Router::connect("/{$prefix}/:plugin/:controller", $indexParams, $match);
+		Router::connect("/{$prefix}/:plugin/:controller/dashboard", $params + array('action' => 'dashboard'), $match);
+		Router::connect("/{$prefix}/:plugin/:controller/:action/*", $params, $match);
+	}
+	Router::connect('/:plugin', array('action' => 'index'), $shortParams);
+	Router::connect('/:plugin/:controller', array('action' => 'index'), $match);
+	Router::connect('/:plugin/:controller/:action/*', array(), $match);
+
+	$namedConfig = Router::namedConfig();
+	if ($namedConfig['rules'] === false) {
+		Router::connectNamed(true);
+	}
+
+	unset($namedConfig, $params, $indexParams, $prefix, $prefixes, $shortParams, $match,
+		$pluginPattern, $plugins, $key, $value);
