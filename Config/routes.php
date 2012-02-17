03e835a7548a8a5d3024b5a9ea3fdc80a590a315
dogmatic69 <dogmatic69@gmail.com>
2012-02-17 21:25:59 +0000

    using InfinitasRouter instead of the cake one

--- Config/routes.php
+++ Config/routes.php
@@ -1,7 +1,8 @@
 <?php	
-	App::uses('InfinitasRouting', 'Routes.Lib');
+	App::uses('InfinitasRouter', 'Routes.Routing');
+	App::uses('Router', 'Routing');
 
-	InfinitasRouting::setup();
+	InfinitasRouter::setup();
 
 	$prefixes = Router::prefixes();
 	$plugins = CakePlugin::loaded();
@@ -17,19 +18,22 @@
 	foreach ($prefixes as $prefix) {
 		$params = array('prefix' => $prefix, $prefix => true);
 		$indexParams = $params + array('action' => 'index');
-		Router::connect("/{$prefix}/:plugin", $indexParams, $shortParams);
-		Router::connect("/{$prefix}/:plugin/:controller", $indexParams, $match);
-		Router::connect("/{$prefix}/:plugin/:controller/dashboard", $params + array('action' => 'dashboard'), $match);
-		Router::connect("/{$prefix}/:plugin/:controller/:action/*", $params, $match);
+		InfinitasRouter::connect("/{$prefix}/:plugin", $indexParams, $shortParams);
+		InfinitasRouter::connect("/{$prefix}/:plugin/:controller", $indexParams, $match);
+		InfinitasRouter::connect("/{$prefix}/:plugin/:controller/dashboard", $params + array('action' => 'dashboard'), $match);
+		InfinitasRouter::connect("/{$prefix}/:plugin/:controller/:action/*", $params, $match);
 	}
-	Router::connect('/:plugin', array('action' => 'index'), $shortParams);
-	Router::connect('/:plugin/:controller', array('action' => 'index'), $match);
-	Router::connect('/:plugin/:controller/:action/*', array(), $match);
+	InfinitasRouter::connect('/:plugin', array('action' => 'index'), $shortParams);
+	InfinitasRouter::connect('/:plugin/:controller', array('action' => 'index'), $match);
+	InfinitasRouter::connect('/:plugin/:controller/:action/*', array(), $match);
 
-	$namedConfig = Router::namedConfig();
+	$namedConfig = InfinitasRouter::namedConfig();
 	if ($namedConfig['rules'] === false) {
-		Router::connectNamed(true);
+		InfinitasRouter::connectNamed(true);
 	}
 
 	unset($namedConfig, $params, $indexParams, $prefix, $prefixes, $shortParams, $match,
 		$pluginPattern, $plugins, $key, $value);
+
+	$request = array('plugin' => 'blog', 'controller' => 'blog_posts', 'action' => 'view');
+	$params = array('category' => 'seo', 'slug' => 'seo', 'foo' => 'bar', 'id' => 1);
