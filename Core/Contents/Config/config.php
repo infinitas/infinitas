b5e96dfceb4d0d0069bce48e84cb5408715d56fc
dogmatic69 <dogmatic69@gmail.com>
2012-02-18 00:37:41 +0000

    fix the copy paste error

--- Core/Contents/Config/config.php
+++ Core/Contents/Config/config.php
@@ -3,8 +3,8 @@
 		'truncate_category' => 100,
 		 'slugUrl' => array(
 			 'category' => array(
-				 'BlogPost.id' => 'id',
-				 'BlogPost.slug' => 'slug',
+				 'GlobalCategory.id' => 'id',
+				 'GlobalCategory.slug' => 'slug',
 				 'url' => array(
 					 'plugin' => 'contents',
 					 'controller' => 'global_categories',
