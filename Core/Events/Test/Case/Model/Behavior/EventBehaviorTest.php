3e9a914c769d0bd771a98e60a69aab6a9bbf7ac3
Ceeram <c33ram@gmail.com>
2012-02-20 01:18:27 +0100

    fixed Events tests, also added AllCoreTestsTest

--- Core/Events/Test/Case/Model/Behavior/EventBehaviorTest.php
+++ Core/Events/Test/Case/Model/Behavior/EventBehaviorTest.php
@@ -1,11 +1,11 @@
 <?php
 	/* Event Test cases generated on: 2010-12-14 22:12:32 : 1292364992*/
-
+	App::uses('Model', 'Model');
 	class DummyModel extends Model {
 		public $useTable = false;
 	}
 
-	class EventBehaviorTestCase extends CakeTestCase {
+	class EventBehaviorTest extends CakeTestCase {
 		public function startTest() {
 			$this->DummyModel = new DummyModel();
 			$this->DummyModel->Behaviors->attach('Events.Event');
@@ -16,18 +16,18 @@
 			ClassRegistry::flush();
 		}
 
-		public function testEvent(){			
+		public function testEvent(){
 			$expected = array('foo'=> array());
 			$this->assertEqual($expected, $this->DummyModel->triggerEvent('foo'));
-			$this->assertEqual($expected, $this->DummyModel->triggerEvent('plugin.foo'));
+			$this->assertEqual($expected, $this->DummyModel->triggerEvent('Plugin.foo'));
 
 			$global = $this->DummyModel->triggerEvent('returnEventForTest');
-			$plugin = $this->DummyModel->triggerEvent('events.returnEventForTest');
+			$plugin = $this->DummyModel->triggerEvent('Events.returnEventForTest');
 
 			/**
 			 * test calling the plugin method vs global returns the same format.
 			 */
-			$this->assertIsA($global['returnEventForTest']['events']->Handler, 'DummyModel');
-			$this->assertIsA($plugin['returnEventForTest']['events']->Handler, 'DummyModel');
+			$this->assertIsA($global['returnEventForTest']['Events']->Handler, 'DummyModel');
+			$this->assertIsA($plugin['returnEventForTest']['Events']->Handler, 'DummyModel');
 		}
 	}
