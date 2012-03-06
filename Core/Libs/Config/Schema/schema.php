74e0724257b119f1c888e896f1268276932ca049
dogmatic69 <dogmatic69@gmail.com>
2012-03-06 16:05:37 +0000

    updating the release for the libs plugin

--- Core/Libs/Config/Schema/schema.php
+++ Core/Libs/Config/Schema/schema.php
@@ -1,26 +1,11 @@
-<?php 
-/* SVN FILE: $Id$ */
-/* Libs schema generated on: 2011-09-09 13:09:30 : 1315570710*/
+<?php
 class LibsSchema extends CakeSchema {
-	var $name = 'Libs';
 
-	function before($event = array()) {
+	public function before($event = array()) {
 		return true;
 	}
 
-	function after($event = array()) {
+	public function after($event = array()) {
 	}
 
-	var $ratings = array(
-		'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'primary', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'class' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'foreign_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
-		'rating' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 3),
-		'user_id' => array('type' => 'integer', 'null' => true, 'default' => NULL),
-		'ip' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
-		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
-		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
-	);
 }
-?>
