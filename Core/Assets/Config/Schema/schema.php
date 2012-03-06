6a7ff638f51ee56660ddf6af8dfe691bb23900d9
dogmatic69 <dogmatic69@gmail.com>
2012-03-06 15:43:07 +0000

    updating the release for the assets plugin

--- Core/Assets/Config/Schema/schema.php
+++ Core/Assets/Config/Schema/schema.php
@@ -1,15 +1,11 @@
-<?php 
-/* SVN FILE: $Id$ */
-/* Assets schema generated on: 2011-07-25 23:07:15 : 1311634395*/
+<?php
 class AssetsSchema extends CakeSchema {
-	var $name = 'Assets';
 
-	function before($event = array()) {
+	public function before($event = array()) {
 		return true;
 	}
 
-	function after($event = array()) {
+	public function after($event = array()) {
 	}
 
 }
-?>
--- Core/Assets/Config/releases/map.php
+++ Core/Assets/Config/releases/map.php
@@ -1,8 +1,6 @@
 <?php
 $map = array(
 	1 => array(
-		'000008_assets' => 'R4c94edcbfba44cae9af978d86318cd70'),
-	2 => array(
-		'000081_assets' => 'R4e2df3dbd42044869d2a42086318cd70'),
+		'000009_Assets' => 'R4f56305f4fdc4b428be83c276318cd70'),
 );
 ?>
