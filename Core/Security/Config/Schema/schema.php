006ec8b393b2e163ee9af5fcc5c730ccc3c6f2c6
dogmatic69 <dogmatic69@gmail.com>
2012-03-06 16:07:43 +0000

    updating the release for the security plugin

--- Core/Security/Config/Schema/schema.php
+++ Core/Security/Config/Schema/schema.php
@@ -1,15 +1,11 @@
-<?php 
-/* SVN FILE: $Id$ */
-/* Security schema generated on: 2011-07-21 19:07:15 : 1311274335*/
+<?php
 class SecuritySchema extends CakeSchema {
-	var $name = 'Security';
 
-	function before($event = array()) {
+	public function before($event = array()) {
 		return true;
 	}
 
-	function after($event = array()) {
+	public function after($event = array()) {
 	}
 
 }
-?>
--- Core/Security/Config/releases/map.php
+++ Core/Security/Config/releases/map.php
@@ -1,6 +1,6 @@
 <?php
 $map = array(
 	1 => array(
-		'000008_security' => 'R4e28755f802c4c31909421136318cd70'),
+		'000009_Security' => 'R4f5634d491e04949bbbe43556318cd70'),
 );
 ?>
