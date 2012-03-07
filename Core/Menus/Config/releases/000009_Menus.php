51c24cab7c2fd936b10c130a39cb6d32b574d8a0
dogmatic69 <dogmatic69@gmail.com>
2012-03-07 01:28:16 +0000

    updating some sample data in the releases

--- Core/Menus/Config/releases/000009_Menus.php
+++ Core/Menus/Config/releases/000009_Menus.php
@@ -31,103 +31,56 @@
 	*/
 		public $migration = array(
 			'up' => array(
-			'create_table' => array(
-				'core_menu_items' => array(
-					'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'primary', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 200, 'collate' => 'utf8_general_ci', 'charset' => 'utf8', 'after' => 'id'),
-					'slug' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 200, 'collate' => 'utf8_general_ci', 'charset' => 'utf8', 'after' => 'name'),
-					'link' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8', 'after' => 'slug'),
-					'prefix' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 50, 'collate' => 'utf8_general_ci', 'charset' => 'utf8', 'after' => 'link'),
-					'plugin' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8', 'after' => 'prefix'),
-					'controller' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8', 'after' => 'plugin'),
-					'action' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8', 'after' => 'controller'),
-					'params' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8', 'after' => 'action'),
-					'force_backend' => array('type' => 'boolean', 'null' => false, 'default' => '0', 'after' => 'params'),
-					'force_frontend' => array('type' => 'boolean', 'null' => false, 'default' => '0', 'after' => 'force_backend'),
-					'class' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8', 'after' => 'force_frontend'),
-					'active' => array('type' => 'boolean', 'null' => false, 'default' => '0', 'after' => 'class'),
-					'menu_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'index', 'after' => 'active'),
-					'group_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'index', 'after' => 'menu_id'),
-					'parent_id' => array('type' => 'integer', 'null' => true, 'default' => '0', 'after' => 'group_id'),
-					'lft' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'after' => 'parent_id'),
-					'rght' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'after' => 'lft'),
-					'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL, 'after' => 'rght'),
-					'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL, 'after' => 'created'),
-					'indexes' => array(
-						'PRIMARY' => array('column' => 'id', 'unique' => 1),
-						'menuItems_groupIndex' => array('column' => 'group_id', 'unique' => 0),
-						'menuItems_menuIndex' => array('column' => 'menu_id', 'unique' => 0),
+				'create_table' => array(
+					'core_menu_items' => array(
+						'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'primary', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+						'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 200, 'collate' => 'utf8_general_ci', 'charset' => 'utf8', 'after' => 'id'),
+						'slug' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 200, 'collate' => 'utf8_general_ci', 'charset' => 'utf8', 'after' => 'name'),
+						'link' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8', 'after' => 'slug'),
+						'prefix' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 50, 'collate' => 'utf8_general_ci', 'charset' => 'utf8', 'after' => 'link'),
+						'plugin' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8', 'after' => 'prefix'),
+						'controller' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8', 'after' => 'plugin'),
+						'action' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8', 'after' => 'controller'),
+						'params' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8', 'after' => 'action'),
+						'force_backend' => array('type' => 'boolean', 'null' => false, 'default' => '0', 'after' => 'params'),
+						'force_frontend' => array('type' => 'boolean', 'null' => false, 'default' => '0', 'after' => 'force_backend'),
+						'class' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8', 'after' => 'force_frontend'),
+						'active' => array('type' => 'boolean', 'null' => false, 'default' => '0', 'after' => 'class'),
+						'menu_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'index', 'after' => 'active'),
+						'group_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'index', 'after' => 'menu_id'),
+						'parent_id' => array('type' => 'integer', 'null' => true, 'default' => '0', 'after' => 'group_id'),
+						'lft' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'after' => 'parent_id'),
+						'rght' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'after' => 'lft'),
+						'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL, 'after' => 'rght'),
+						'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL, 'after' => 'created'),
+						'indexes' => array(
+							'PRIMARY' => array('column' => 'id', 'unique' => 1),
+							'menuItems_groupIndex' => array('column' => 'group_id', 'unique' => 0),
+							'menuItems_menuIndex' => array('column' => 'menu_id', 'unique' => 0),
+						),
+						'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB'),
 					),
-					'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB'),
-				),
-				'core_menus' => array(
-					'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'primary', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8', 'after' => 'id'),
-					'type' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'key' => 'index', 'collate' => 'utf8_general_ci', 'charset' => 'utf8', 'after' => 'name'),
-					'item_count' => array('type' => 'integer', 'null' => false, 'default' => '0', 'after' => 'type'),
-					'active' => array('type' => 'boolean', 'null' => false, 'default' => '0', 'after' => 'item_count'),
-					'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL, 'after' => 'active'),
-					'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL, 'after' => 'created'),
-					'indexes' => array(
-						'PRIMARY' => array('column' => 'id', 'unique' => 1),
-						'menu_index' => array('column' => array('type', 'active'), 'unique' => 0),
+					'core_menus' => array(
+						'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'primary', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+						'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8', 'after' => 'id'),
+						'type' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'key' => 'index', 'collate' => 'utf8_general_ci', 'charset' => 'utf8', 'after' => 'name'),
+						'item_count' => array('type' => 'integer', 'null' => false, 'default' => '0', 'after' => 'type'),
+						'active' => array('type' => 'boolean', 'null' => false, 'default' => '0', 'after' => 'item_count'),
+						'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL, 'after' => 'active'),
+						'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL, 'after' => 'created'),
+						'indexes' => array(
+							'PRIMARY' => array('column' => 'id', 'unique' => 1),
+							'menu_index' => array('column' => array('type', 'active'), 'unique' => 0),
+						),
+						'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB'),
 					),
-					'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB'),
-				),
+				)
 			),
-			'drop_table' => array(
-				'menu_items', 'menus'
-			),
-		),
-		'down' => array(
-			'drop_table' => array(
-				'core_menu_items', 'core_menus'
-			),
-			'create_table' => array(
-				'menu_items' => array(
-					'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'primary', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 200, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'slug' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 200, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'link' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'prefix' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 50, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'plugin' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'controller' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'action' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'params' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'force_backend' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
-					'force_frontend' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
-					'class' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'active' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
-					'menu_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'index'),
-					'group_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'index'),
-					'parent_id' => array('type' => 'integer', 'null' => true, 'default' => '0'),
-					'lft' => array('type' => 'integer', 'null' => false, 'default' => NULL),
-					'rght' => array('type' => 'integer', 'null' => false, 'default' => NULL),
-					'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
-					'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
-					'indexes' => array(
-						'PRIMARY' => array('column' => 'id', 'unique' => 1),
-						'menuItems_groupIndex' => array('column' => 'group_id', 'unique' => 0),
-						'menuItems_menuIndex' => array('column' => 'menu_id', 'unique' => 0),
-					),
-					'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB'),
-				),
-				'menus' => array(
-					'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'primary', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'type' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'key' => 'index', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'item_count' => array('type' => 'integer', 'null' => false, 'default' => '0'),
-					'active' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
-					'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
-					'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
-					'indexes' => array(
-						'PRIMARY' => array('column' => 'id', 'unique' => 1),
-						'menu_index' => array('column' => array('type', 'active'), 'unique' => 0),
-					),
-					'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB'),
+			'down' => array(
+				'drop_table' => array(
+					'core_menu_items', 'core_menus'
 				),
 			),
-		),
 		);
 
 	
--- Core/ServerStatus/Config/releases/000009_ServerStatus.php
+++ Core/ServerStatus/Config/releases/000009_ServerStatus.php
@@ -31,35 +31,9 @@
 	*/
 		public $migration = array(
 			'up' => array(
-			'create_table' => array(
-				'core_crons' => array(
-					'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'primary', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'process_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 8),
-					'year' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 4, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'month' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 2),
-					'day' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 2),
-					'start_time' => array('type' => 'time', 'null' => false, 'default' => NULL),
-					'start_mem' => array('type' => 'integer', 'null' => false, 'default' => '0'),
-					'start_load' => array('type' => 'string', 'null' => false, 'default' => '0', 'length' => 10, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'end_time' => array('type' => 'time', 'null' => true, 'default' => NULL),
-					'end_mem' => array('type' => 'integer', 'null' => true, 'default' => NULL, 'length' => 10),
-					'end_load' => array('type' => 'string', 'null' => false, 'default' => '0', 'length' => 10, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'mem_ave' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 10, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'load_ave' => array('type' => 'string', 'null' => false, 'default' => '0', 'length' => 10, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'tasks_ran' => array('type' => 'integer', 'null' => false, 'default' => '0', 'length' => 3),
-					'done' => array('type' => 'integer', 'null' => false, 'default' => '0', 'length' => 3),
-					'indexes' => array(
-						'PRIMARY' => array('column' => 'id', 'unique' => 1),
-					),
-					'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB'),
-				),
 			),
-		),
-		'down' => array(
-			'drop_table' => array(
-				'core_crons'
+			'down' => array(
 			),
-		),
 		);
 
 	
