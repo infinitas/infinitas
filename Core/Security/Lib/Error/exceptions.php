d861e3206931e06e03c7eeb68088fc381841c5f1
dogmatic69 <dogmatic69@gmail.com>
2012-05-21 20:47:53 +0100

    adding custom security exceptions and pages

---
+++ Core/Security/Lib/Error/exceptions.php
@@ -0,0 +1,22 @@
+<?php
+	/**
+	 * @brief base Security plugin exception 
+	 */
+	class SecurityException extends InfinitasException {
+		public $layout = 'error_security';
+		
+		public function getCustomLayout() {
+			return $this->layout();
+		}
+	}
+	
+	/**
+	 * @brief exception thrown when a users IP address has been blocked 
+	 */
+	class SecurityIpAddressBlockedException extends SecurityException {
+		protected $_messageTemplate = 'Your IP address "%s" has been temporarily blocked.';
+
+		public function __construct($message, $code = null) {
+			parent::__construct($message, $code);
+		}
+	}
---
+++ Core/Security/View/Errors/ip_address_blocked.ctp
@@ -0,0 +1,9 @@
+<h2><?php echo $name; ?></h2>
+<p class="error">
+	<strong><?php echo __d('infinitas', 'Error'); ?>: </strong>
+	<?php echo __d('security', 'Your access to this site has been temporarily blocked due to your actions'); ?>
+</p>
+<p class="notice">
+	<strong><?php echo __d('infinitas', 'Notice'); ?>: </strong>
+	<?php echo __d('security', 'If you feel this has happened in error, please contact the site admin'); ?>
+</p>
