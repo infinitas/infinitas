edbdf32123b281835c7ec71979d01bff98b8dbd2
dogmatic69 <dogmatic69@gmail.com>
2012-08-10 20:18:31 +0100

    customising the coverage to exclude all the test files and other files that do not require testing

--- Test/Case/AllTestsTest.php
+++ Test/Case/AllTestsTest.php
@@ -7,11 +7,34 @@ class AllTestsTest extends PHPUnit_Framework_TestSuite {
  * @return void
  */
 	public static function suite() {
-		$suite = new CakeTestSuite('All test');
+		$suite = new static('All Application Tests');
 		$path = APP . 'Test' . DS . 'Case' . DS;
 		$suite->addTestFile($path . 'AllCoreTestsTest.php');
 		$suite->addTestFile($path . 'AllDeveloperTestsTest.php');
 		$suite->addTestFile($path . 'AllPluginTestsTest.php');
 		return $suite;
+	}
+
+	public function run(PHPUnit_Framework_TestResult $result = null, $filter = false, array $groups = array(), array $excludeGroups = array(), $processIsolation = false) {
+		if ($result === null) {
+			$result = $this->createResult();
+		}
+
+		if (!isset($this->coverageSetup) || !$this->coverageSetup) {
+			$coverage = $result->getCodeCoverage();
+			$coverage->setProcessUncoveredFilesFromWhitelist(true);
+
+			$coverageFilter = $coverage->filter();
+
+			$coverageFilter->addDirectoryToBlacklist(CORE_PATH);
+			$coverageFilter->addDirectoryToBlacklist(APP . DS . 'Test');
+			$coverageFilter->addDirectoryToBlacklist(APP . DS . 'Config');
+			foreach(InfinitasPlugin::listPlugins('all') as $plugin) {
+				$coverageFilter->addDirectoryToBlacklist(CakePlugin::path($plugin) . 'Test');
+			}
+
+			$this->coverageSetup = true;
+		}
+		return parent::run($result, $filter, $groups, $excludeGroups, $processIsolation);
 	}
 }
