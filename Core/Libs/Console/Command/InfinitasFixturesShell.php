11aa857dd97478827701f99385bb4d0eb101e641
dogmatic69 <dogmatic69@gmail.com>
2012-08-13 16:50:21 +0100

    removing reference to old code

--- Core/Libs/Console/Command/InfinitasFixturesShell.php
+++ Core/Libs/Console/Command/InfinitasFixturesShell.php
@@ -1,10 +1,10 @@
 <?php
 	App::import('lib', 'Libs.InfinitasAppShell');
 
-	class InfinitasFixturesShell extends InfinitasAppShell {
+	class InfinitasFixturesShell extends AppShell {
 		public $uses = array();
 
-		public $tasks = array('Infinitas', 'Fixture', 'InfinitasFixtureBake');
+		public $tasks = array('Fixture', 'Libs.InfinitasFixtureBake');
 
 		/**
 		 * @brief map options to methods
@@ -19,12 +19,12 @@
 
 		public function main() {
 			Configure::write('debug', 2);
-			$this->Infinitas->h1('Infinitas Fixtures');
+			$this->h1('Infinitas Fixtures');
 
-			$this->Infinitas->out('[G]enerate');
-			$this->Infinitas->out('[U]pdate');
-			$this->Infinitas->out('[I]mport');
-			$this->Infinitas->out('[Q]uit');
+			$this->out('[G]enerate');
+			$this->out('[U]pdate');
+			$this->out('[I]mport');
+			$this->out('[Q]uit');
 
 			$method = strtoupper($this->in(__('What would you like to do?'), array_keys($this->__options)));
 
@@ -44,7 +44,7 @@
 			if(!$plugins) {
 				return false;
 			}
-			
+
 			foreach($plugins as $plugin) {
 				$this->__generateFixture($plugin, true);
 			}
@@ -68,13 +68,13 @@
 		private function __generateFixture($plugin, $new) {
 			$models = App::objects('model', App::pluginPath($plugin) . 'models', false);
 			$models = array_combine(range(1, count($models)), $models);
-			
+
 			foreach($models as $k => $model) {
 				$models[$k] = Inflector::humanize(str_replace('.php', '', $model));
 			}
 			$models['A'] = 'all';
 			$models['B'] = 'back';
-			
+
 			$this->tabbedList($models);
 
 			$option = null;
