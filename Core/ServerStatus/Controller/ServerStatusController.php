e1b3d787c5a89415ced59733688a9c7efb2039fb
dogmatic69 <dogmatic69@gmail.com>
2012-09-28 15:04:47 +0100

    adding backend for checking / clearing cache

--- Core/ServerStatus/Controller/ServerStatusController.php
+++ Core/ServerStatus/Controller/ServerStatusController.php
@@ -1,88 +1,82 @@
 <?php
-	class ServerStatusController extends ServerStatusAppController {
-		public $helpers = array(
-			'ViewCounter.ViewCounter',
-			'Charts.Charts' => array(
-				'Google.GoogleStatic'
+App::uses('ClearCache', 'Data.Lib');
+
+class ServerStatusController extends ServerStatusAppController {
+	public $uses = array(
+		'ServerStatus.ServerStatus'
+	);
+
+	public function __construct(CakeRequest $request = null, CakeResponse $response = null) {
+		$this->serverLoad = serverLoad(false);
+		$this->memoryUsage = memoryUsage(false, false);
+		parent::__construct($request, $response);
+	}
+
+	public function admin_dashboard() {
+
+	}
+
+	public function admin_cache_status() {
+		$this->set('cacheStatus', ClearCache::status());
+	}
+
+	public function admin_clear_cache() {
+		ClearCache::run();
+		$this->notice(
+			__d('server_status', 'Cache cleared'),
+			array(
+				'redirect' => true
 			)
 		);
+	}
 
-		public $uses = array(
-			'ServerStatus.ServerStatus'
+	public function admin_status() {
+		$current = $current['Load'] = $allTime = array();
+
+		if(count($this->serverLoad) >= 3) {
+			$current['Load'] = array(
+				'1 min' => $this->serverLoad[0],
+				'5 min' => $this->serverLoad[1],
+				'15 min' => $this->serverLoad[2],
+			);
+		}
+
+		$current['Memory'] = array(
+			'current' => $this->memoryUsage['current'],
+			'limit' => str_replace('M', '', $this->memoryUsage['limit']) * 1024 * 1024,
 		);
 
-		public function __construct($request = null, $response = null) {
-			$this->serverLoad = serverLoad(false);
-			$this->memoryUsage = memoryUsage(false, false);
-			parent::__construct($request, $response);
-		}
+		unset($this->serverLoad, $this->memoryUsage);
+		$this->set('current', array_merge(systemInfo(), $current));
 
-		public function admin_dashboard() {
-
-		}
-
-		public function admin_cache_status() {
-			
-		}
-
-		public function admin_clear_cache() {
-			App::uses('ClearCache', 'Data.Lib');
-			ClearCache::run();
+		$data = $this->Event->trigger('Crons.areCronsSetup');
+		if(!current($data['areCronsSetup'])) {
 			$this->notice(
-				__d('server_status', 'Cache cleared'),
+				__('Crons are not currently running, reporting is disabled'),
 				array(
-					'redirect' => true
+					'level' => 'error',
+				)
+			);
+
+			$allTime = $lastTwoWeeks = $lastSixMonths = $byHour = $byDay = array();
+		}
+		else{
+			$allTime = $this->ServerStatus->reportAllTime();
+			$lastTwoWeeks = $this->ServerStatus->reportLastTwoWeeks();
+			$lastSixMonths = $this->ServerStatus->reportLastSixMonths();
+			$byHour = $this->ServerStatus->reportByHour();
+			$byDay = $this->ServerStatus->reportByDay();
+		}
+
+		$this->set(compact('allTime', 'lastTwoWeeks', 'lastSixMonths', 'byHour', 'byDay'));
+
+		if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+			$this->notice(
+				__('This information may not be accurate on windows based systems'),
+				array(
+					'level' => 'warning'
 				)
 			);
 		}
-
-		public function admin_status() {
-			$current = $current['Load'] = $allTime = array();
-
-			if(count($this->serverLoad) >= 3) {
-				$current['Load'] = array(
-					'1 min' => $this->serverLoad[0],
-					'5 min' => $this->serverLoad[1],
-					'15 min' => $this->serverLoad[2],
-				);
-			}
-
-			$current['Memory'] = array(
-				'current' => $this->memoryUsage['current'],
-				'limit' => str_replace('M', '', $this->memoryUsage['limit']) * 1024 * 1024,
-			);
-
-			unset($this->serverLoad, $this->memoryUsage);
-			$this->set('current', array_merge(systemInfo(), $current));
-
-			$data = $this->Event->trigger('Crons.areCronsSetup');
-			if(!current($data['areCronsSetup'])) {
-				$this->notice(
-					__('Crons are not currently running, reporting is disabled'),
-					array(
-						'level' => 'error',
-					)
-				);
-
-				$allTime = $lastTwoWeeks = $lastSixMonths = $byHour = $byDay = array();
-			}
-			else{
-				$allTime = $this->ServerStatus->reportAllTime();
-				$lastTwoWeeks = $this->ServerStatus->reportLastTwoWeeks();
-				$lastSixMonths = $this->ServerStatus->reportLastSixMonths();
-				$byHour = $this->ServerStatus->reportByHour();
-				$byDay = $this->ServerStatus->reportByDay();
-			}
-
-			$this->set(compact('allTime', 'lastTwoWeeks', 'lastSixMonths', 'byHour', 'byDay'));
-
-			if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-				$this->notice(
-					__('This information may not be accurate on windows based systems'),
-					array(
-						'level' => 'warning'
-					)
-				);
-			}
-		}
 	}
+}
--- Core/ServerStatus/View/ServerStatus/admin_cache_status.ctp
+++ Core/ServerStatus/View/ServerStatus/admin_cache_status.ctp
@@ -4,13 +4,44 @@
 		<thead>
 			<tr>
 				<th><?php echo __d('server_status', 'Section'); ?></th>
-				<th><?php echo __d('server_status', 'Size'); ?></th>
+				<th><?php echo __d('server_status', 'Used'); ?></th>
+				<th><?php echo __d('server_status', 'Total'); ?></th>
+				<th><?php echo __d('server_status', 'Available'); ?></th>
 			</tr>
 		</thead>
 		<tbody>
+			<?php
+				$total = 0;
+				foreach($cacheStatus as $type => $amount) { ?>
+					<tr>
+						<td>
+							<?php
+								$type = __d('server_status', Inflector::humanize($type));
+								switch(strtolower($type)) {
+									case 'apc':
+										$type = $this->Html->link(
+											$type,
+											array(
+												'plugin' => 'server_status',
+												'controller' => 'php',
+												'action' => 'apc'
+											)
+										);
+										break;
+								}
+								echo $type;
+							?>
+						</td>
+						<td><?php echo convert($amount['used']); ?></td>
+						<td><?php echo convert($amount['total']); ?></td>
+						<td><?php echo convert($amount['available']); ?></td>
+					</tr><?php
+					$total += $amount['used'];
+				}
+			?>
 			<tr>
-				<td>123</td>
-				<td>123</td>
+				<td><?php echo __d('server_status', 'Total Cache'); ?></td>
+				<td colspan="100"><?php echo convert($total); ?></td>
 			</tr>
 		</tbody>
 	</table>
