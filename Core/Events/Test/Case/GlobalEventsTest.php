fe7a046dc4c280ddd0e9232693d46266ce025c73
dogmatic69 <dogmatic69@gmail.com>
2012-09-29 18:22:32 +0100

    adding a test file that will run the tests for all events in the app

---
+++ Core/Events/Test/Case/GlobalEventsTest.php
@@ -0,0 +1,40 @@
+<?php
+class GlobalEventsTest extends PHPUnit_Framework_TestSuite {
+
+/**
+ * Suite define the tests for this suite
+ *
+ * @return void
+ */
+	public static function suite() {
+		$suite = new CakeTestSuite('Events Tests');
+
+		$it = new AllEventsTestsFilterIterator(
+                  new RecursiveIteratorIterator(
+                      new RecursiveDirectoryIterator(APP)));
+
+		for ($it->rewind(); $it->valid(); $it->next()) {
+			$suite->addTestFile($it->current()->getPathname());
+		}
+
+		return $suite;
+	}
+}
+
+/**
+ * @brief Iterator for finding event test files
+ */
+class AllEventsTestsFilterIterator extends FilterIterator {
+/**
+ * @brief find all event test files
+ *
+ * Will return true if its a match
+ *
+ * @return boolean
+ */
+	public function accept() {
+		if($this->current()->getExtension() == 'php') {
+			return strstr($this->current(), 'EventsTest.php');
+		}
+	}
+}
