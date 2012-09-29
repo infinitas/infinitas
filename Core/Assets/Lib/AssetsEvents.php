1a83fc1b58a93f3ce02e6aae3ac677d69030b64a
dogmatic69 <dogmatic69@gmail.com>
2012-09-29 15:12:33 +0100

    adding tests for asset events

--- Core/Assets/Lib/AssetsEvents.php
+++ Core/Assets/Lib/AssetsEvents.php
@@ -1,33 +1,64 @@
 <?php
-	class AssetsEvents extends AppEvents {
-		public function onRequireHelpersToLoad($event = null) {
-			return array(
-				'Assets.Compress'
-			);
-		}
+/**
+ * @brief the Events file for the Assets plugin
+ *
+ *
+ * @copyright Copyright (c) 2009 Carl Sutton ( dogmatic69 )
+ * @link http://infinitas-cms.org
+ * @package Assets.Lib
+ * @license http://www.opensource.org/licenses/mit-license.php The MIT License
+ * @since 0.9a
+ *
+ * @author dogmatic69
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ */
 
-		public function onRequireJavascriptToLoad($event, $data = null) {
-			$return = array(
-				'Assets.3rd/jquery',
-				'Assets.3rd/jquery_ui',
-				'Assets.3rd/metadata',
-				'Assets.infinitas',
-				'Assets.libs/core',
-				'Assets.libs/form',
-				'Assets.libs/html',
-				'Assets.libs/number'
-			);
-			
-			if(isset($event->Handler->params['admin']) && $event->Handler->params['admin']) {
-				$return[] = 'Assets.3rd/date';
-				$return[] = 'Assets.3rd/image_drop_down';
-			}
-			
-			else{
-				$return[] = 'Assets.3rd/rater';
-				$return[] = 'Assets.3rd/moving_boxes';
-			}
-
-			return $return;
-		}
+class AssetsEvents extends AppEvents {
+/**
+ * @brief get helpers to load
+ *
+ * @param Event $event The Event being triggered
+ *
+ * @return array
+ */
+	public function onRequireHelpersToLoad(Event $event) {
+		return array(
+			'Assets.Compress'
+		);
 	}
+
+/**
+ * @brief load javascript assets
+ *
+ * @param Event $event The Event being triggered
+ *
+ * @return array
+ */
+	public function onRequireJavascriptToLoad(Event $event) {
+		$return = array(
+			'Assets.3rd/jquery',
+			'Assets.3rd/jquery_ui',
+			'Assets.3rd/metadata',
+			'Assets.infinitas',
+			'Assets.libs/core',
+			'Assets.libs/form',
+			'Assets.libs/html',
+			'Assets.libs/number'
+		);
+
+		$added = array(
+			'Assets.3rd/rater',
+			'Assets.3rd/moving_boxes'
+		);
+		if(isset($event->Handler->params['admin']) && $event->Handler->params['admin']) {
+			$added = array(
+				'Assets.3rd/date',
+				'Assets.3rd/image_drop_down'
+			);
+		}
+
+		return array_merge($return, $added);
+	}
+}
--- Core/Assets/Test/Case/Lib/AssetsEventsTest.php
+++ Core/Assets/Test/Case/Lib/AssetsEventsTest.php
@@ -1,19 +1,74 @@
 <?php
-	class AssetsEventsTest extends CakeTestCase {
-		public function startTest() {
-			$this->Event = EventCore::getInstance();
-		}
-
-		public function endTest() {
-			unset($this->Event);
-			ClassRegistry::flush();
-		}
-
-		public function testStuff() {
-			$this->assertInstanceOf('EventCore', $this->Event);
-
-			$expected = array('requireHelpersToLoad' => array('Assets' => array('Assets.Compress')));
-			$result = $this->Event->trigger(new Object(), 'Assets.requireHelpersToLoad');
-			$this->assertEquals($expected, $result);
-		}
+class AssetsEventsTest extends CakeTestCase {
+/**
+ * @brief set up required objects for testing
+ */
+	public function startTest() {
+		$this->Object = new Object();
+		$this->Event = EventCore::getInstance();
 	}
+
+/**
+ * @brief clear up the enviroment
+ */
+	public function endTest() {
+		unset($this->Event, $this->Object);
+		ClassRegistry::flush();
+	}
+
+/**
+ * @brief test the instance is loaded correctly
+ */
+	public function testInstance() {
+		$this->assertInstanceOf('EventCore', $this->Event);
+	}
+
+/**
+ *@brief test required helpers load correctly
+ */
+	public function testRequireHelpers() {
+		$this->Object->params = array();
+		$expected = array('requireHelpersToLoad' => array('Assets' => array('Assets.Compress')));
+		$result = $this->Event->trigger($this->Object, 'Assets.requireHelpersToLoad');
+		$this->assertEquals($expected, $result);
+	}
+
+/**
+ * @brief test required js loads correctly
+ */
+	public function testRequireJavascript() {
+		$this->Object->params = array();
+		$expected = array('requireJavascriptToLoad' => array('Assets' => array(
+			'Assets.3rd/jquery',
+			'Assets.3rd/jquery_ui',
+			'Assets.3rd/metadata',
+			'Assets.infinitas',
+			'Assets.libs/core',
+			'Assets.libs/form',
+			'Assets.libs/html',
+			'Assets.libs/number',
+			'Assets.3rd/rater',
+			'Assets.3rd/moving_boxes'
+		)));
+		$result = $this->Event->trigger($this->Object, 'Assets.requireJavascriptToLoad');
+		$this->assertEquals($expected, $result);
+
+		$this->Object->params = array(
+			'admin' => true
+		);
+		$expected = array('requireJavascriptToLoad' => array('Assets' => array(
+			'Assets.3rd/jquery',
+			'Assets.3rd/jquery_ui',
+			'Assets.3rd/metadata',
+			'Assets.infinitas',
+			'Assets.libs/core',
+			'Assets.libs/form',
+			'Assets.libs/html',
+			'Assets.libs/number',
+			'Assets.3rd/date',
+			'Assets.3rd/image_drop_down'
+		)));
+		$result = $this->Event->trigger($this->Object, 'Assets.requireJavascriptToLoad');
+		$this->assertEquals($expected, $result);
+	}
+}
