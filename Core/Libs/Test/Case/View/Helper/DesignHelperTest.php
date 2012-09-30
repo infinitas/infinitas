82728a6b23ae82ae9496240b3ab1af25501b2f21
dogmatic69 <dogmatic69@gmail.com>
2012-09-30 17:54:51 +0100

    adding tests for some view methods

--- Core/Libs/Test/Case/View/Helper/DesignHelperTest.php
+++ Core/Libs/Test/Case/View/Helper/DesignHelperTest.php
@@ -34,33 +34,289 @@ class DesignHelperTest extends CakeTestCase {
 /**
  * testArrayToList method
  *
- * @return void
+ * @dataProvider arrayToListDataProvider
  */
-	public function testArrayToList() {
+	public function testArrayToList($data, $expected) {
+		if(isset($data['div'])) {
+			$result = $this->Design->arrayToList($data['data'], $data['options'], $data['div']);
+		} else {
+			$result = $this->Design->arrayToList($data['data'], $data['options']);
+		}
+		$this->assertTags($result, $expected);
 	}
 
 /**
- * testInfoBox method
- *
- * @return void
+ * @brief array to list data provider
  */
-	public function testInfoBox() {
+	public function arrayToListDataProvider() {
+		return array(
+			'plain' => array(
+				array(
+					'data' => array('a', 'b'),
+					'options' => null,
+					'div' => false
+				),
+				array(
+					array('ul' => true),
+					array('li' => true),
+					'a',
+					'/li',
+					array('li' => true),
+					'b',
+					'/li',
+					'/ul'
+				)
+			),
+			'with_div' => array(
+				array(
+					'data' => array('a', 'b'),
+					'options' => null,
+					'div' => true
+				),
+				array(
+					array('div' => true),
+					array('ul' => true),
+					array('li' => true),
+					'a',
+					'/li',
+					array('li' => true),
+					'b',
+					'/li',
+					'/ul',
+					'/div'
+				)
+			),
+			'options_div' => array(
+				array(
+					'data' => array('a', 'b'),
+					'options' => array('div' => true)
+				),
+				array(
+					array('div' => true),
+					array('ul' => true),
+					array('li' => true),
+					'a',
+					'/li',
+					array('li' => true),
+					'b',
+					'/li',
+					'/ul',
+					'/div'
+				)
+			),
+			'div_class' => array(
+				array(
+					'data' => array('a', 'b'),
+					'options' => array('div' => 'foo')
+				),
+				array(
+					array('div' => array('class' => 'foo')),
+					array('ul' => true),
+					array('li' => true),
+					'a',
+					'/li',
+					array('li' => true),
+					'b',
+					'/li',
+					'/ul',
+					'/div'
+				)
+			),
+			'div_id' => array(
+				array(
+					'data' => array('a', 'b'),
+					'options' => array('div_id' => 'foo')
+				),
+				array(
+					array('div' => array('id' => 'foo')),
+					array('ul' => true),
+					array('li' => true),
+					'a',
+					'/li',
+					array('li' => true),
+					'b',
+					'/li',
+					'/ul',
+					'/div'
+				)
+			),
+			'ul_class' => array(
+				array(
+					'data' => array('a', 'b'),
+					'options' => 'foo'
+				),
+				array(
+					array('div' => array('class' => 'foo')),
+					array('ul' => array('class' => 'foo')),
+					array('li' => true),
+					'a',
+					'/li',
+					array('li' => true),
+					'b',
+					'/li',
+					'/ul',
+					'/div'
+				)
+			),
+			'ul_class_no_div' => array(
+				array(
+					'data' => array('a', 'b'),
+					'options' => 'foo', 'div' => false
+				),
+				array(
+					array('ul' => array('class' => 'foo')),
+					array('li' => true),
+					'a',
+					'/li',
+					array('li' => true),
+					'b',
+					'/li',
+					'/ul'
+				)
+			),
+			'ul_class_id' => array(
+				array(
+					'data' => array('a', 'b'),
+					'options' => array('ul' => 'foo', 'ul_id' => 'bar'),
+					'div' => false
+				),
+				array(
+					array('ul' => array('class' => 'foo', 'id' => 'bar')),
+					array('li' => true),
+					'a',
+					'/li',
+					array('li' => true),
+					'b',
+					'/li',
+					'/ul'
+				)
+			),
+			'div_class_id_ul_class_id' => array(
+				array(
+					'data' => array('a', 'b'),
+					'options' => array('ul' => 'ul-class', 'ul_id' => 'ul-id', 'div' => 'div-class', 'div_id' => 'div-id'),
+				),
+				array(
+					array('div' => array('class' => 'div-class', 'id' => 'div-id')),
+					array('ul' => array('class' => 'ul-class', 'id' => 'ul-id')),
+					array('li' => true),
+					'a',
+					'/li',
+					array('li' => true),
+					'b',
+					'/li',
+					'/ul',
+					'/div'
+				)
+			)
+		);
 	}
 
 /**
- * testQuickLink method
+ * @brief test tab exception
  *
- * @return void
+ * @expectedException InvalidArgumentException
  */
-	public function testQuickLink() {
+	public function testTabException() {
+		$result = $this->Design->tabs(array('a', 'b'), array('A'));
 	}
 
 /**
  * testTabs method
  *
- * @return void
+ * @dataProvider tabsDataProvider
  */
-	public function testTabs() {
+	public function testTabs($data, $expected) {
+		$result = $this->Design->tabs($data['tabs'], $data['content']);
+		$this->assertTags($result, $expected);
+	}
+
+/**
+ * @brief data provider for testing tabs
+ *
+ * @return array
+ */
+	public function tabsDataProvider() {
+		$linkRegex = 'preg:/#([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})\-%d/';
+		$divRegex = 'preg:/([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})\-%d/';
+		return array(
+			'basic' => array(
+				array(
+					'tabs' => array('A'),
+					'content' => array('a')
+				),
+				array(
+					array('div' => array('class' => 'tabs')),
+						array('ul' => true),
+							array('li' => true),
+								array('a' => array('href' => sprintf($linkRegex, 0))),
+									'A',
+								'/a',
+							'/li',
+						'/ul',
+						array('div' => array('id' => sprintf($divRegex, 0))),
+							'a',
+						'/div',
+					'/div'
+				)
+			),
+			'multi' => array(
+				array(
+					'tabs' => array(array('text' => 'A'), 'B', 'C'),
+					'content' => array('a', 'b', 'c')
+				),
+				array(
+					array('div' => array('class' => 'tabs')),
+						array('ul' => true),
+							array('li' => true),
+								array('a' => array('href' => sprintf($linkRegex, 0))),
+									'A',
+								'/a',
+							'/li',
+							array('li' => true),
+								array('a' => array('href' => sprintf($linkRegex, 1))),
+									'B',
+								'/a',
+							'/li',
+							array('li' => true),
+								array('a' => array('href' => sprintf($linkRegex, 2))),
+									'C',
+								'/a',
+							'/li',
+						'/ul',
+						array('div' => array('id' => sprintf($divRegex, 0))),
+							'a',
+						'/div',
+						array('div' => array('id' => sprintf($divRegex, 1))),
+							'b',
+						'/div',
+						array('div' => array('id' => sprintf($divRegex, 2))),
+							'c',
+						'/div',
+					'/div'
+				)
+			),
+			'custom_links' => array(
+				array(
+					'tabs' => array(array('text' => 'A', 'config' => array('class' => 'foo'))),
+					'content' => array('a')
+				),
+				array(
+					array('div' => array('class' => 'tabs')),
+						array('ul' => true),
+							array('li' => true),
+								array('a' => array('href' => sprintf($linkRegex, 0), 'class' => 'foo')),
+									'A',
+								'/a',
+							'/li',
+						'/ul',
+						array('div' => array('id' => sprintf($divRegex, 0))),
+							'a',
+						'/div',
+					'/div'
+				)
+			)
+		);
 	}
 
 }
--- Core/Libs/View/Helper/DesignHelper.php
+++ Core/Libs/View/Helper/DesignHelper.php
@@ -1,192 +1,120 @@
 <?php
-	/**
-	*/
-	class DesignHelper extends AppHelper {
-		public $helpers = array('Text', 'Html');
+/**
+ * @brief DesignHelper to provide methods for building the interface
+ */
+class DesignHelper extends AppHelper {
+/**
+ * @brief helpers that are used in this helper
+ *
+ * @var array
+ */
+	public $helpers = array(
+		'Text',
+		'Html'
+	);
 
-		public function arrayToList($array = array(), $class = null, $div = false) {
-			if(!empty($class['div'])) {
+/**
+ * @brief convert an array to a list <li>
+ *
+ * @param array $array the data to convert
+ * @param mixed $class the class / configs for the list
+ * @param boolean $div Wrap the list in a div
+ *
+ * @return string
+ */
+	public function arrayToList(array $array, $class = null, $div = null) {
+		if(isset($class['div']) && $class['div']) {
+			if(is_bool($class['div'])) {
+				unset($class['div']);
+			}
+
+			if($div === null) {
 				$div = true;
 			}
-			if(!is_array($class)) {
-				$class = array(
-					'div' => $class,
-					'div-id' => null,
-					'ul' => $class,
-					'ul-id' => null,
-					'li' => null
-				);
-			}
-			$class = array_merge(
-				array('div' => null, 'div_id' => null, 'ul' => null, 'ul_id' => null, 'li' => null),
-				$class
-			);
-			$base = '%s';
-			if($div) {
-				$base = '<div id=":div_id" class=":div">%s</div>';
-			}
-
-			$base = sprintf(
-				$base,
-				sprintf(
-					'<ul id=":ul_id" class=":ul"><li class=":li">%s</li></ul>',
-					implode('</li><li class=":li">', (array)$array)
-				)
-			);
-			
-
-			return str_replace(array('id=""', 'class=""'), '', String::insert($base, $class));
 		}
-
-		public function infoBox($info = array(), $truncate = 24) {
-			list($heading, $data, $link, $image) = $info;
-
-			if (empty($data)) {
-				$this->errors[] = 'No data supplied';
-				return false;
+		if(isset($class['div_id']) && $class['div_id']) {
+			if($div === null) {
+				$div = true;
 			}
-
-			$out = '<div class="infoBox">';
-			$out .= '<div class="heading">' . $heading . '</div>';
-			$out .= '<div class="body">';
-			foreach($data as $id => $text) {
-				if ($text == '' || empty($text) || !$text) {
-					continue;
-				}
-
-				$text = $this->Text->truncate($text, $this->__adjustTruncate($truncate, $image, $link));
-
-				if ($image) {
-					list($imagePath, $url, $params) = $image;
-					$url = $this->__createUrl($url, $id);
-
-					$urlParams = $params;
-					unset($urlParams['alt']);
-
-					$out .= $this->Html->link($this->Html->image($imagePath, $params) . ' ' . $text,
-						$url,
-						$urlParams + array('escape' => false)
-					);
-				}
-
-				else if ($link) {
-					list($url, $params) = $link;
-					$url = $this->__createUrl($url, $id);
-
-					unset($params['alt']);
-					$out .= $this->Html->link($text,
-						$url,
-						$params
-						);
-				}
-
-				else if ($data) {
-					$out .= $text;
-				}
-			}
-
-			$out .= '</div>';
-			$out .= '</div>';
-
-			return $out;
 		}
-
-		public function quickLink($info = array(), $truncate = 24) {
-			if (count($info) < 2 || empty($info)) {
-				$this->errors[] = 'No data to generate links';
-				return false;
-			}
-
-			list($link, $url, $params) = $info;
-
-			$link = $this->__niceText($link, $truncate);
-
-			if (empty($link)) {
-				$this->errors[] = 'No text for the link supplied';
-				return false;
-			}
-
-			return $this->Html->link($link,
-				$url,
-				(array)$params + array('class' => 'quickLink')
+		if(!is_array($class)) {
+			$class = array(
+				'div' => $class,
+				'div_id' => null,
+				'ul' => $class,
+				'ul_id' => null,
+				'li' => null
 			);
 		}
-
-		public function tabs($tabs, $content) {
-			if(count($tabs) != count($content)) {
-				throw new Exception('Tab count does not match content');
-			}
-
-			$uuid = String::uuid();
-			$i = 0;
-			foreach($tabs as $k => $tab) {
-				if(!is_array($tab)) {
-					$tab = array('text' => $tab);
-				}
-				
-				$tab = array_merge(
-					array('text' => 'Missing', 'url' => '', 'config' => array()),
-					$tab
-				);
-
-				$tabs[$k] = $this->Html->link(
-					$tab['text'],
-					$tab['url'] . sprintf('#%s-%d', $uuid, $i),
-					$tab['config']
-				);
-
-				$content[$k] = sprintf('<div id="%s-%d">%s</div>', $uuid, $i, $content[$k]);
-				
-				$i++;
-			}
-
-			return sprintf(
-				'<div class="tabs">%s%s</div>',
-				$this->arrayToList($tabs),
-				implode('', $content)
-			);
+		$class = array_merge(
+			array('div' => null, 'div_id' => null, 'ul' => null, 'ul_id' => null, 'li' => null),
+			$class
+		);
+		$base = '%s';
+		if($div) {
+			$base = $this->Html->tag('div', '%s', array(
+				'id' => ':div_id',
+				'class' => ':div'
+			));
 		}
 
-		private function __createUrl($url = null, $id = 0) {
-			if (!$url) {
-				$this->errors[] = 'No url passed, returning root';
-				return '/';
-			}
+		$base = sprintf(
+			$base,
+			sprintf(
+				$this->Html->tag('ul', $this->Html->tag('li', '%s', array('class' => ':li')), array(
+					'id' => ':ul_id',
+					'class' => ':ul'
+				)),
+				implode('</li><li class=":li">', (array)$array)
+			)
+		);
 
-			if (is_array($url)) {
-				$url = $url + array($id);
-			}
 
-			else {
-				$url = rtrim($url, '/');
-				$url .= '/' . $id;
-			}
-
-			return $url;
-		}
-
-		private function __niceText($text = '', $truncate = false) {
-			$text = Inflector::humanize($text);
-			if ($truncate) {
-				$text = $this->Text->truncate($text, $truncate);
-			}
-
-			return $text;
-		}
-
-		private function __adjustTruncate($truncate = false, $image = false, $link = false) {
-			if (!$truncate) {
-				return $truncate;
-			}
-			// decrease a bit for the image
-			if ($image) {
-				$truncate = $truncate - 5;
-			}
-			// increase a bit cos there is no link
-			else if (!$link) {
-				$truncate = $truncate + 5;
-			}
-
-			return $truncate;
-		}
+		return str_replace(array('id=""', 'class=""'), '', String::insert($base, $class));
 	}
+
+/**
+ * @brief generate a tab box
+ *
+ * @param array $tabs
+ * @param array $content
+ *
+ * @return string
+ *
+ * @throws Exception
+ */
+	public function tabs(array $tabs, array $content) {
+		if(count($tabs) != count($content)) {
+			throw new InvalidArgumentException('Tab count does not match content');
+		}
+
+		$uuid = String::uuid();
+		$i = 0;
+		foreach($tabs as $k => $tab) {
+			if(!is_array($tab)) {
+				$tab = array('text' => $tab);
+			}
+
+			$tab = array_merge(
+				array('text' => 'Missing', 'url' => '', 'config' => array()),
+				$tab
+			);
+
+			$tabs[$k] = $this->Html->link(
+				$tab['text'],
+				$tab['url'] . sprintf('#%s-%d', $uuid, $i),
+				$tab['config']
+			);
+
+			$content[$k] = sprintf('<div id="%s-%d">%s</div>', $uuid, $i, $content[$k]);
+
+			$i++;
+		}
+
+		return sprintf(
+			'<div class="tabs">%s%s</div>',
+			$this->arrayToList($tabs),
+			implode('', $content)
+		);
+	}
+}
