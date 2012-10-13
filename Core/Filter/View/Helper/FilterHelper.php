454f903ede3f423a1b96f56eb199ae76802a43fd
dogmatic69 <dogmatic69@gmail.com>
2012-10-13 12:46:32 +0100

    better names on the filter removal links

--- Core/Filter/View/Helper/FilterHelper.php
+++ Core/Filter/View/Helper/FilterHelper.php
@@ -111,10 +111,15 @@
 
 				$done[] = $_f;
 
-				$text = explode(':', $_f);
-				$text = explode('.', $text[0]);
+				$text = explode('.', current(explode(':', $_f)));
 				$text = (count($text ) > 1) ? $text[1] : $text[0];
 
+				if(strpos($text, $this->request->params['plugin']) === 0) {
+					$text = substr_replace($text, '', 0, strlen($this->request->params['plugin']));
+				}
+				if(substr($text, -3) == '_id') {
+					$text = substr($text, 0, -3);
+				}
 				$out .= '<div class="left">'.
 							'<div class="remove">'.
 									$this->Html->link(
