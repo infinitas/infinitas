a980ba0755dd684706c3bdbc99ccc06a1a5c0c29
dogmatic69 <dogmatic69@gmail.com>
2012-10-20 22:47:55 +0100

    cleaning up the theme code

--- Core/Themes/Controller/ThemesController.php
+++ Core/Themes/Controller/ThemesController.php
@@ -1,111 +1,135 @@
 <?php
-	/**
-	 * Themes controller for managing the themes in infinitas
-	 *
-	 * Copyright (c) 2009 Carl Sutton ( dogmatic69 )
-	 *
-	 * Licensed under The MIT License
-	 * Redistributions of files must retain the above copyright notice.
-	 * @filesource
-	 * @copyright Copyright (c) 2009 Carl Sutton ( dogmatic69 )
-	 * @link http://infinitas-cms.org
-	 * @package sort
-	 * @subpackage sort.comments
-	 * @license http://www.opensource.org/licenses/mit-license.php The MIT License
-	 * @since 0.5a
-	 */
+/**
+ * Themes controller for managing the themes in infinitas
+ *
+ * Copyright (c) 2009 Carl Sutton ( dogmatic69 )
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ * @filesource
+ * @copyright Copyright (c) 2009 Carl Sutton ( dogmatic69 )
+ * @link http://infinitas-cms.org
+ * @package sort
+ * @subpackage sort.comments
+ * @license http://www.opensource.org/licenses/mit-license.php The MIT License
+ * @since 0.5a
+ */
 
-	class ThemesController extends ThemesAppController {
-		public function admin_index() {
-			$this->Theme->recursive = 1;
-			$themes = $this->Paginator->paginate(null, $this->Filter->filter);
+class ThemesController extends ThemesAppController {
+/**
+ * @brief list available themes
+ * 
+ * @return void
+ */
+	public function admin_index() {
+		$this->Theme->recursive = 1;
+		$themes = $this->Paginator->paginate(null, $this->Filter->filter);
 
-			$filterOptions = $this->Filter->filterOptions;
-			$filterOptions['fields'] = array(
-				'name' => $this->Theme->find('list'),
-				'licence',
-				'author',
-				'core' => Configure::read('CORE.core_options'),
-				'active' => Configure::read('CORE.active_options')
-			);
+		$filterOptions = $this->Filter->filterOptions;
+		$filterOptions['fields'] = array(
+			'name' => $this->Theme->find('list'),
+			'licence',
+			'author',
+			'core' => Configure::read('CORE.core_options'),
+			'active' => Configure::read('CORE.active_options')
+		);
 
-			$this->set(compact('themes', 'filterOptions'));
-		}
+		$this->set(compact('themes', 'filterOptions'));
+	}
 
-		public function admin_add() {
-			parent::admin_add();
-			
-			if(!$themes = $this->Theme->notInstalled()) {
-				$this->notice(
-					__('You do not have any themes to add'),
-					array(
-						'level' => 'warning',
-						'redirect' => true
-					)
-				);
-			}
-			
-			$this->set(compact('themes'));
-		}
-
-		public function admin_edit($id) {
-			parent::admin_edit($id);
-			$themes = $this->Theme->notInstalled();
-			$themes[$this->request->data['Theme']['name']] = $this->request->data['Theme']['name'];
-			$this->set(compact('themes'));
-		}
+/**
+ * @brief add a new theme
+ * 
+ * @return void
+ */
+	public function admin_add() {
+		parent::admin_add();
 		
-		public function frontend_css() {
-			$this->layout = 'ajax';
-			$this->response->type('css');
-			$css = $this->Event->trigger('requireCssToLoad');
-			$this->set('css', array_filter(array_values(Set::flatten($css))));
-		}
-
-		/**
-		 * Mass toggle action.
-		 *
-		 * This overwrites the default toggle action so that other themes can
-		 * be deactivated first as you should only have one active at a time.
-		 *
-		 * @var array $ids the id of the theme to toggle
-		 */
-		public function __massActionToggle($ids) {
-			if (count($ids) > 1) {
-				$this->notice(
-					__('Please select only one theme to be active'),
-					array(
-						'level' => 'warning',
-						'redirect' => true
-					)
-				);
-			}
-
-			if ($this->Theme->deactivateAll()) {
-				return $this->MassAction->toggle($ids);
-			}
-
+		if(!$themes = $this->Theme->notInstalled()) {
 			$this->notice(
-				__('There was a problem deactivating the other theme'),
+				__d('themes', 'You do not have any themes to add'),
 				array(
-					'level' => 'error',
+					'level' => 'warning',
 					'redirect' => true
 				)
 			);
 		}
 		
-		/**
-		 * @brief redirect to the installer to add a new theme.
-		 * 
-		 * @param null $ids not used
-		 */
-		public function __massActionInstall($ids) {
-			$this->redirect(
+		$this->set(compact('themes'));
+	}
+
+/**
+ * @brief edit an existing theme
+ * 
+ * @param string $id the id of the theme to edit
+ * 
+ * @return void
+ */
+	public function admin_edit($id) {
+		parent::admin_edit($id);
+		$themes = $this->Theme->notInstalled();
+		$themes[$this->request->data['Theme']['name']] = $this->request->data['Theme']['name'];
+		try{
+			$defaultLayouts = InfinitasTheme::layouts($this->request->data['Theme']['id']);
+		} catch(Exception $e) {
+			$this->notice($e->getMessage(), array(
+				'level' => 'warning'
+			));
+		}
+		$this->set(compact('themes', 'defaultLayouts'));
+	}
+	
+	public function frontend_css() {
+		$this->layout = 'ajax';
+		$this->response->type('css');
+		$css = $this->Event->trigger('requireCssToLoad');
+		$this->set('css', array_filter(array_values(Set::flatten($css))));
+	}
+
+	/**
+	 * Mass toggle action.
+	 *
+	 * This overwrites the default toggle action so that other themes can
+	 * be deactivated first as you should only have one active at a time.
+	 *
+	 * @var array $ids the id of the theme to toggle
+	 */
+	public function __massActionToggle($ids) {
+		if (count($ids) > 1) {
+			$this->notice(
+				__d('themes', 'Please select only one theme to be active'),
 				array(
-					'plugin' => 'installer',
-					'controller' => 'plugins',
-					'action' => 'install'
+					'level' => 'warning',
+					'redirect' => true
 				)
 			);
 		}
+
+		if ($this->Theme->deactivateAll()) {
+			return $this->MassAction->toggle($ids);
+		}
+
+		$this->notice(
+			__d('themes', 'There was a problem deactivating the other theme'),
+			array(
+				'level' => 'error',
+				'redirect' => true
+			)
+		);
 	}
+	
+	/**
+	 * @brief redirect to the installer to add a new theme.
+	 * 
+	 * @param null $ids not used
+	 */
+	public function __massActionInstall($ids) {
+		$this->redirect(
+			array(
+				'plugin' => 'installer',
+				'controller' => 'plugins',
+				'action' => 'install'
+			)
+		);
+	}
+}
--- Core/Themes/View/Themes/admin_form.ctp
+++ Core/Themes/View/Themes/admin_form.ctp
@@ -1,41 +1,49 @@
 <?php
-    /**
-     * Management Config admin edit post.
-     *
-     * this page is for admin to manage the setup of the site
-     *
-     * Copyright (c) 2009 Carl Sutton ( dogmatic69 )
-     *
-     * Licensed under The MIT License
-     * Redistributions of files must retain the above copyright notice.
-     *
-     * @filesource
-     * @copyright     Copyright (c) 2009 Carl Sutton ( dogmatic69 )
-     * @link          http://infinitas-cms.org
-     * @package       management
-     * @subpackage    management.views.configs.admin_edit
-     * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
-     */
+/**
+ * Management Config admin edit post.
+ *
+ * this page is for admin to manage the setup of the site
+ *
+ * Copyright (c) 2009 Carl Sutton ( dogmatic69 )
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @filesource
+ * @copyright     Copyright (c) 2009 Carl Sutton ( dogmatic69 )
+ * @link          http://infinitas-cms.org
+ * @package       management
+ * @subpackage    management.views.configs.admin_edit
+ * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
+ */
 
-    echo $this->Form->create('Theme');
-        echo $this->Infinitas->adminEditHead(); ?>
-		<fieldset>
-			<h1><?php echo __('Theme'); ?></h1><?php
-			echo $this->Form->input('id');
-			echo $this->Form->input('name', array('options' => $themes, 'type' => 'select', 'empty' => Configure::read('Website.empty_select')));
-			
-			if($this->request->params['action'] == 'admin_edit') {
-				echo $this->Form->input('default_layout', array('options' => InfinitasTheme::layouts($this->request->data['Theme']['id'])));
-			}
-			
-			echo $this->Form->input('active'); ?>
-		</fieldset>
-		<fieldset>
-			<h1><?php echo __('Author'); ?></h1><?php
-			echo $this->Form->input('author');
-			echo $this->Form->input('url');
-			echo $this->Form->input('update_url');
-			echo $this->Form->input('licence');
-			echo $this->Infinitas->wysiwyg('Theme.description'); ?>
-		</fieldset>
-    <?php echo $this->Form->end(); ?>
+echo $this->Form->create(null, array(
+    'inputDefaults' => array(
+        'empty' => Configure::read('Website.empty_select')
+    )
+));
+    echo $this->Infinitas->adminEditHead(); 
+    echo $this->Form->input('id');
+
+    $tabs = array(
+        __d('themes', 'Theme'),
+        __d('themes', 'Author')
+    );
+
+    $contents = array(
+        implode('', array(
+            $this->Form->input('name', array('options' => $themes, 'type' => 'select')),
+            !empty($defaultLayouts) ? $this->Form->input('default_layout', array('options' => $defaultLayouts)) : '',
+            $this->Form->input('active')
+        )),
+        implode('', array(
+            $this->Form->input('author'),
+            $this->Form->input('url'),
+            $this->Form->input('update_url'),
+            $this->Form->input('licence'),
+            $this->Infinitas->wysiwyg('Theme.description')
+        ))
+    );
+
+    echo $this->Design->tabs($tabs, $contents);
+echo $this->Form->end();
