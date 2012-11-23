6af597133ad3d5522cc1bdf10febd277ccb4bea7
dogmatic69 <dogmatic69@gmail.com>
2012-11-23 16:15:55 +0000

    update the theme record to have admin/frontend enabled

--- Core/Themes/Config/releases/000092_Themes.php
+++ Core/Themes/Config/releases/000092_Themes.php
@@ -54,6 +54,27 @@
 		),
 		);
 
+	public $fixtures = array(
+		'core' => array(
+			'Theme' => array(
+				array(
+					'id' => '2f5a3a81-f1cc-44b4-0000-199b6318cd63',
+					'name' => 'infinitas',
+					'description' => 'The default theme for Infinitas CMS',
+					'author' => 'Infinitas CMS',
+					'url' => 'http://infinitas-cms.org',
+					'update_url' => 'http://infinitas-cms.org',
+					'licence' => 'MIT',
+					'default_layout' => null,
+					'admin' => 1,
+					'frontend' => 1,
+					'created' => '2012-11-22 01:39:54',
+					'modified' => '2012-11-22 01:39:54'
+				)
+			)
+		)
+	);
+
 
 	/**
 	* Before migration callback
