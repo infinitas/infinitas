b0cada6caf021082f5cc2f3b498e5ad3cbb579f1
dogmatic69 <dogmatic69@gmail.com>
2012-12-01 01:05:38 +0000

    adding cookie defaults

--- Core/Security/Config/config.php
+++ Core/Security/Config/config.php
@@ -5,6 +5,15 @@
 	$config['Security'] = array(
 		'level' => 'medium',
 		'salt' => 'dev-DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi',
+		'Cookie' => array(
+			'name' => 'InfinitasCookie',
+			'key' => 'is~#^Ow!aAqs*&sXHKb$@1v!@*(XSLdre@34S#$%)asGqSI2321~_+!@#',
+			'domain' => '',
+			'time' => '5 days',
+			'path' => '/',
+			'secure' => false,
+			'http_only' => false
+		),
 		'login_attempts' => 3,
 
 		'encryption_secret' => '­sç„¢˜4m™¤ÀšžË',
