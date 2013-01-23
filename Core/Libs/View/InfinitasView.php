9b282e6daa64ac9df61edf3113eca10176b57a4e
dogmatic69 <dogmatic69@gmail.com>
2013-01-23 00:17:59 +0000

    cleaning up the view class

--- Core/Libs/View/InfinitasView.php
+++ Core/Libs/View/InfinitasView.php
@@ -1,238 +1,275 @@
 <?php
-	/**
-	 * Infinitas View
-	 *
-	 * makes the mustache templating class available in the views, and extends
-	 * the Theme View to allow the use of themes.
-	 *
+/**
+ * Infinitas View
+ *
+ * makes the mustache templating class available in the views, and extends
+ * the Theme View to allow the use of themes.
+ *
+ * @copyright Copyright (c) 2010 Carl Sutton ( dogmatic69 )
+ *
+ * @link http://www.infinitas-cms.org
+ * @package Infinitas.Libs.View
+ * @license http://www.opensource.org/licenses/mit-license.php The MIT License
+ * @since 0.8a
+ *
+ * @author Carl Sutton <dogmatic69@infinitas-cms.org>
+ */
 
-	 *
-	 * @filesource
-	 * @copyright Copyright (c) 2010 Carl Sutton ( dogmatic69 )
-	 * @link http://www.infinitas-cms.org
-	 * @package Infinitas.Libs.View
-	 * @license http://www.opensource.org/licenses/mit-license.php The MIT License
-	 * @since 0.8a
-	 *
-	 * @author Carl Sutton <dogmatic69@infinitas-cms.org>
-	 */
-	App::uses('Mustache', 'Libs.Lib');
+App::uses('Mustache', 'Libs.Lib');
 
-	class InfinitasView extends View {
-		/**
-		 * place holder for the mustache templating engine.
-		 */
-		public $Mustache = null;
+/**
+ * Infinitas View
+ *
+ * @package Infinitas.Libs.View
+ *
+ * @param Mustache $Mustache
+ */
 
-		/**
-		 * internal cache of template parts from the entire system
-		 */
-		private $__mustacheTemplates = array();
+class InfinitasView extends View {
 
-		/**
-		 * internal cache of vars that are used in the mustache template rendering
-		 */
-		private $__vars = array();
+/**
+ * place holder for the mustache templating engine.
+ *
+ * @var array
+ */
+	public $Mustache = null;
 
-		/**
-		 * get the
-		 */
-		public function __construct($Controller, $register = true) {
-			$this->Mustache = new Mustache();
-			parent::__construct($Controller, $register);
+/**
+ * internal cache of template parts from the entire system
+ *
+ * @var array
+ */
+	private $_mustacheTemplates = array();
 
-			$this->__setJsVariables();
+/**
+ * internal cache of vars that are used in the mustache template rendering
+ *
+ * @var array
+ */
+	private $_vars = array();
+
+/**
+ * Constructor
+ *
+ * @param Controller $Controller the controller instance
+ *
+ * @return void
+ */
+	public function __construct(Controller $Controller) {
+		$this->Mustache = new Mustache();
+		parent::__construct($Controller);
+
+		$this->_setJsVariables();
+	}
+
+/**
+ * render views
+ *
+ * Let cake render the view as per normal, then pass the data to Mustache
+ * to render the data into any templates
+ *
+ * @param string $viewFile the file that is being rendered
+ * @param array $data see cake docs
+ *
+ * @return string
+ */
+	protected function _render($viewFile, $data = array()) {
+		$this->_loadHelpers();
+
+		$out = parent::_render($viewFile, $data);
+		$this->_renderMustache($out);
+		$this->_parseSnips($out);
+
+		return $out;
+	}
+
+/**
+ * render any mustache templates with the viewVars
+ *
+ * you can pass ?mustache=false in the url to see the raw output skipping
+ * the template rendering. could be handy for debugging. if debug is off
+ * this has no effect.
+ *
+ * @param string $out the output for the browser
+ *
+ * @return void
+ */
+	protected function _renderMustache(&$out) {
+		if (Configure::read('debug') < 1 && isset($this->request['url']['mustache'])) {
+			unset($this->request['url']['mustache']);
 		}
 
-		/**
-		 * render views
-		 *
-		 * Let cake render the view as per normal, then pass the data to Mustache
-		 * to render the data into any templates
-		 *
-		 * @param string $viewFile the file that is being rendered
-		 * @param array $data see cake docs
-		 *
-		 * @return string
-		 */
-		protected function _render($viewFile, $data = array()) {
-			$this->__loadHelpers();
-
-			$out = parent::_render($viewFile, $data);
-			$this->__renderMustache($out);
-			$this->__parseSnips($out);
-
-			return $out;
+		if ($this->_skipMustacheRender()) {
+			return;
 		}
 
-		/**
-		 * render any mustache templates with the viewVars
-		 *
-		 * you can pass ?mustache=false in the url to see the raw output skipping
-		 * the template rendering. could be handy for debugging. if debug is off
-		 * this has no effect.
-		 *
-		 * @param string $out the output for the browser
-		 *
-		 * @return void
-		 */
-		private function __renderMustache(&$out) {
-			if (Configure::read('debug') < 1 && isset($this->request['url']['mustache'])) {
-				unset($this->request['url']['mustache']);
-			}
-
-			if ($this->__skipMustacheRender()) {
-				return;
-			}
-
-			if (empty($this->__mustacheTemplates)) {
-				$this->__mustacheTemplates = array_filter(current($this->Event->trigger('requireGlobalTemplates')));
-			}
-
-			foreach ($this->__mustacheTemplates as $plugin => $template) {
-				$this->__vars['viewVars'][Inflector::classify($plugin) . 'Template'] = $template;
-			}
-
-			$this->__vars['viewVars']  = &$this->viewVars;
-			$this->__vars['viewVars']['templates'] =& $this->__mustacheTemplates['requireGlobalTemplates'];
-			$this->__vars['params']	= &$this->request->params;
-
-			if (empty($this->__vars['viewVars']['infinitasJsData']['config'])) {
-				$this->__vars['viewVars']['infinitasJsData']['config'] = Configure::read();
-			}
-			$out = $this->renderTemplate($out, $this->__vars['viewVars']);
+		if (empty($this->_mustacheTemplates)) {
+			$this->_mustacheTemplates = array_filter(current($this->Event->trigger('requireGlobalTemplates')));
 		}
 
-		/**
-		 * @breif render a mustache template with the passed in variables
-		 *
-		 * @param string $template the mustache template
-		 * @param array $variables variables that will be instered into the template
-		 *
-		 * @return string
-		 */
-		public function renderTemplate($template, array $variables = array()) {
-			return $this->Mustache->render($template, $variables);
+		foreach ($this->_mustacheTemplates as $plugin => $template) {
+			$this->_vars['viewVars'][Inflector::classify($plugin) . 'Template'] = $template;
 		}
 
-		/**
-		 * check if mustache should be used to render a template
-		 *
-		 * only on for admin or it renders the stuff in the editor which is pointless
-		 * could maybe just turn it off for edit or some other work around
-		 */
-		private function __skipMustacheRender() {
-			return (isset($this->request->params['admin']) && $this->request->params['admin']) || !($this->Mustache instanceof Mustache) ||
-				(isset($this->request['url']['mustache']) && $this->request['url']['mustache'] == 'false');
+		$this->_vars['viewVars']  = &$this->viewVars;
+		$this->_vars['viewVars']['templates'] =& $this->_mustacheTemplates['requireGlobalTemplates'];
+		$this->_vars['params']	= &$this->request->params;
+
+		if (empty($this->_vars['viewVars']['infinitasJsData']['config'])) {
+			$this->_vars['viewVars']['infinitasJsData']['config'] = Configure::read();
 		}
+		$out = $this->renderTemplate($out, $this->_vars['viewVars']);
+	}
 
-		private function __loadHelpers() {
-			$helpers = EventCore::trigger($this, 'requireHelpersToLoad');
-			foreach ($helpers['requireHelpersToLoad'] as $plugin) {
-				foreach ((array)$plugin as $helper => $config) {
-					if (is_int($helper) && is_string($config)) {
-						$helper = $config;
-						$config = array();
-					}
+/**
+ * render a mustache template with the passed in variables
+ *
+ * @param string $template the mustache template
+ * @param array $variables variables that will be instered into the template
+ *
+ * @return string
+ */
+	public function renderTemplate($template, array &$variables = array()) {
+		return $this->Mustache->render($template, $variables);
+	}
 
-					$this->Helpers->load($helper, $config);
-				}
-			}
-		}
+/**
+ * check if mustache should be used to render a template
+ *
+ * only on for admin or it renders the stuff in the editor which is pointless
+ * could maybe just turn it off for edit or some other work around
+ *
+ * @return boolean
+ */
+	protected function _skipMustacheRender() {
+		return (isset($this->request->params['admin']) && $this->request->params['admin']) ||
+			!($this->Mustache instanceof Mustache) ||
+			(isset($this->request['url']['mustache']) && $this->request['url']['mustache'] == 'false');
+	}
 
-		/**
-		 * Set some data for the infinitas js lib.
-		 */
-		private function __setJsVariables() {
-			if ($this->request->is('ajax')) {
-				return false;
-			}
-			if (!empty($this->request->params['models'])) {
-				$model = current($this->request->params['models']);
-			}
-			$infinitasJsData['base']	= $this->request->base;
-			$infinitasJsData['here']	= $this->request->here;
-			$infinitasJsData['plugin']	= $this->request->params['plugin'];
-			$infinitasJsData['name']	= $this->name;
-			$infinitasJsData['action']	= $this->request->params['action'];
-			$infinitasJsData['params']	= $this->request->params;
-			$infinitasJsData['passedArgs'] = !empty($this->request->params['pass']) ? $this->request->params['pass'] : array();
-			$infinitasJsData['data']	   = $this->request->data;
-
-			$infinitasJsData['model']	   = isset($model) ? $model['className'] : null;
-
-			$infinitasJsData['config']	 = Configure::read();
-
-			$this->set(compact('infinitasJsData'));
-		}
-
-		/**
-		 * look for and insert dynamic snips
-		 *
-		 * @param string $out the data for output by reference
-		 *
-		 * @return void
-		 */
-		private function __parseSnips(&$out) {
-			if (substr($this->request->params['action'], 0, 5) == 'admin') {
-				return;
-			}
-
-			preg_match_all('/\[(snip|module):([A-Za-z0-9_\-\.\#]*)(.*?)\]/i', $out, $snips);
-			$snips = array_unique($snips[0]);
-
-			if (empty($snips)) {
-				return;
-			}
-
-			foreach ($snips as $key => $match) {
-				try {
-					$pos = strpos($out, $match);
-					if ($pos !== false) {
-						$out = substr_replace($out, $this->__parseSnipParams($match), $pos, strlen($match));
-					}
+/**
+ * Load helpers from events
+ *
+ * @return void
+ */
+	protected function _loadHelpers() {
+		$helpers = EventCore::trigger($this, 'requireHelpersToLoad');
+		foreach ($helpers['requireHelpersToLoad'] as $plugin) {
+			foreach ((array)$plugin as $helper => $config) {
+				if (is_int($helper) && is_string($config)) {
+					$helper = $config;
+					$config = array();
 				}
 
-				catch(Exception $e) {
-					$out = str_replace($match, $e->getMessage(), $out);
-					continue;
-				}
-			}
-		}
-
-		/**
-		 * figure out what was requested and load the module
-		 *
-		 * @param <type> $match
-		 * @return <type>
-		 */
-		private function __parseSnipParams($match) {
-			if (empty($match)) {
-				return false;
-			}
-
-			$params = array(
-				'modelClass' => null,
-				'id' => null
-			);
-
-			$match = str_replace(array('[', ']'), '', $match);
-			if (strstr($match, '#') === false) {
-				$match .= '#';
-			}
-
-			list($params['modelClass'], $params['id']) = explode('#', $match);
-			list($params['type'], $params['modelClass']) = explode(':', $params['modelClass']);
-			list($params['plugin'], $params['model']) = pluginSplit($params['modelClass']);
-
-			switch($params['type']) {
-				case 'snip':
-					return $this->ModuleLoader->loadDirect($params['plugin'] . '.snip', $params);
-					break;
-
-				case 'module':
-					return $this->ModuleLoader->loadDirect($params['plugin'] . '.' . $params['model'], $params);
-					break;
+				$this->Helpers->load($helper, $config);
 			}
 		}
 	}
+
+/**
+ * Set some data for the infinitas js lib.
+ *
+ * @return void
+ */
+	protected function _setJsVariables() {
+		if ($this->request->is('ajax')) {
+			return false;
+		}
+		if (!empty($this->request->params['models'])) {
+			$model = current($this->request->params['models']);
+		}
+		$infinitasJsData['base']	= $this->request->base;
+		$infinitasJsData['here']	= $this->request->here;
+		$infinitasJsData['plugin']	= $this->request->params['plugin'];
+		$infinitasJsData['name']	= $this->name;
+		$infinitasJsData['action']	= $this->request->params['action'];
+		$infinitasJsData['params']	= $this->request->params;
+		$infinitasJsData['passedArgs'] = !empty($this->request->params['pass']) ? $this->request->params['pass'] : array();
+		$infinitasJsData['data']	   = $this->request->data;
+
+		$infinitasJsData['model']	   = isset($model) ? $model['className'] : null;
+
+		$infinitasJsData['config']	 = Configure::read();
+
+		$this->set(compact('infinitasJsData'));
+	}
+
+/**
+ * look for and insert dynamic snips
+ *
+ * Page output is passed by reference. Snips are not parsed in the backend due to issues of saving
+ * the parsed content for example.
+ *
+ * @param string $out the data for output by reference
+ *
+ * @return void
+ */
+	protected function _parseSnips(&$out) {
+		if (substr($this->request->params['action'], 0, 5) == 'admin') {
+			return;
+		}
+
+		$snips = array();
+		preg_match_all('/\[(snip|module):([A-Za-z0-9_\-\.\#]*)(.*?)\]/i', $out, $snips);
+		$snips = array_unique($snips[0]);
+
+		if (empty($snips)) {
+			return;
+		}
+
+		foreach ($snips as $match) {
+			try {
+				$pos = strpos($out, $match);
+				if ($pos !== false) {
+					$out = substr_replace($out, $this->_parseSnipParams($match), $pos, strlen($match));
+				}
+			} catch(Exception $e) {
+				$out = str_replace($match, $e->getMessage(), $out);
+				continue;
+			}
+		}
+	}
+
+/**
+ * figure out what was requested and load the module
+ *
+ * - snip: load a module called snip.ctp / snip/module.ctp with params
+ * - module: load the selected module
+ *
+ * [snip:Plugin#params]
+ * [module:Plugin.module#params]
+ *
+ * @param string $match the text to match on
+ *
+ * @return boolean|string
+ */
+	protected function _parseSnipParams($match) {
+		if (empty($match)) {
+			return false;
+		}
+
+		$params = array(
+			'modelClass' => null,
+			'id' => null
+		);
+
+		$match = str_replace(array('[', ']'), '', $match);
+		if (strstr($match, '#') === false) {
+			$match .= '#';
+		}
+
+		list($params['modelClass'], $params['id']) = explode('#', $match);
+		list($params['type'], $params['modelClass']) = explode(':', $params['modelClass']);
+		list($params['plugin'], $params['model']) = pluginSplit($params['modelClass']);
+
+		switch($params['type']) {
+			case 'snip':
+				return $this->ModuleLoader->loadDirect($params['plugin'] . '.snip', $params);
+				break;
+
+			case 'module':
+				return $this->ModuleLoader->loadDirect($params['plugin'] . '.' . $params['model'], $params);
+				break;
+		}
+	}
+}
