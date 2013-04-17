627667f4c2a933e8f42a4cb990b632afd5e023fc
dogmatic69 <dogmatic69@gmail.com>
2013-04-17 10:29:18 +0100

    cache groups

--- Core/Data/Lib/ClearCache.php
+++ Core/Data/Lib/ClearCache.php
@@ -133,6 +133,68 @@ class ClearCache {
 	}
 
 /**
+ * Clears groups of cache engines
+ *
+ * @param mixed any amount of strings - keys of configured cache groups
+ * @return array associative array with cleanup results
+ */
+	public function groups() {
+		if ($cacheDisabled = (bool) Configure::read('Cache.disable')) {
+			Configure::write('Cache.disable', false);
+		}
+
+		$result = array();
+
+		$groups = self::getGroups();
+
+		if ($args = func_get_args()) {
+			$groups = array_intersect_key($groups, array_fill_keys($args, null));
+		}
+
+		foreach ($groups as $group => $engines) {
+			$result[$group] = array();
+
+			foreach ($engines as $engine) {
+				$result[$group][$engine] = Cache::clear(false, $engine);
+			}
+		}
+
+		if ($cacheDisabled) {
+			Configure::write('Cache.disable', $cacheDisabled);
+		}
+
+		return $result;
+	}
+
+/**
+ * Get list of groups with their associated cache configurations
+ *
+ * @return array
+ */
+	public static function getGroups() {
+		$groups = array();
+		$keys = Cache::configured();
+
+		foreach ($keys as $key) {
+			$config = Cache::config($key);
+
+			if (!empty($config['settings']['groups'])) {
+				foreach ($config['settings']['groups'] as $group) {
+					if (!isset($groups[$group])) {
+						$groups[$group] = array();
+					}
+
+					if (!in_array($key, $groups[$group])) {
+						$groups[$group][] = $key;
+					}
+				}
+			}
+		}
+
+		return $groups;
+	}
+
+/**
  * delete files
  *
  * @param array $files
